2f0a1667f756743d1683d4cd53c577f1347ea930
Olivier Reynard <olivier.reynard17@gmail.com>
2022-05-06 01:36:56 +0200

    pushing

--- Codewars week 21/main.js
+++ Codewars week 21/main.js
@@ -410,3 +410,19 @@ function goodVsEvil(good, evil){
 //console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'));
 
 //==========================================================================
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    return str.split('').filter(letter => !vowels.includes(letter)).join('')
+}

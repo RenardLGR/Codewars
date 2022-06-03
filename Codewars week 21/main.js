38d5738b7fdd313552eda58bac080f1df94e667a
Olivier Reynard <olivier.reynard17@gmail.com>
2022-06-03 23:30:28 +0200

    pushing

--- Codewars week 21/main.js
+++ Codewars week 21/main.js
@@ -306,3 +306,107 @@ function lastWordChar(x){
 
 
 //============================================================================
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function() {
+    return this.split('').every(letter => !alphaL.includes(letter))
+}
+
+String.prototype.isUpperCaseBis = function() {
+    return this == this.toUpperCase()
+}
+
+
+//==========================================================================
+// https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/javascript
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+function sumOfDifferences(arr) {
+    //looks like max-min ...
+  let sorted = arr.sort((a,b) => b-a)
+  let res=0
+  for(let i=0 ; i<sorted.length-1 ; i++) {
+      res += (sorted[i]-sorted[i+1])
+  }
+
+  return res
+}
+
+//console.log(sumOfDifferences([2, 1, 10]));
+
+function sumOfDifferencesBis(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+}
+
+//==========================================================================
+// https://www.codewars.com/kata/52761ee4cffbc69732000738/train/javascript
+// Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different races will certainly be involved. Each race has a certain worth when battling against others. On the side of good we have the following races, with their associated worth:
+
+// Hobbits: 1
+// Men: 2
+// Elves: 3
+// Dwarves: 3
+// Eagles: 4
+// Wizards: 10
+
+// On the side of evil we have:
+// Orcs: 1
+// Men: 2
+// Wargs: 2
+// Goblins: 2
+// Uruk Hai: 3
+// Trolls: 5
+// Wizards: 10
+// Although weather, location, supplies and valor play a part in any battle, if you add up the worth of the side of good and compare it with the worth of the side of evil, the side with the larger worth will tend to win.
+
+// Thus, given the count of each of the races on the side of good, followed by the count of each of the races on the side of evil, determine which side wins.
+
+// Input:
+// The function will be given two parameters. Each parameter will be a string of multiple integers separated by a single space. Each string will contain the count of each race on the side of good and evil.
+
+// The first parameter will contain the count of each race on the side of good in the following order:
+
+// Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+// The second parameter will contain the count of each race on the side of evil in the following order:
+
+// Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+// All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+
+// Output:
+// Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
+
+function goodVsEvil(good, evil){
+    let goodWorth = [1, 2, 3, 3, 4, 10]
+    let evilWorth = [1, 2, 2, 2, 3, 5, 10]
+
+    let goodTotal = good.split(' ').reduce( (acc, cur, idx) => {
+        return acc+ +cur*goodWorth[idx]
+    }, 0)
+    let evilTotal = evil.split(' ').reduce( (acc, cur, idx) => {
+        return acc+ +cur*evilWorth[idx]
+    }, 0)
+
+    //console.log(goodTotal, evilTotal);
+
+    return goodTotal > evilTotal ? "Battle Result: Good triumphs over Evil" : (goodTotal == evilTotal ? "Battle Result: No victor on this battle field" : "Battle Result: Evil eradicates all trace of Good")
+}
+
+//console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1'));
+
+//==========================================================================

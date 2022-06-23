7dde00b4e6afdcf9daf9224cc242311a6ebb46bb
Olivier Reynard <olivier.reynard17@gmail.com>
2022-06-23 18:08:54 +0200

    daily push

--- Codewars week 24/main.js
+++ Codewars week 24/main.js
@@ -146,3 +146,56 @@ function isSmooth(n) {
 // console.log(isSmooth(60));
 // console.log(isSmooth(98));
 // console.log(isSmooth(111));
+
+//================================================================================
+// https://www.codewars.com/kata/62a933d6d6deb7001093de16/train/javascript
+// You are given a random string of lower-case letters. Your job is to find out how many ordered and consecutive vowels there are in the given string beginning from "a". Keep in mind that the consecutive vowel to "u" is "a" and the cycle continues.
+
+// Return an integer with the length of the consecutive vowels found.
+
+// This is better explained with a couple of examples:
+
+// You are given the string "agrtertyfikfmroyrntbvsukldkfa". The logic is that you start from the "a" and make your way right. The next vowel is an "e" and it is the consecutive vowel, then "i" and so forth until you get to "u". If you keep moving right you find "a" which happens to be the consecutive vowel. Return 6.
+// This is a slightly trickier example: you are given the string "erfaiekjudhyfimngukduo". As you move right you ignore all vowels until you get to the "a", then ignore the rest until you get to the "e", which is the consecutive vowel and so forth until you get to the "o". Return 4.
+// Note
+// For this kata, the vowels are 'a', 'e', 'i', 'o', 'u', in that order. y is not considered a vowel in this kata.
+
+function getTheVowels(word) {
+    let wordCpy = word.slice('')
+
+    let res = 0
+    let temp = 0
+    for(let i=0 ; i<wordCpy.length ; i++) {
+        if(wordCpy[i] === 'a' && temp===0){
+            res++
+            temp++
+        }
+
+        if(wordCpy[i] === 'e' && temp===1){
+            res++
+            temp++
+        }
+
+        if(wordCpy[i] === 'i' && temp===2){
+            res++
+            temp++
+        }
+
+        if(wordCpy[i] === 'o' && temp===3){
+            res++
+            temp++
+        }
+
+        if(wordCpy[i] === 'u' && temp===4){
+            res++
+            temp=0
+        }
+    }
+
+    return res
+}
+
+// console.log(getTheVowels('agrtertyfikfmroyrntbvsukldkfa'));
+// console.log(getTheVowels('erfaiekjudhyfimngukduo'));
+
+//=================================================================================

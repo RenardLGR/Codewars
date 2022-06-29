ca13599e3485bb81b458a7beab5c9c85ef34fbf6
Olivier Reynard <olivier.reynard17@gmail.com>
2022-06-29 14:30:56 +0200

    daily push

--- Codewars week 25/main.js
+++ Codewars week 25/main.js
@@ -131,7 +131,6 @@ function uncensor(infected, discovered) {
 
     for(let i=0 ; i<infected.length ; i++){
         if(arrayInfected[i]==='*'){
-            console.log('here');
             arrayInfected[i]=arrayDiscovered.shift()
         }
     }
@@ -139,4 +138,218 @@ function uncensor(infected, discovered) {
     return arrayInfected.join('')
 }
 
-console.log(uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"));
+//console.log(uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"));
+
+function uncensorBis(infected, discovered){
+    let arrayInfected=infected.split('')
+    let arrayDiscovered=discovered.split('')
+
+    return arrayInfected.map(char => char==='*' ? arrayDiscovered.shift() : char).join('')
+}
+
+//====================================================================================
+// https://www.codewars.com/kata/563d59dd8e47a5ed220000ba/train/javascript
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
+function getSumOfDigits(integer) {
+    return integer.toString().split('').reduce((acc, curr) => acc+Number(curr),0)
+}
+
+//console.log(getSumOfDigits(1337));
+
+//=================================================================================
+// https://www.codewars.com/kata/55f2b110f61eb01779000053
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a,b){
+    if(a==b) return a
+    else {
+        //How about a n(n+1)/2 ?
+        let res=0
+        for(let i=Math.min(a,b) ; i<=Math.max(a,b) ; i++){
+            res+=i
+        }
+
+        return res
+    }
+}
+
+function getSumBis(a,b){
+    //Example with getSumBis(3,8)
+    // S= 3 4 5 6 7 8
+    // S= 8 7 6 5 4 3
+    // 2S= nbTerms * (max+min+1) = (max-min) * (max+min+1)
+
+    let min = Math.min(a, b)
+    let max = Math.max(a, b)
+    return (max - min + 1) * (min + max) / 2;
+}
+
+//==================================================================================
+// https://www.codewars.com/kata/5694d22eb15d78fe8d00003a/train/javascript
+// Write a function groupIn10s which takes any number of arguments, groups them into tens, and sorts each group in ascending order.
+//Removes also duplicates
+
+// The return value should be an array of arrays, so that numbers between 0 and 9 inclusive are in position 0, numbers between 10 and 19 are in position 1, etc.
+
+// Here's an example of the required output:
+
+// const grouped = groupIn10s(8, 12, 38, 3, 17, 19, 25, 35, 50) 
+
+// grouped[0]     // [3, 8]
+// grouped[1]     // [12, 17, 19]
+// grouped[2]     // [25]
+// grouped[3]     // [35, 38]
+// grouped[4]     // undefined
+// grouped[5]     // [50]
+
+function groupIn10s(){
+    let set = new Set(arguments)
+    let noDuplicates=Array.from(set)
+
+    let tenthes = Math.floor(Math.max(...noDuplicates)/10)
+
+    let grouped= Array(tenthes+1).fill([])
+
+    //this will add the number at the right index
+    noDuplicates.forEach(num => {
+        //version with push is giving me troubles for unknown reason...
+        grouped[Math.floor(num/10)] = (grouped[Math.floor(num/10)]).concat([num])
+    })
+
+    return grouped.map(arr => arr.length==0 ? undefined : arr)
+}
+
+// console.log(groupIn10s(1, 2, 3));
+// console.log(groupIn10s(8, 12, 38, 3, 17, 19, 25, 35, 50));
+// console.log(groupIn10s(12, 10, 11, 13, 25, 28, 29, 49, 51, 90));
+//console.log(groupIn10s(91,71,1,57,2,72,24,45,23,30,29,58,50,90,59,1,55,78,17,51,23,29,9));
+
+
+function groupIn10sBis(...args){
+    let set = new Set(args)
+    let noDuplicates=Array.from(set)
+  
+    let res = []
+    for(let i=0 ; i<=Math.max(...args) ; i+=10){
+        let temp=[]
+        for(let j=0 ; j<10 ; j++){
+            if(noDuplicates.includes(i+j)) temp.push(i+j)
+        }
+        res.push(temp.length == 0 ? undefined : temp)
+    }
+
+    return res
+}
+
+// console.log(groupIn10sBis(1, 2, 3));
+// console.log(groupIn10sBis(8, 12, 38, 3, 17, 19, 25, 35, 50));
+// console.log(groupIn10sBis(12, 10, 11, 13, 25, 28, 29, 49, 51, 90));
+//console.log(groupIn10sBis(91,71,1,57,2,72,24,45,23,30,29,58,50,90,59,1,55,78,17,51,23,29,9));
+
+function groupIn10sThrice(...args) {
+    let arr = args.reduce((groups, cur) => {
+        let i = Math.floor(cur/10)
+        groups[i] = (groups[i] || []).concat([cur])
+        return groups
+    }, [])
+
+    return arr.map(group => group.sort((a,b) => a-b))
+}
+
+//console.log(groupIn10sThrice(91,71,1,57,2,72,24,45,23,30,29,58,50,90,59,1,55,78,17,51,23,29,9));
+
+
+//==================================================================================
+// https://www.codewars.com/kata/546e2562b03326a88e000020
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+    return parseInt(num.toString().split('').reduce((str, dig) => str+ Number(dig*dig), ''))
+}
+
+//==================================================================================
+// https://www.codewars.com/kata/5acc79efc6fde7838a0000a0/train/javascript
+// Given a number and a binary tree ( not a Binary Search Tree! ):
+
+// return True/true if the given number is in the tree
+// return False/false if it isn't
+// Each node in the binary tree is either of this Node class or null:
+
+// class Node {
+//   constructor(value, left = null, right = null) {
+//     this.value = value;
+//     this.left = left;
+//     this.right = right;
+//   }
+// }
+
+
+function searchBinTree(n, root) {
+    if(root){
+        if(root.value === n) return true
+        else {
+            return searchBinTree(n, root.left) || searchBinTree(n, root.right)
+        }
+    }else {
+        return false
+    }
+}
+
+//==============================================================================
+// https://www.codewars.com/kata/59b710ed70a3b7dd8f000027/train/javascript
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+// Example:
+
+// a=[1,2,0,3]
+// a.length-1=3 
+// a includes [0,3] ,hence the function should return true
+
+function isAllPossibilities(x){
+    return x.length===0 ? false : x.every((el, idx) => x.includes(idx))
+}
+
+//==============================================================================
+// https://www.codewars.com/kata/58aa68605aab54a26c0001a6/train/javascript
+// The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+// Now your task is to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+
+// Input/Output
+// [input] integer year
+// 1000 ≤ year ≤ 9000
+
+// [output] an integer
+// the minimum year number that is strictly larger than the input number year and all its digits are distinct.
+
+function distinctDigitYear(year) {
+    let res = year+1
+
+    while(new Set(res.toString()).size < 4){
+        res++
+    }
+
+    return res
+}
+
+//===============================================================================

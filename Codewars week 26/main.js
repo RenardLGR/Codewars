e0ad42a92ea2fe008126725eea41a10e049d1d1d
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-05 14:31:08 +0200

    daily push

--- Codewars week 26/main.js
+++ Codewars week 26/main.js
@@ -85,3 +85,72 @@ function sumGroups(arr) {
 
 
 //==================================================================================
+// https://www.codewars.com/kata/5476f4ca03810c0fc0000098/train/javascript
+// For any given linear sequence, calculate the function [f(x)] and return it as a string.
+
+// Assumptions for this kata are:
+
+// the sequence argument will always contain 5 values equal to f(0) - f(4).
+// the function will always be in the format "nx +/- m", "x +/- m", "nx', "x" or "m"
+// if a non-linear sequence simply return "Non-linear sequence" or Nothing in Haskell.
+
+// Examples (input -> output):
+// [0, 1, 2, 3, 4]   -> "f(x) = x"
+// [0, 3, 6, 9, 12]  -> "f(x) = 3x"
+// [1, 4, 7, 10, 13] -> "f(x) = 3x + 1"
+// [0, 0, 1, 1, 1]   -> "Non-linear sequence"
+
+function getFunction(sequence){
+    let m=sequence[0] //constant
+    let n=sequence[1]-m //slope
+
+    if(!sequence.every((el, idx) => {
+        return el===n*idx+m
+    })){ //It is not a linear function
+        return  "Non-linear sequence"
+    }
+    else{ //It is a linear function
+        let res = "f(x) = "
+
+        if(n===0){ //slope is equal to 0, result of form f(x) = +/- m
+            res+= (m>0) ? m : '- '+Math.abs(m)
+        }
+
+        else if(n===1){ //slope equal to 1
+            res+='x'
+            if(m!==0){ //non null constant, result of form "f(x) = x +/- m"
+                res+= (m>0) ? ' + '+m : ' - '+Math.abs(m)
+            }else{//null constant, result of form "f(x) = x"
+                
+            }
+        }
+
+        else if(n===-1){ //slope equal to -1
+            res+='-x'
+            if(m!==0){ //non null constant, result of form "f(x) = -x +/- m"
+                res+= (m>0) ? ' + '+m : ' - '+Math.abs(m)
+            }else{//null constant, result of form "f(x) = -x"
+                
+            }
+        }
+
+        else{//slope is included in ]-Infinity; -1[ U ]1; +Infinity[
+            res+=n+'x'
+            if(m!==0){ //non null constant, result of form "f(x) = +/-nx +/- m"
+                res+= (m>0) ? ' + '+m : ' - '+Math.abs(m)
+            }else{//null constant, result of form "f(x) = +/-nx"
+                
+            }
+        }
+
+        return res
+    }
+}
+
+// console.log(getFunction([0, 1, 2, 3, 4]));
+// console.log(getFunction([0, 3, 6, 9, 12]));
+// console.log(getFunction([1, 4, 7, 10, 13]));
+// console.log(getFunction([0, 0, 1, 1, 1]));
+
+
+//==================================================================================

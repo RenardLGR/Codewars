346ce002d50dd4da3ea9d5ffb599024b8cb4874f
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-09 23:36:46 +0200

    daily push

--- Codewars week 26/main.js
+++ Codewars week 26/main.js
@@ -305,3 +305,90 @@ function variance(town, strng) {
 }
 
 //=================================================================================
+// https://www.codewars.com/kata/53b138b3b987275b46000115
+// Karan's company makes software that provides different features based on the version of operating system of the user.
+
+// For finding which version is more recent, Karan uses the following method:
+
+// function compareVersions (version1, version2) {
+//   return parseFloat(version1) >= parseFloat(version2);
+// }
+// While this function worked for OS versions 10.6, 10.7, 10.8 and 10.9, the Operating system company just released OS version 10.10.
+
+// Karan's function fails for the new version:
+
+// compareVersions ("10.9", "10.10");       // returns true, while it should return false
+// Karan now wants to spend some time to right a more robust version comparison function that works for any future version/sub-version updates.
+
+// Help Karan write this function. Here are a few sample cases:
+
+// compareVersions("11", "10");                    // returns true
+// compareVersions("11", "11");                    // returns true
+// compareVersions("10.4.6", "10.4");              // returns true
+// compareVersions("10.4", "11");                  // returns false
+// compareVersions("10.4", "10.10");               // returns false
+// compareVersions("10.4.9", "10.5");              // returns false
+// It can be assumed that version strings are non empty and only contain numeric literals and the character '.'.
+
+function compareVersions (version1, version2) {
+    let v1=version1.split('.').map(el => Number(el))
+    let v2=version2.split('.').map(el => Number(el))
+
+    let maxLength = Math.max(v1.length, v2.length)
+    while (v1.length!==maxLength){
+        v1.push(0)
+    }
+    while (v2.length!==maxLength){
+        v2.push(0)
+    }
+
+    //for compareVersions("10.4.6", "10.4") I should have:
+    //v1 == [ 10, 4, 6 ] v2 == [ 10, 4, 0 ]
+
+    for(let i=0 ; i<maxLength ; i=i){
+        if(v1[i] > v2[i]){
+            return true
+        }
+        else if(v1[i] < v2[i]){
+            return false
+        }
+        else{ //in case of equality, check the sub version
+            i++
+        }
+    }
+
+    return true //in case every subversions are equal return true
+}
+
+// console.log(compareVersions("10.4.6", "10.4"));
+// console.log(compareVersions("10.4.9", "10.5"));
+// console.log(compareVersions("10.4.9", "10.4.9"));
+
+function compareVersionsBis(version1, version2){
+    let v1=version1.split('.').map(el => Number(el))
+    let v2=version2.split('.').map(el => Number(el))
+    let maxLength = Math.max(v1.length, v2.length)
+
+    while (v1.length!==maxLength){
+        v1.push(0)
+    }
+    while (v2.length!==maxLength){
+        v2.push(0)
+    }
+
+    for(let i=0 ;i<maxLength ; i++){
+        if(v1[i] > v2[i]){
+            return true
+        }
+        else if(v1[i] < v2[i]){
+            return false
+        }
+        else{
+            continue
+        }
+    }
+
+    return true
+}
+
+//================================================================================

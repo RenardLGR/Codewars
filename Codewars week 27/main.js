de34b0d73ec2df258219920ca9ede1221b6384aa
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-14 14:44:27 +0200

    daily push

--- Codewars week 27/main.js
+++ Codewars week 27/main.js
@@ -305,3 +305,42 @@ function faroCount(deckSize) {
 
 
 //====================================================================================
+// https://www.codewars.com/kata/56968ce7753513604b000055
+// Your task is to calculate the maximum possible height of a perfectly square pyramid (the number of complete layers) that we can build, given n number of cubes as the argument.
+
+// The top layer is always only 1 cube and is always present.
+// There are no hollow areas, meaning each layer must be fully populated with cubes.
+// The layers are thus so built that the corner cube always covers the inner 25% of the corner cube below it and so each row has one more cube than the one below it.
+// If you were given only 5 cubes, the lower layer would have 4 cubes and the top 1 cube would sit right in the middle of them, where the lower 4 cubes meet.
+
+// If you were given 14 cubes, you could build a pyramid of 3 layers (base: 9 cubes, 1st floor: 4 cubes, 2nd floor: 1cube), but 13 wouldn't be enough as you would be missing one cube, so only 2 layers would be complete and some cubes left over!
+
+// What is the tallest pyramid possible we can build from the given number of cubes? Simply return the number of complete layers.
+
+// Examples
+//  4  -->  1
+//  5  -->  2
+// 13  -->  2
+// 14  -->  3
+
+function pyramidHeight(n) {
+  //each additional floor adds an n² cubes
+  //So the total amount of cubes for a N floor pyramid is S(N) = N² + (N-1)² + ... + 1²
+  //S(N) = n*(n+1)*(2*n+1)/6
+
+  let nFloors=0
+  let nCubes=0
+  while(nCubes <= n) {
+    nFloors++
+    nCubes+=Math.pow(nFloors, 2)
+  }
+  return nFloors-1
+}
+
+// console.log(pyramidHeight(1));
+// console.log(pyramidHeight(5));
+// console.log(pyramidHeight(13));
+// console.log(pyramidHeight(14));
+// console.log(pyramidHeight(100));
+
+//==================================================================================

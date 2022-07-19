b40870abaeed01bc8290cbb6096cbc41d8a3c0f6
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-19 02:05:12 +0200

    daily push

--- Codewars week 28/main.js
+++ Codewars week 28/main.js
@@ -144,3 +144,44 @@ function persistence(num) {
 // console.log(persistence(4))
 
 //====================================================================================
+// https://www.codewars.com/kata/541c8630095125aba6000c00
+// Digital root is the recursive sum of all the digits in a number.
+// https://en.wikipedia.org/wiki/Digital_root
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+
+    let temp = n
+    while(temp.toString().length>1){
+        temp = sumDigits(temp)
+    }
+
+    return temp
+
+    function sumDigits(n){
+        return  n.toString().split('').reduce((acc, curr) => acc+ +curr,0)
+    }
+
+    // console.log(sumDigits(942));
+    // console.log(sumDigits(132189));
+}
+
+// console.log(digital_root(132189))
+// console.log(digital_root(493193))
+
+
+function digital_rootBis(n) {
+    if (n < 10) return n;
+    
+    return digital_rootBis(
+      n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+}
+
+//===============================================================================

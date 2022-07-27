d829bc3548072338f93373651e75587b785f5f08
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-27 14:10:48 +0200

    daily push

--- Codewars week 29/main.js
+++ Codewars week 29/main.js
@@ -154,3 +154,30 @@ function diagonal(n) {
 //console.log(diagonal(5));
 
 //=====================================================================================
+// https://www.codewars.com/kata/546f922b54af40e1e90001da
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let position = text.split('').map(char => {
+        if(alphabet.includes(char.toLowerCase())){ //if char is a letter, return letter+ space
+            return (alphabet.indexOf(char.toLowerCase()) + 1) + ' '
+        }else{
+            return ''
+        }
+    })
+
+    return position.join('').trim() //join and removes last space if any
+}
+
+// console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+//==================================================================================

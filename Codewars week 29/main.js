0179fca21873b7338fba85dc42ad36de56cc3628
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-28 07:32:56 +0200

    daily push

--- Codewars week 29/main.js
+++ Codewars week 29/main.js
@@ -181,3 +181,79 @@ function alphabetPosition(text) {
 // console.log(alphabetPosition("The sunset sets at twelve o' clock."));
 
 //==================================================================================
+// https://www.codewars.com/kata/5268acac0d3f019add000203
+// Create a finite automaton that has three states. Finite automatons are the same as finite state machines for our purposes.
+
+// Our simple automaton, accepts the language of A, defined as {0, 1} and should have three states: q1, q2, and q3. Here is the description of the states:
+
+// q1 is our start state, we begin reading commands from here
+// q2 is our accept state, we return true if this is our last state
+// And the transitions:
+
+// q1 moves to q2 when given a 1, and stays at q1 when given a 0
+// q2 moves to q3 when given a 0, and stays at q2 when given a 1
+// q3 moves to q2 when given a 0 or 1
+// The automaton should return whether we end in our accepted state (q2), or not (true/false).
+
+// Your task
+// You will have to design your state objects, and how your Automaton handles transitions. Also make sure you set up the three states, q1, q2, and q3 for the myAutomaton instance. The test fixtures will be calling against myAutomaton.
+
+// As an aside, the automaton accepts an array of strings, rather than just numbers, or a number represented as a string, because the language an automaton can accept isn't confined to just numbers. An automaton should be able to accept any 'symbol.'
+
+// Here are some resources on DFAs (the automaton this Kata asks you to create):
+
+// http://en.wikipedia.org/wiki/Deterministic_finite_automaton
+// http://www.cs.odu.edu/~toida/nerzic/390teched/regular/fa/dfa-definitions.html
+// http://www.cse.chalmers.se/~coquand/AUTOMATA/o2.pdf
+// Example
+// var a = new Automaton();
+// a.readCommands(["1", "0", "0", "1", "0"]);  ==> false
+// We make these transitions:
+
+// input: ["1", "0", "0", "1", "0"]
+
+// 1: q1 -> q2
+// 0: q2 -> q3
+// 0: q3 -> q2
+// 1: q2 -> q2
+// 0: q2 -> q3
+// We end in q3 which is not our accept state, so we return false
+
+function Automaton()
+{
+   this.states = [];
+}
+
+Automaton.prototype.readCommands = function(commands)
+{
+    //states will be an array of states : ["q1", "q2", "q3", "q3", ...]
+    this.states = []
+    this.states.push('q1')
+    for(let i=0 ; i<commands.length ; i++){
+        switch (this.states[i]) {
+            case 'q1':
+                if(commands[i] === "1") this.states.push('q2')
+                else this.states.push('q1')
+                break;
+
+            case 'q2':
+                if(commands[i] === "0") this.states.push('q3')
+                else this.states.push('q2')
+                break;
+
+            case 'q3':
+                this.states.push('q2')
+                break;
+        
+            default:
+                break;
+        }
+    }
+    return this.states[commands.length] === 'q2'
+}
+
+var myAutomaton = new Automaton();
+
+// Do anything necessary to set up your automaton's states, q1, q2, and q3.
+
+//=================================================================================

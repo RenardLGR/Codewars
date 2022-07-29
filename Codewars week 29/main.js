e50d759b6a41c8734d3cb9b7983bf35ca9bd1eb3
Olivier Reynard <olivier.reynard17@gmail.com>
2022-07-29 13:28:43 +0200

    daily push

--- Codewars week 29/main.js
+++ Codewars week 29/main.js
@@ -257,3 +257,54 @@ var myAutomaton = new Automaton();
 // Do anything necessary to set up your automaton's states, q1, q2, and q3.
 
 //=================================================================================
+// https://www.codewars.com/kata/5fc7d2d2682ff3000e1a3fbc
+// In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
+
+// For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
+
+// Notes:
+
+// Messages are composed of only letters and digits
+// Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
+// Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
+// If the message is an empty string, you should return true
+
+function isAValidMessage(message){
+    //I will try to get the arr of numbers and the arr of words
+    //To do so I will turn every letter into ' ' and split that to return the numbers
+    //Do the same but with number turned into ' ' to return the words
+    //And check if numbers[i] matches words[i].length
+
+    if(message.length === 0) return true //edge case
+
+    let digits = '0123456789'
+
+    if (!digits.includes(message[0])) return false //if the message starts with a letter, it is false, we want  numbers - message - numbers - message, etc
+
+    let m = message.split('')
+
+    let numbers = m.map(char => {
+        if(digits.includes(char)) return char
+        else return ' '
+    }).join('').split(' ').filter(e => e !== '').map(str => Number(str))
+
+    let words = m.map(char => {
+        if(digits.includes(char)) return ' '
+        else return char
+    }).join('').split(' ').filter(e => e !== '')
+
+    //So far with message = "4code13hellocodewars" ; we have numbers = [ 4, 13 ] and words = [ 'code', 'hellocodewars' ]
+
+    if(numbers.length !== words.length){
+        return false
+    }else{
+        return numbers.every( (num, idx) => num === words[idx].length)
+    }
+}
+
+console.log(isAValidMessage("1a2bb3ccc4dddd5eeeee")); //true
+console.log(isAValidMessage("4code13hellocodewars")); //true
+console.log(isAValidMessage("hello5")); //false
+console.log(isAValidMessage("2hi2")); //false
+
+//==================================================================================

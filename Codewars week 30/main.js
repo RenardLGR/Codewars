5c44736b6c2afa0e4235d9c7f0fa52d514c3b15f
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-06 08:23:09 +0200

    daily push

--- Codewars week 30/main.js
+++ Codewars week 30/main.js
@@ -340,3 +340,61 @@ function countOddPentaFib(n) {
 }
 
 //=================================================================================
+// https://www.codewars.com/kata/56b3b9c7a6df24cf8c00000e/train/javascript
+// A nested list (or array in JavaScript) is a list that apears as a value inside another list,
+
+// [item, item, [item, item], item]
+// in the above list, [item, item] is a nested list.
+
+// Your goal is to write a function that determines the depth of the deepest nested list within a given list.
+// return 1 if there are no nested lists. The list passed to your function can contain any data types.
+
+// A few examples:
+
+// arrayDepth([true]) // returns 1
+
+// arrayDepth([]) // returns 1
+
+// arrayDepth([2, "yes", [true, false]]) // returns 2
+
+// arrayDepth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1]) // returns 6
+
+// arrayDepth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]]) // returns 2
+
+function arrayDepth(array) {
+    //We are going to check if array contains an array
+    //And flat() it until it doesn't
+	let counter = 1
+    let arr = array.slice()
+
+    while(arr.some(el => Array.isArray(el))){
+        arr = arr.flat()
+        counter++
+    }
+
+    return counter
+}
+
+// console.log(arrayDepth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]]));
+// console.log(arrayDepth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1]));
+
+//I believe it is working, codewars doesn't support .flat() method, so here is an other solution:
+
+function arrayDepthBis(array) {
+	let counter = 1
+    let arr = array.slice()
+
+    while(arr.some(el => Array.isArray(el))){
+        //arr.flat() is equivalent to arr.reduce((acc, val) => acc.concat(val), [])
+        arr = arr.reduce((acc, cur) => acc.concat(cur) , [])
+        counter++
+    }
+
+    return counter
+}
+
+// console.log(arrayDepthBis([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]]));
+// console.log(arrayDepthBis([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1]));
+
+
+//=================================================================================

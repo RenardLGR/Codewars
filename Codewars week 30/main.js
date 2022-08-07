dbe91a8f45ba291479be8062c32fc556bb324d8d
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-07 13:23:11 +0200

    end of the week

--- Codewars week 30/main.js
+++ Codewars week 30/main.js
@@ -398,3 +398,72 @@ function arrayDepthBis(array) {
 
 
 //=================================================================================
+// https://www.codewars.com/kata/5629b94e34e04f8fb200008e/train/javascript
+// Every positive integer number, that is not prime, may be decomposed in prime factors. For example the prime factors of 20, are:
+
+// 2, 2, and 5, because: 20 = 2 . 2 . 5
+// The first prime factor (the smallest one) of 20 is 2 and the last one (the largest one) is 5. The sum of the first and the last prime factors, sflpf of 20 is: sflpf = 2 + 5 = 7
+
+// The number 998 is the only integer in the range [4, 1000] that has a value of 501 , so its sflpf equals to 501, but in the range [4, 5000] we will have more integers with sflpf = 501 and are: 998, 1996, 2994, 3992, 4990.
+
+// We need a function sflpf_data() (javascript: sflpfData()that receives two arguments, val as the value of sflpf and nMax as a limit, and the function will output a sorted list of the numbers between 4 to nMax(included) that have the same value of sflpf equals to val.
+
+// Let's see some cases:
+
+// sflpf_data(10, 100) == [21, 25, 63]
+// /// the prime factorization of these numbers are:
+// Number  Prime Factorization     Sum First and Last Prime Factor
+// 21       = 3 . 7      ---->                 3 + 7 = 10
+// 25       = 5 . 5      ---->                 5 + 5 = 10
+// 63       = 3 . 3 . 7  ---->                 3 + 7 = 10
+// sflpf_data(10, 200) == [21, 25, 63, 105, 125, 147, 189]
+// sflpf_data(15, 150) == [26, 52, 78, 104, 130]
+// (Advice:Try to discard primes in a fast way to have a more agile code)
+
+function sflpfData(k, nMax){
+    //naive way :
+    //I gather a list of all prime factors for each number in [4...nMax]
+    //I keep those with max+min === k
+
+    let res = []
+    for(let i=4 ; i<=nMax ; i++){
+        let pFactors = primeFactors(i)
+        let minPFactor = pFactors.shift()
+        let maxPFactor = pFactors.pop()
+        if(minPFactor+maxPFactor === k){
+            res.push(i)
+        }
+    }
+
+    return res
+
+    function primeFactors(n) {
+        //returns the list of primes factors
+        // from : https://www.codewars.com/kata/542f3d5fd002f86efc00081a
+        if(n === 1) return []//edge case
+    
+        let primeFactors = []
+        let temp = 2
+    
+        while(temp<=n){
+            if(n%temp === 0){
+                primeFactors.push(temp)
+                n=n/temp
+            }else{
+                temp++
+            }
+        }
+    
+        return primeFactors
+    }
+    
+    // console.log(primeFactors(1)); // -> []
+    // console.log(primeFactors(2)); // -> [2]
+    // console.log(primeFactors(612)); // -> [2, 2, 3, 3, 17]
+}
+
+// console.log(sflpfData(10, 200)); // -> [21, 25, 63, 105, 125, 147, 189]
+// console.log(sflpfData(15, 150)); // -> [26, 52, 78, 104, 130]
+// console.log(sflpfData(501, 5000)); // -> [998, 1996, 2994, 3992, 4990]
+
+//It works, I was afraid it was rather long in terms of memory and time but in turned out fine. Solutions all use this approach.

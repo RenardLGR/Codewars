699b74b0839cd2a33bf9dca3ff60abc3ab553a9d
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-09 09:20:00 +0200

    daily push

--- Codewars week 31/main.js
+++ Codewars week 31/main.js
@@ -101,3 +101,27 @@ function findUsBis(n1, n2, k, factors, digits) {
 //This simple trick made the code so much faster and made it work !
 
 //=================================================================================
+// https://www.codewars.com/kata/54da539698b8a2ad76000228
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+function isValidWalk(walk) {
+    //Step 1 : for a walk to take 10 min, we need 10 directions
+    //Step 2 : for a walk to get back to starting point, we need equal amount of north and south so they cancel each other and equal amount of west and east so they cancel each other
+
+    if(walk.length === 10){
+        //step 1
+        let north = walk.filter(dir => dir === 'n').length
+        let south = walk.filter(dir => dir === 's').length
+        let west = walk.filter(dir => dir === 'w').length
+        let east = walk.filter(dir => dir === 'e').length
+        if(north === south && west === east){
+            //step 2
+            return true
+        }
+    }
+    return false
+}
+
+//====================================================================================

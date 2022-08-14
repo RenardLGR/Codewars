8b96767f1e72a3e59cd0ef3a851102419cec4000
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-15 01:19:31 +0200

    end of the week

--- Codewars week 31/main.js
+++ Codewars week 31/main.js
@@ -495,3 +495,39 @@ function sumDigNthTerm(initval, pattern, nthterm) {
 // console.log(sumDigNthTerm(10, [2, 1, 3], 157)) // -> 7
 
 //===================================================================================
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+function duplicateEncode(word){
+    word = word.toLowerCase() //ignore capitalization
+    return word.split('').map((letter, idx, arr) => {
+        if(idx === arr.indexOf(letter) && idx === arr.lastIndexOf(letter)){ //if the position is the same as the position of the first idx and the last indx, that letter is unique
+            return "("
+        }else{
+            return ")"
+        }
+    }).join('')
+}
+
+// console.log(duplicateEncode("Success"));
+// console.log(duplicateEncode("(( @"));
+
+function duplicateEncodeBis(word){
+    //a version a bit smarter
+    word = word.toLowerCase() //ignore capitalization
+    return word.split('').map((letter, idx, arr) => {
+        if(arr.indexOf(letter) === arr.lastIndexOf(letter)){ //if the position is the same as the position of the first idx and the last indx, that letter is unique
+            return "("
+        }else{
+            return ")"
+        }
+    }).join('')
+}

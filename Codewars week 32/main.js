f0daa978fdf32d36fd49db43ea91a31474299b31
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-16 21:06:01 +0200

    pushing

--- Codewars week 32/main.js
+++ Codewars week 32/main.js
@@ -6,3 +6,27 @@ const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 //=================================================================================
 
 //=================================================================================
+function fisrtRepeatedChar(s) {
+    //returns the indx of first char that have at least one duplicate
+    for(let i=0 ; i<s.length ; i++){
+        if(s.lastIndexOf(s[i]) !== i){
+            return i
+        }
+    }
+    return -1
+};
+
+function firstUniqChar(s){
+    //returns the idx of first char that is unique
+    for(let i=0 ;i<s.length ; i++){
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+            return i
+        }
+    }
+    return -1
+}
+
+// console.log(fisrtRepeatedChar('leetcode')) // -> 1 (for e)
+// console.log(firstUniqChar('leetcode')) // -> 0 (for l)
+
+//===============================================================================

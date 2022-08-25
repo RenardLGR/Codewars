f8d2a076492d7ad54227a9038e47e52356737911
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-25 03:00:23 +0200

    daily push

--- Codewars week 33/main.js
+++ Codewars week 33/main.js
@@ -183,3 +183,29 @@ function smallEnoughBis(a, limit) {
 }
 
 //============================================================================
+// https://www.codewars.com/kata/56c19316e8b139dff60006da/train/javascript
+// Write a function capitalize() which capitalizes the first letter (if any) of the given string. For example:
+
+// Input	Output
+// string	String
+// hello World	Hello World
+// i love codewars	I love codewars
+// This sentence is already capitalized	This sentence is already capitalized
+// 0123the first character of this sentence is not a letter	0123the first character of this sentence is not a letter
+// JavaScript / CoffeeScript: Extend the String prototype with a method capitalize() so you can call it on a string like so: "string".capitalize(). Learn about inheritance and the prototype chain.
+
+// Furthermore, the built-in string methods toUpperCase() and toLowerCase() are disabled for this Kata.
+
+String.prototype.capitalize = function() {
+    const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+    const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let sentence = this.slice(1)
+    let firstChar = alphaU[alphaL.indexOf(this.slice(0,1))]
+    if(firstChar){
+      return firstChar + sentence
+    }else{
+      return this.slice()
+    }
+}
+
+//==========================================================================

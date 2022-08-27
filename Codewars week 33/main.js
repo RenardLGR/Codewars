e6d82cb5780c7d90dda080644ee2ceb5ce9fc9eb
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-28 00:20:49 +0200

    daily push

--- Codewars week 33/main.js
+++ Codewars week 33/main.js
@@ -320,3 +320,60 @@ function toRoman(number){
 // console.log(toRoman(4));
 
 //===========================================================================
+// https://www.codewars.com/kata/58885a7bf06a3d466e0000e3
+// Task
+// Yesterday you found some shoes in your room. Each shoe is described by two values:
+
+// type indicates if it's a left or a right shoe;
+// size is the size of the shoe.
+// Your task is to check whether it is possible to pair the shoes you found in such a way that each pair consists of a right and a left shoe of an equal size.
+
+// Example
+// For:
+
+// shoes = [[0, 21], 
+//          [1, 23], 
+//          [1, 21], 
+//          [0, 23]]
+// the output should be true;
+
+// For:
+
+// shoes = [[0, 21], 
+//          [1, 23], 
+//          [1, 21], 
+//          [1, 23]]
+// the output should be false.
+
+// Input/Output
+// [input] 2D integer array shoes
+// Array of shoes. Each shoe is given in the format [type, size], where type is either 0 or 1 for left and right respectively, and size is a positive integer.
+
+// Constraints: 2 ≤ shoes.length ≤ 50,  1 ≤ shoes[i][1] ≤ 100.
+
+// [output] a boolean value
+
+// true if it is possible to pair the shoes, false otherwise.
+
+function pairOfShoes(shoes) {
+    //let's create an object of sizes containing sizes as key and an array of left right as value
+    //then check if the array of left right as value has same number of left and right shoe
+    let sizes = shoes.reduce((acc, shoe)=> {
+        acc[shoe[1]] = (acc[shoe[1]] || []).concat(shoe[0]) //apparently [].push(1) returns a size so I need concat
+        return acc
+    }, {})
+
+    let res = true
+    for(let size in sizes){
+        if(sizes[size].filter(s => s===0).length !== sizes[size].filter(s => s===1).length){
+            res = false
+        }
+    }
+
+    return res
+}
+
+
+// console.log(pairOfShoes([[0, 21], [1, 23], [1, 21], [0, 23]]));
+// console.log(pairOfShoes([[0, 23], [1, 23], [1, 23], [0, 23], [0, 23], [0, 23]]));
+

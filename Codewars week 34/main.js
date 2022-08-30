876292aecaf86ded28228bf2b66f53e7a7aa0cf3
Olivier Reynard <olivier.reynard17@gmail.com>
2022-08-30 17:53:24 +0200

    new week

---
+++ Codewars week 34/main.js
@@ -0,0 +1,5 @@
+const hi = 'HELLO'
+const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//=============================================================================

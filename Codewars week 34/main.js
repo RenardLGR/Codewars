aa07d1b2401834f61c2a30b69b5fd826b938d862
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-01 07:54:05 +0200

    daily push

--- Codewars week 34/main.js
+++ Codewars week 34/main.js
@@ -120,3 +120,20 @@ function ballTest(s, r) {
 // console.log(ballTest(1, "__")); //false
 
 //==================================================================================
+// https://www.codewars.com/kata/57f759bb664021a30300007d
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+    return x.split('').map(e => {
+        if(e==='a') return 'b'
+        if(e==='b') return 'a'
+        return e
+    }).join('')
+}
+
+//====================================================================================

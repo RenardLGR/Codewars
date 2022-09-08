cb385a48bf544d7b5bac748462b0b631c6f49ba1
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-08 16:41:49 +0200

    daily push

--- Codewars week 35/main.js
+++ Codewars week 35/main.js
@@ -87,3 +87,79 @@ function flip(d,a){
 
 
 //============================================================================
+// https://www.codewars.com/kata/55466989aeecab5aac00003e
+// The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+
+// SEE IMG
+
+// Can you translate this drawing into an algorithm?
+
+// You will be given two dimensions
+
+// a positive integer length
+// a positive integer width
+// You will return a collection or a string (depending on the language; Shell bash, PowerShell, Pascal and Fortran return a string) with the size of each of the squares.
+
+// Examples in general form:
+// (depending on the language)
+
+//   sqInRect(5, 3) should return [3, 2, 1, 1]
+//   sqInRect(3, 5) should return [3, 2, 1, 1]
+  
+//   You can see examples for your language in **"SAMPLE TESTS".**
+// Notes:
+// lng == wdth as a starting case would be an entirely different problem and the drawing is planned to be interpreted with lng != wdth. (See kata, Square into Squares. Protect trees! http://www.codewars.com/kata/54eb33e5bc1a25440d000891 for this problem).
+
+// When the initial parameters are so that lng == wdth, the solution [lng] would be the most obvious but not in the spirit of this kata so, in that case, return None/nil/null/Nothing or return {} with C++, Array() with Scala, [] with Perl, Raku.
+
+// In that case the returned structure of C will have its sz component equal to 0.
+
+// Return the string "nil" with Bash, PowerShell, Pascal and Fortran.
+
+function sqInRect(lng, wdth){
+    if(lng===wdth) return null //edge cause
+
+    //We can create a square using the min(l, w) giving us the first indx of our result and a new smaller rectangle of size (max(l, w)-min(l, w) ; min(l, w))
+    //And repeat the process until l or w is 0
+    //Recursively:
+    let res = []
+
+    return f(lng, wdth, res)
+
+    function f(l, w, arr){
+        let res = arr
+        if(l===0 || w===0){ //ending case
+            return arr
+        }else{
+            res.push(Math.min(l, w))
+            let max = Math.max(l, w)-Math.min(l, w)
+            let min = Math.min(l, w)
+            return f(max, min, res)
+        }
+    }
+}
+
+// console.log(sqInRect(5, 3));
+
+function sqInRectBis(length, width) {
+    if (length === width) {
+      return null;
+    }
+    
+    let result = [];
+    
+    while (length > 0 && width > 0) {
+      result.push(Math.min(length, width));
+      
+      if (length > width) {
+        length -= width;
+      } else {
+        width -= length;
+      }
+    }
+    
+    return result;
+  }
+
+  //========================================================================
+  

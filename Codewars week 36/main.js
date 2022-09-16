89b46a414984c888ee20d9bb22f5ccd05d75027a
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-16 11:00:33 +0200

    daily push

--- Codewars week 36/main.js
+++ Codewars week 36/main.js
@@ -154,3 +154,79 @@ function parseB(str){
 // console.log(parseB('iiisdoso'));
 
 //===============================================================================
+// https://www.codewars.com/kata/54dc6f5a224c26032800005c
+// A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+
+// In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+// For example an extract of a stocklist could be:
+
+// L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+// or
+// L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+// You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+// M = {"A", "B", "C", "W"} 
+// or
+// M = ["A", "B", "C", "W"] or ...
+// and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+// For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket/Prolog a list of pairs):
+
+// (A : 20) - (B : 114) - (C : 50) - (W : 0)
+// where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 114 the sum corresponding to "BKWRK" and "BTSQZ", 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+// If L or M are empty return string is "" (Clojure/Racket/Prolog should return an empty array/list instead).
+
+// Notes:
+// In the result codes and their values are in the same order as in M.
+// See "Samples Tests" for the return.
+
+function stockList(listOfArt, listOfCat){
+  if(listOfArt.length === 0 || listOfCat.length === 0) return '' //edge case
+
+  //first letter is a category
+  let res = listOfCat.reduce((acc, cur) => { //setting up our object {A:0, B:0, C:0, W:0}
+    acc[cur] = 0
+    return acc
+  }, {})
+
+  listOfArt.forEach(cur => {
+    if(listOfCat.includes(cur[0])){ //if the first letter is a category we are looking for
+      let nBooks = parseInt(cur.split(' ')[1])
+      res[cur[0]] += nBooks //adding to our object
+    }
+  })
+
+  let r = []
+  for(let cat in res){
+    r.push(`(${cat} : ${res[cat]})`)
+  }
+
+  return r.join(' - ')
+}
+
+// console.log(stockList(["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"], ["A", "B", "C", "W"]));
+
+
+function stockListB(listOfArt, listOfCat){
+  return listOfArt.reduce((acc, cur) => {
+    if(listOfCat.includes(cur[0])){ //if the first letter is a category we are looking for
+      let nBooks = parseInt(cur.split(' ')[1])
+      let idx = acc.findIndex(el => el[0]===cur[0])
+      if(idx === -1){
+        acc.push(cur[0]+ ' ' + nBooks)
+      }else{
+        let totaBooks = Number(acc[idx].split(' ')[1])
+        totaBooks += nBooks
+        acc[idx] = acc[idx].split(' ')[0] + ' ' + totaBooks
+      }
+    }
+    return acc
+  }, []).join(' - ') //acc is an array [['A 20'], [C 50], ...]
+}
+
+// console.log(stockListB(["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"], ["A", "B", "C", "W"]));
+//good try but doesn't work ! formatin wrong and if a category has no books, it won't appear
+
+//===============================================================================

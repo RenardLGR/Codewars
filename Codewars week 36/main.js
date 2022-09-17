b0d99c0b1f3590bcce3758a9e688797afb4454d2
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-17 04:03:34 +0200

    daily push

--- Codewars week 36/main.js
+++ Codewars week 36/main.js
@@ -230,3 +230,25 @@ function stockListB(listOfArt, listOfCat){
 //good try but doesn't work ! formatin wrong and if a category has no books, it won't appear
 
 //===============================================================================
+// https://www.codewars.com/kata/595bbea8a930ac0b91000130
+// You just got done with your set at the gym, and you are wondering how much weight you could lift if you did a single repetition. Thankfully, a few scholars have devised formulas for this purpose (from Wikipedia) :
+
+// SEE ON WEBSITE
+ 
+// Your function will receive a weight w and a number of repetitions r and must return your projected one repetition maximum. Since you are not sure which formula to use and you are feeling confident, your function will return the largest value from the three formulas shown above, rounded to the nearest integer. However, if the number of repetitions passed in is 1 (i.e., it is already a one rep max), your function must return w. Also, if the number of repetitions passed in is 0 (i.e., no repetitions were completed), your function must return 0.
+
+function calculate1RM(w, r){
+  if(r===0){
+    return 0
+  }else if(r===1){
+    return w
+  }else{
+    let epley = w*(1+r/30)
+    let mcglothin = 100*w/(101.3-2.67123*r)
+    let lombardi = w*Math.pow(r, 0.10)
+
+    return Math.round(Math.max(epley, mcglothin, lombardi))
+  }
+}
+
+//==============================================================

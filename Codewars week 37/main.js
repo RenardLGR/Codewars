855d24d01c7a13a1d2f6e684b856d89bdf4a1b36
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-20 13:00:58 +0200

    daily push

--- Codewars week 37/main.js
+++ Codewars week 37/main.js
@@ -30,3 +30,21 @@ function order(words){
 // console.log(order("is2 Thi1s T4est 3a"));
 
 //====================================================================
+// https://www.codewars.com/kata/57057a035eef1f7e790009ef/train/javascript
+// Chuck Norris is the world's toughest man - he once kicked a horse in the chin. Its descendants today are known as giraffes.
+
+// Like his punches, Chuck NEVER needs more than one line of code.
+
+// Your task, to please Chuck, is to create a function that chains 4 methods on a SINGLE LINE! You can pass with multiple lines, but CHuck will pity you. Go big or go home. ONE LINE!!
+
+// Chuck expects his list of favourite items to be split, sorted, joined AND have any occurrences of the letters 'e' and 'a' removed - why, you ask? Well Nunchuks hasn't got the letters 'a' or 'e' in it has it?? Chuck says shut your mouth... and don't forget the capitals.
+
+// If anyone dares to provide Chuck with an empty string, an integer or an array, just return a description of their face once Chuck finds out: 'Broken!'
+
+// Go, go go!!!
+
+//or if typeof !== string
+function onePunch(items){return (items === '' || typeof items === 'number' || typeof items === 'object') ? 'Broken!' : items.split(' ').sort((a,b) => a.localeCompare(b)).map(word => word.split('').filter(letter => letter.toLowerCase() !== 'a' && letter.toLowerCase() !== 'e').join('')).join(' ')} //Don't leave this line!!
+
+
+//======================================================================

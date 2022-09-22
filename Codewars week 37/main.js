cc94d4f9bd041bddab2b7efa88772582387c8272
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-22 06:40:26 +0200

    daily push

--- Codewars week 37/main.js
+++ Codewars week 37/main.js
@@ -68,3 +68,105 @@ function flyBy(lamps, drone){
 }
 
 //=========================================================================
+// https://www.codewars.com/kata/592993301fad491f26000072
+// Welcome to Canvas Fun ;-) In this series, we'll learning how to draw the image on a canvas.
+
+// Task
+// Given twoRectangles, your task is to draw them on the canvas. The color of rectangles should be "blue", and if two rectangles have a intersecting part, this part should be "black" color.
+
+// twoRectangles is given by a 3D integer array. looks like this:
+
+// [[[0,0],[70,70]],[[30,30],[99,99]]]
+
+// - [0,0] and [70,70] are the x,y coordinates of 
+//   two diagonal points of the rectangle1.
+
+// - [30,30] and [99,99] are the x,y coordinates of 
+//   two diagonal points of the rectangle2.
+// It's guaranteed that the two points are in the diagonal position(no same x coordinates and y coordinates), but it's NOT guarantee that it's always be the main diagonal(may be the vice diagonal) and their order is also random. ;-)
+
+// The basic canvas(always 100 x 100 pixel),background color(white) are already difined in the initial code(please don't delete or modify them).
+
+//Point [0,0] is top left
+//Pont [99,99] is bottom right
+
+// You just need to draw the rectangles, use the following two methods:
+
+// fillStyle(colorCode)
+// Set the color of fill action. colorCode can be a Hex string, such as "#ffffff","#000000"; Or a word of color, such as "white","black","gray".
+
+// In this kata, we using "blue" as the color of rectangles, and "black" as the color of intersecting part.
+
+// fillRect(x,y,width,height)
+// Coordinate x,y as the top-left coner of rectangle, drawing a rectangle of size width x height.
+
+// Example
+// For twoRectangles = [[[0,0],[70,70]],[[30,30],[99,99]]], the output image should be:
+
+
+// Intersecting part should be "black", other parts should be "blue".
+
+// For twoRectangles = [[[10,10],[90,90]],[[40,40],[60,60]]], the output image should be:
+
+
+// Rectangle2 inside rectangle1.
+
+// For twoRectangles = [[[10,10],[30,30]],[[60,60],[80,80]]], the output image should be:
+
+
+// There is no intersecting part.
+
+function draw(twoRectangles) {
+    var canvas = new Canvas(100,100)  //Create a 100 x 100 canvas
+    var ctx = canvas.getContext('2d'); 
+    ctx.fillStyle="#ffffff"
+    ctx.fillRect(0,0,100,100)  //Draw background
+    //Don't delete or modify the code above
+    
+    //Your code:
+    ctx.fillStyle="blue" //color of choice is blue
+    // fillRect(x,y,width,height)
+    ctx.fillRect(twoRectangles[0][0][0],twoRectangles[0][0][1],twoRectangles[0][1][0]-twoRectangles[0][0][0],twoRectangles[0][1][1]-twoRectangles[0][1][0]) //draws first rectangle
+    ctx.fillRect(twoRectangles[1][0][0],twoRectangles[1][0][1],twoRectangles[1][1][0]-twoRectangles[1][0][0],twoRectangles[1][1][1]-twoRectangles[1][1][0]) //draws second rectangle
+
+
+    ctx.fillStyle="black" //color of choice is black
+    //dx1 est la coordonnée en x du départ du premier rectangle
+    //ax1 est la coordonnée en x de l'arrivée du premier rectangle
+    //Il existera de même dy1 et ay1 pour les coordonnée en y de départ et d'arrivée du premuier rectangle
+    //Il existera de même dx2, ax2, dy2 et ay2
+    let dx1 = twoRectangles[0][0][0]
+    let ax1 = twoRectangles[0][1][0]
+    let dy1 = twoRectangles[0][0][1]
+    let ay1 = twoRectangles[0][1][1]
+    
+    let dx2 = twoRectangles[1][0][0]
+    let ax2 = twoRectangles[1][1][0]
+    let dy2 = twoRectangles[1][0][1]
+    let ay2 = twoRectangles[1][1][1]
+    
+    if(dx1 <= dx2 && dx2 <= ax1 && dy1 <=dy2 && dy2 <= ay1){ //cas chevauchement 2 chevauche 1
+      ctx.fillRect(dx2,dy2,ax1-dx2,ay1-dy2) //draws black rectangle
+    }
+    
+    if(dx2 <= dx1 && dx1 <= ax2 && dy2 <= dy1 && dy1 <= ay2){ //cas chevauchement 1 chevauche 2
+       ctx.fillRect(dx1,dy1,ax2-dx1,ay2-dy1) //draws black rectangle
+    }
+    
+    if(dx2 <= dx1 && dx1 <= ax2 && dx2 <= ax1 && ax1 <= ax2 && dy2 <= dy1 && dy1 <= ay2 && dy2 <= ay1 && ay1 <= ay2){ //cas inclusion 1 inclus dans 2
+      ctx.fillRect(dx1, dy1, ax1-dx1, ay1-dy1)
+    }
+    
+      if(dx1 <= dx2 && dx2 <= ax1 && dx1 <= ax2 && ax2 <= ax1 && dy1 <= dy2 && dy2 <= ay1 && dy1 <= ay2 && ay2 <= ay1){ //cas inclusion 2 inclus dans 1
+      ctx.fillRect(dx2, dy2, ax2-dx2, ay2-dy2)
+    }
+    
+    
+    //Don't delete or modify the following code
+    return canvas.toDataURL() //Returns the image data
+  }
+
+  // I give up
+
+  //==================================================================
+  

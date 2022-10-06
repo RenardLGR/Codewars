32ac4404789b58c85339ccdbf6bc56747561bceb
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-06 21:13:35 +0200

    daily push

--- Codewars week 39/main.js
+++ Codewars week 39/main.js
@@ -92,4 +92,33 @@ function isSumOfCubes(s){
 }
 
 
-console.log(isSumOfCubes("371407298a --- dreary, ###100.153 I thought, 9926315strong -127&() 1")); // -> "371 407 153 1 932 Lucky"
+// console.log(isSumOfCubes("371407298a --- dreary, ###100.153 I thought, 9926315strong -127&() 1")); // -> "371 407 153 1 932 Lucky"
+
+//=============================================================
+// https://www.codewars.com/kata/5727bb0fe81185ae62000ae3
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+function cleanString(s) {
+    return s.split('').reduce((acc, cur) => {
+        if(cur === '#'){
+            acc.pop()
+        }else{
+            acc.push(cur)
+        }
+        return acc
+    }, []).join('')
+}
+
+// console.log(cleanString("abc#d##c"));
+// console.log(cleanString("abc##d######"));
+
+//=======================================================================

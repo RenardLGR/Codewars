88ed7b1f76db0cd2acf3f90b2470bb4d7a71a7bf
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-11 05:50:37 +0200

    daily push

---
+++ Codewars week 40/main.js
@@ -0,0 +1,23 @@
+const hi = 'HELLO'
+const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//==========================================================
+// https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+// Alphabetically ordered
+
+// Case-sensitive!
+
+// Not prime numbers
+
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)), 2)
+}
+
+//============================================================

900073aba10480eb4379ff080309cccbdba23b58
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-12 00:34:44 +0200

    daily push

--- Codewars week 40/main.js
+++ Codewars week 40/main.js
@@ -91,3 +91,28 @@ function notPrimes(a,b){
 //It works and somehow is not that long to execute
 
 //==================================================================
+// https://www.codewars.com/kata/5a34b80155519e1a00000009
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+// Some cases:
+// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+// [68, -1, 1, -7, 10, 10] => [-1, 10]
+
+// [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+
+
+function multipleOfIndex(array) {
+    return array.reduce((acc, cur, idx) =>{
+        if(cur%idx === 0){
+            acc.push(cur)
+        }
+        return acc
+    } ,[])
+}
+
+function multipleOfIndexB(array) {
+    return array.filter((num, i) => num % i === 0);
+}
+
+//=====================================================================

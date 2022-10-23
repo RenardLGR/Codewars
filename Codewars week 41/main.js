f2254c8693b48928fe5076637587c29b4f6ab8af
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-23 04:31:55 +0200

    daily push

--- Codewars week 41/main.js
+++ Codewars week 41/main.js
@@ -510,3 +510,55 @@ const maxLenZeroBis = (input) => {
 //Solution with the restriction would need regex
 
 //================================================================
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+// Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+// array = [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+// For better understanding, please follow the numbers of the next array consecutively:
+
+// array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+// snail(array) #=> [1,2,3,4,5,6,7,8,9]
+// This image will illustrate things more clearly:
+
+// SEE IMG
+// NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+// NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+function snail(array){
+    let res = []
+
+    while(array.length){
+        // add the 0th array to the result and removes it from the input
+        res.push(array.shift())
+
+        //add the last item on the right to the result and removes them from the input
+        if(array.length){  
+            let lastRightElem = array.map(arr => arr.pop())
+            res.push(lastRightElem)
+        }
+
+        // add the last array to the result and removes it from the input
+        if(array.length){
+            res.push(array.pop().reverse())
+        }
+
+        //add the 0th item on the left to the result and removes them from the input
+        if(array.length){
+            let firstItemLeft = array.map(arr => arr.shift()).reverse()
+            res.push(firstItemLeft)
+        }
+    }
+
+
+    return res.flat()
+}
+
+// console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+
+//=====================================================

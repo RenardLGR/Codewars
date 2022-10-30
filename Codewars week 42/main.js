aacac18dbbfcedc55643f3a9f8433a74257e293e
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-30 03:15:38 +0100

    daily push

--- Codewars week 42/main.js
+++ Codewars week 42/main.js
@@ -562,4 +562,48 @@ function validParentheses(parens) {
     return parensCpy.length === 0
 }
 
+function validParenthesesBis(parens){
+    let n = 0;
+    for (let i = 0; i < parens.length; i++) {
+        if (parens[i] == '(') n++;
+        if (parens[i] == ')') n--;
+        if (n < 0) return false;
+    }
+    
+    return n === 0;
+}
+
 //===========================================================================
+// https://www.codewars.com/kata/5a58ca28e626c55ae000018a/train/javascript
+// Write the following function:
+
+// function areaOfPolygonInsideCircle(circleRadius, numberOfSides)
+// It should calculate the area of a regular polygon of numberOfSides sides inside a circle of radius circleRadius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle). The answer should be a number rounded to 3 decimal places.
+
+// Input :: Output Examples
+
+// areaOfPolygonInsideCircle(3, 3) // returns 11.691
+
+// areaOfPolygonInsideCircle(5.8, 7) // returns 92.053
+
+// areaOfPolygonInsideCircle(4, 5) // returns 38.042
+// Note: if you need to use Pi in your code, use the native value of your language unless stated otherwise.
+
+function areaOfPolygonInsideCircle(circleRadius, numberOfSides) {
+    // https://www.editions-petiteelisabeth.fr/calculs_perimetre_aire_polygone_regulier_2.php
+    // https://www.accromaths.fr/comment-calculer-laire-dun-polygone/
+
+    let n = numberOfSides
+    let r = circleRadius
+
+    let res = n*r*r/2 * Math.sin(2*Math.PI/n)
+    return Number(res.toFixed(3))
+}
+
+// console.log(areaOfPolygonInsideCircle(3, 3)) // returns 11.691
+
+// console.log(areaOfPolygonInsideCircle(5.8, 7)) // returns 92.053
+
+// console.log(areaOfPolygonInsideCircle(4, 5)) // returns 38.042
+
+//======================================================================

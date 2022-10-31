d134aea2725874dc51f4f8a83bc8155df2eb0b03
Olivier Reynard <olivier.reynard17@gmail.com>
2022-10-31 04:50:44 +0100

    daily push

--- Codewars week 42/main.js
+++ Codewars week 42/main.js
@@ -607,3 +607,122 @@ function areaOfPolygonInsideCircle(circleRadius, numberOfSides) {
 // console.log(areaOfPolygonInsideCircle(4, 5)) // returns 38.042
 
 //======================================================================
+// Write a function that given a size, return every combinations of bits of that size
+function everyCombination(size){
+    let combinations = []
+
+    createSequences(size, [])
+
+    function createSequences(size, inProgress){
+        if(size===0){
+            combinations.push(inProgress)
+        }else{
+            createSequences(size-1, [...inProgress, 0])
+            createSequences(size-1, [...inProgress, 1])
+        }
+    }
+
+    return combinations
+}
+// console.log(everyCombination(3));
+// console.log(everyCombination(5));
+
+//======================================================================
+// https://www.codewars.com/kata/628df6b29070907ecb3c2d83
+// A runner, who runs with base speed s with duration t will cover a distances d: d = s * t
+// However, this runner can sprint for one unit of time with double speed s * 2
+// After sprinting, base speed s will permanently reduced by 1, and for next one unit of time runner will enter recovery phase and can't sprint again.
+
+// Your task, given base speed s and time t, is to find the maximum possible distance d.
+
+// Input:
+// 1 <= s < 1000
+// 1 <= t < 1000
+
+// Example:
+// Given s = 2 and t = 4.
+// We could schedule when runner should sprint so we could get these possible sequences:
+
+// Seq.: RRRR
+// => s + s + s + s
+// => 2 + 2 + 2 + 2 = 8
+// Seq.: RRRS
+// => s + s + s + s*2
+// => 2 + 2 + 2 + 2*2 = 10
+// Seq.: RRSR
+// => s + s + s*2 + (s-1)
+// => 2 + 2 + 2*2 + (2-1) = 9
+// Seq.: RSRR
+// => s + s*2 + (s-1) + (s-1)
+// => 2 + 2*2 + (2-1) + (2-1) = 8
+// Seq.: RSRS
+// => s + s*2 + (s-1) + (s-1)*2
+// => 2 + 2*2 + (2-1) + (2-1)*2 = 9
+// Seq.: SRRR
+// => s*2 + (s-1) + (s-1) + (s-1)
+// => 2*2 + (2-1) + (2-1) + (2-1) = 7
+// Seq.: SRRS
+// => s*2 + (s-1) + (s-1) + (s-1)*2
+// => 2*2 + (2-1) + (2-1) + (2-1)*2 = 8
+// Seq.: SRSR
+// => s*2 + (s-1) + (s-1)*2 + (s-1-1)
+// => 2*2 + (2-1) + (2-1)*2 + (2-1-1) = 7
+
+// Where:
+// - R: Normal Run / Recovery
+// - S: Sprint
+// Based on above sequences, the maximum possible distance d is 10.
+
+function runSchedule(speed, duration){
+
+    let maxSequence = []
+    let maxD = 0
+
+
+    createSequence(duration, speed, [])
+    // console.log(maxSequence, maxD);
+    return maxD
+
+    //Helper functions
+
+    //Create a sequence of R and S folowing the rules
+    function createSequence(duration, speed, inProgress){
+        if(duration===0){
+            let d = distanceCovered(inProgress, speed)
+            if(d>maxD){
+                maxD = d
+                maxSequence = inProgress.slice()
+            }
+        }else{
+            if(inProgress.slice(-1)==='S'){
+                createSequence(duration-1, speed, [...inProgress, 'R'])
+            }else{
+                createSequence(duration-1, speed, [...inProgress, 'R'])
+                createSequence(duration-1, speed, [...inProgress, 'S'])
+            }
+        }
+    }
+
+    //Given a sequence of run/sprint and a speed returns the total distance covered
+    function distanceCovered(sequence, speed){
+        let d=0
+        let s=speed
+        sequence.forEach(e => {
+            if(e==='R'){
+                d+=s
+            }else{
+                d+=2*s
+                s--
+            }
+        })
+
+        return d
+    }
+    //console.log(distanceCovered(['S', 'R', 'R', 'S'], 2)) // =>8
+
+}
+
+// console.log(runSchedule(2,4)) // => 10, sequence [ 'R', 'R', 'R', 'S' ]
+//Probably works but takes too much time for long distance input (as expected)
+
+//===================================================================

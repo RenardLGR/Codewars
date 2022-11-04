f2c831dd218eb36bb218b0b3e1b3e8cf532ad07c
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-04 15:22:43 +0100

    daily push

--- Codewars week 43/main.js
+++ Codewars week 43/main.js
@@ -380,3 +380,36 @@ function comp(array1, array2) {
 // console.log(comp([10, 2, 7, 7, 10, 4, 4, 1, 2, 0, 8, 5, 0, 7, 7, 1, 0, 1, 3, 7] , [49, 25, 4, 100, 1, 0, 0, 49, 16, 4, 9, 1, 1, 49, 64, 100, 1, 49, 49, 16]));
 
 //=================================================================
+// https://www.codewars.com/kata/5601c5f6ba804403c7000004
+// The medians of a triangle are the segments that unit the vertices with the midpoint of their opposite sides. The three medians of a triangle intersect at the same point, called the barycenter or the centroid. Given a triangle, defined by the cartesian coordinates of its vertices we need to localize its barycenter or centroid.
+
+// The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices A, B and C  as three different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]
+
+// This is how our asked function should work: the result of the coordinates should be expressed up to four decimals, (rounded result).
+
+// You know that the coordinates of the barycenter are given by the following formulas.
+
+// x0 = (xA + xB + xC)/3 y0 = (yA + yB + yC)/3
+
+// For additional information about this important point of a triangle see at: (https://en.wikipedia.org/wiki/Centroid)
+
+// Let's see some cases:
+
+// barTriang([4, 6], [12, 4], [10, 10]) ------> [8.6667, 6.6667]
+
+// barTriang([4, 2], [12, 2], [6, 10]) ------> [7.3333, 4.6667]
+// The given points form a real or a degenerate triangle but in each case the above formulas can be used.
+
+function barTriang(p1, p2, p3){
+    let x0 = (p1[0] + p2[0] + p3[0])/3
+    let y0 = (p1[1] + p2[1] + p3[1])/3
+
+    x0=Number(x0.toFixed(4))
+    y0=Number(y0.toFixed(4))
+
+    return [x0, y0]
+}
+
+// console.log(barTriang([4, 2], [12, 2], [6, 10]));
+
+//=====================================================================

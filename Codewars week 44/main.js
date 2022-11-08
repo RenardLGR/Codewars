524716cd2048b848859e88704fe39941f071b281
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-08 21:46:28 +0100

    dail push

--- Codewars week 44/main.js
+++ Codewars week 44/main.js
@@ -168,3 +168,35 @@ function findMaxProduct(arr){
 // console.log(findMaxProduct([4, 0, -19]));
 
 //==============================================================
+// https://www.codewars.com/kata/595aa94353e43a8746000120/train/javascript
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+function findDeletedNumber(arr, mixArr) {
+    let deletedNumber
+    for(let i=0 ; i<arr.length ; i++){
+        if(!mixArr.includes(arr[i])){
+            deletedNumber = arr[i]
+        }
+    }
+
+    if(deletedNumber===undefined){
+        return 0
+    }else{
+        return deletedNumber
+    }
+}
+
+//===================================================================
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.

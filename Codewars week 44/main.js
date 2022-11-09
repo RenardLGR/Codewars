86ef5c2aadbf64091d42be00fee7d6478a3174b4
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-09 09:30:10 +0100

    daily push

--- Codewars week 44/main.js
+++ Codewars week 44/main.js
@@ -195,8 +195,64 @@ function findDeletedNumber(arr, mixArr) {
     }
 }
 
+function findDeletedNumberBis(arr, mixArr){
+    if(arr.length === mixArr.length){
+        return 0
+    }else{
+        return arr.filter(n => !mixArr.includes(n))[0]
+    }
+}
+
 //===================================================================
 // https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 // Simple, given a string of words, return the length of the shortest word(s).
 
 // String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    return Math.min(...s.split(' ').map(word => word.length))
+}
+
+//======================================================================
+// https://www.codewars.com/kata/514b92a657cdc65150000006
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// Courtesy of projecteuler.net (Problem 1)
+// https://projecteuler.net/problem=1
+
+function multiple3or5(number){
+    if(number<=0){ //edge case
+        return 0
+    }else{
+        let multiples = []
+        for(let i=1 ; i<number ; i++){
+            if(i%3===0){
+                multiples.push(i)
+            }else{//Another if() would count 15 two times, we want an else
+                if(i%5===0){
+                    multiples.push(i)
+                }
+            }
+        }
+
+        return multiples.reduce((acc, cur) => acc+cur, 0)
+    }
+}
+//What a weird solve...
+
+function multiple3or5Bis(number){
+    let sum = 0
+    for(let i=1 ; i<number ; i++){
+        if(i%3===0 || i%5===0){
+            sum+=i
+        }
+    }
+
+    return sum
+}
+
+//=================================================================

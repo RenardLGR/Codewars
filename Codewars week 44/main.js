b92eff18fc5def28926ba9e24ce4b717bb60d8e1
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-11 13:51:13 +0100

    daily push

--- Codewars week 44/main.js
+++ Codewars week 44/main.js
@@ -288,3 +288,67 @@ function countBits(n){
     return toBits.split('').filter(b => b==='1').length
     //.split('0').length saves us a filter
 }
+
+//======================================================================
+// https://www.codewars.com/kata/556deca17c58da83c00002db
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+// But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+// If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+//https://www.codewars.com/kata/fibonacci-tribonacci-and-friends
+
+// [Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+
+function tribonacci(signature, n){
+    if(n===0){//edge (actually res.slice(0, 0) would still return [])
+        return []
+    }
+
+    let res = signature.slice()
+    while(res.length < n){
+        res.push(res.slice(-3).reduce((acc, cur) => acc+cur, 0))
+    }
+    
+    return res.slice(0, n)
+}
+
+// console.log(tribonacci([1,2,3], 10)); // -> [1,2,3,6,11,20,37,68,125,230]
+
+//===================================================================
+// https://www.codewars.com/kata/fibonacci-tribonacci-and-friends
+// If you have completed the Tribonacci sequence kata, you would know by now that mister Fibonacci has at least a bigger brother. If not, give it a quick look to get how things work.
+//https://www.codewars.com/kata/tribonacci-sequence
+
+// Well, time to expand the family a little more: think of a Quadribonacci starting with a signature of 4 elements and each following element is the sum of the 4 previous, a Pentabonacci (well Cinquebonacci would probably sound a bit more italian, but it would also sound really awful) with a signature of 5 elements and each following element is the sum of the 5 previous, and so on.
+
+// Well, guess what? You have to build a Xbonacci function that takes a signature of X elements - and remember each next element is the sum of the last X elements - and returns the first n elements of the so seeded sequence.
+
+// xbonacci {1,1,1,1} 10 = {1,1,1,1,4,7,13,25,49,94}
+// xbonacci {0,0,0,0,1} 10 = {0,0,0,0,1,1,2,4,8,16}
+// xbonacci {1,0,0,0,0,0,1} 10 = {1,0,0,0,0,0,1,2,3,6}
+// xbonacci {1,1} produces the Fibonacci sequence
+
+function Xbonacci(signature, n){
+    let res = signature.slice()
+    let len = signature.length
+    while(res.length < n){
+        res.push(res.slice(-len).reduce((a, c) => a+c, 0))
+    }
+
+    return res.slice(0, n)
+}
+
+//console.log(Xbonacci([1,0,0,0,0,0,0,0,0,0], 20)) // -> [1,0,0,0,0,0,0,0,0,0,1,1,2,4,8,16,32,64,128,256]
+
+//=====================================================================

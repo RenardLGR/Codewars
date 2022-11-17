c61a55b05bf49b247d46926093643b72328aba21
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-17 12:02:51 +0100

    pushing

--- Codewars week 45/main.js
+++ Codewars week 45/main.js
@@ -450,3 +450,23 @@ function rowSumOddNumbersBis(n){
 }
 
 //=========================================================================
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+    let words = str.split(' ')
+    let pigs = words.map(w => {
+        if(w[0].match(/[a-z]/gi)){ //check if it is a word, i.e has alphabetical character, case insensitive
+            return w.slice(1)+w[0]+'ay'
+        }else{
+            return w
+        }
+    })
+    return pigs.join(' ')
+}
+
+//===============================================================================

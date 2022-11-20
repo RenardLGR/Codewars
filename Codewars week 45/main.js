7cb056fa289a981f54efae7841f4f600f30e0449
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-20 17:17:54 +0100

    daily push

--- Codewars week 45/main.js
+++ Codewars week 45/main.js
@@ -736,3 +736,57 @@ function everyCombination(size){
 // console.log(everyCombination(4));
 
 //===========================================================================
+// https://www.codewars.com/kata/513e08acc600c94f01000001
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+function rgb(r, g, b){
+    let rgb = [r, g, b]
+    let sanitizedRgb = rgb.map(color => { //sanitizing
+        if(color<0){
+            return 0
+        }else if(color>255){
+            return 255
+        }else{
+            return color
+        }
+    })
+
+    let hexa = sanitizedRgb.map(color => color.toString(16).toUpperCase()) //converting to hexa
+    hexa = hexa.map(c => c.length===1 ? '0'+c : c) //adding leading zeroes
+
+    return hexa.join('')
+}
+
+
+function rgbBis(r, g, b){
+    return toHex(r)+toHex(g)+toHex(b);
+
+    //Helper
+    function toHex(d) {
+        if(d < 0 ) {return "00"}
+        if(d > 255 ) {return "FF"}
+        return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+    }
+}
+
+//====================================================================
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+function invertArr(array) {
+    return array.map(e => -e)
+}

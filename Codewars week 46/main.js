a41b13a3cc9f4fd7bee1beef81ab7ca5fda3d534
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-23 15:00:04 +0100

    daily push

--- Codewars week 46/main.js
+++ Codewars week 46/main.js
@@ -320,3 +320,55 @@ function dirReducBis(arr){
 // console.log(dirReducBis(["NORTH", "WEST", "SOUTH", "EAST"] )); // -> ["NORTH", "WEST", "SOUTH", "EAST"]
 
 //==================================================================
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+    return x.reduce((a, c) => a*c, 1)
+}
+
+//====================================================================
+// https://www.codewars.com/kata/583203e6eb35d7980400002a
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Example
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+// Note
+// In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+
+function countSmileys(arr) {
+    let regex = new RegExp(/([:;]+[-~]?[)D]+)+/)
+    return arr.filter(s => s.match(regex)).length
+}
+
+function countSmileysBis(arr) {
+    return arr.filter(s => /^[:;][-~]?[)D]$/.test(s)).length;
+}
+
+function countSmileysTer(arr) {
+    let smileys = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+    let count = 0;
+    
+    for (let i=0; i<arr.length; i++){
+        if(smileys.includes(arr[i])) count++
+    }
+    return count;
+}
+
+//======================================================================
+
--- Codewars week 46/sudoku.js
+++ Codewars week 46/sudoku.js
@@ -30,6 +30,7 @@
 
 
 // Explanation :
+//Algo 1 : Finding the number with the help of the surroundings
 //                  We know the 1 will be in this col
 // var puzzle = [                 |
 //             [0,0,0,| 0,0,1,| 0,0,0],
@@ -37,16 +38,35 @@
 //             [0,0,1,| 0,0,0,| 0,0,0],
 //            _________________________
 //             [0,0,0,| 0,0,0,| 0,X,0], --We know the 1 will be in this line
-//             [0,0,0,| 0,1,0,| 0,0,0],
-//             [0,1,0,| 0,0,0,| 0,0,0],
+//             [0,0,0,| 0,1,0,| 0,0,0], --Can't be on this line
+//             [0,1,0,| 0,0,0,| 0,0,0], --Can't be on this line
 //            _________________________
 //             [0,6,0,| 0,0,0,| 0,0,0],
 //             [0,0,0,| 0,0,0,| 0,0,0],
 //             [0,0,0,| 0,0,0,| 1,0,0]];
+//                              |   |
+//                          Can't be on these cols
 
 //So if I know a number is both missing from this line and this col, I can add it here.
 //We also need to make sure the 1 is not yet represented in the square
 //For each zeroes, I check for each k from 1 to 9 if there is k known in its surrounding (col 0-2, 3-5, 6-8 and line 0-2, 3-5, 6-8) but not on the same line/col
+
+//Algo 2 : Finding the number by elimination
+//                        We know it can't be 8, 9 from the col
+// var puzzle = [                   |
+//             [0,0,0,| 0,0,0,| 0,0,0],
+//             [0,0,0,| 0,0,0,| 0,0,0],
+//             [0,0,0,| 0,0,0,| 0,0,0],
+//            _________________________
+//             [0,0,0,| 0,0,0,| 1,2,0], -- We know it can't be 1, 2, 3 from the square
+//             [0,0,0,| 0,0,0,| 0,3,0],
+//             [0,0,0,| 4,5,6,| 0,0,X], -- We know it can't be 4, 5, 6 from the line : It must be a 7
+//            _________________________
+//             [0,6,0,| 0,0,0,| 0,0,0],
+//             [0,0,0,| 0,0,0,| 0,0,8],
+//             [0,0,0,| 0,0,0,| 0,0,9]];
+
+//So if there is one and only one number between 1 and 9 that is in neither of thoses lists, I can add it here
 
 function sudoku(puzzle) {
     let cpy=puzzle.slice()
@@ -58,10 +78,11 @@ function sudoku(puzzle) {
     while(!isDone && it>0){
         isDone = true
         it--
-        for(let line=0 ; line<9 ; line++){
-            for(let index=0 ; index<9 ; index++){
-                if(puzzle[line][index]===0){//if it is a zero, try to find the number //if a zero is found, the algo is not done
-                    findNumber(line, index)
+        for(let line=0 ; line<9 ; line++){ //goes line by line
+            for(let index=0 ; index<9 ; index++){ //goes index by index so cols by cols
+                if(cpy[line][index]===0){//if it is a zero, try to find the number //if a zero is found, the algo is not done
+                    findNumberWithSurroundings(line, index)
+                    findNumberByElimination(line, index)
                     isDone = false
                 }
             }
@@ -71,23 +92,46 @@ function sudoku(puzzle) {
     return cpy
 
 
-    //helper
-    function findNumber(coordLine, coordIndex){
+    //helpers
+    //Algo 1 defined in explanation
+    function findNumberWithSurroundings(coordLine, coordIndex){
+        //TODO : idea behind this algo is prbly wrong
         let arrLine = generateLine(coordLine)
         let arrCol = generateCol(coordIndex)
+        let arrSquare = generateSquare(coordLine, coordIndex)
         for(let k=1 ; k<=9 ; k++){ //k is a number between 1 and 9 that will be placed if all requirements are met
-            if(!arrLine.includes(k) && !arrCol.includes(k)){//if k is missing from both the col and the line
+            if(!arrLine.includes(k) && !arrCol.includes(k) && !arrSquare.includes(k)){//if k is missing from the col, the line and the square
                 let surroundingLines = giveSurroundingLines(coordLine)
                 let surroundingCols = giveSurroundingCols(coordIndex)
                 let surroundingLine1 = generateLine(surroundingLines[0])
                 let surroundingLine2 = generateLine(surroundingLines[1])
                 let surroundingCol1 = generateCol(surroundingCols[0])
                 let surroundingCol2 = generateCol(surroundingCols[1])
-                let square = generateSquare(coordLine, coordIndex)
-                if(surroundingLine1.includes(k) && surroundingLine2.includes(k) && surroundingCol1.includes(k) && surroundingCol2.includes(k) && !square.includes(k)){ //if every surrounding lines and cols have this k and the square itself has not, the zero must be replaced by k
+                if(surroundingLine1.includes(k) && surroundingLine2.includes(k) && surroundingCol1.includes(k) && surroundingCol2.includes(k)){ //if every surrounding lines and cols have this k, the zero must be replaced by k
                     cpy[coordLine][coordIndex] = k
                 }
             }
+        }
+    }
+
+    //Algo 2 defined in explanation
+    function findNumberByElimination(coordLine, coordIndex){
+        let arrLine = generateLine(coordLine)
+        // console.log("🚀 ~ file: sudoku.js ~ line 117 ~ findNumberByElimination ~ arrLine", arrLine)
+        let arrCol = generateCol(coordIndex)
+        // console.log("🚀 ~ file: sudoku.js ~ line 118 ~ findNumberByElimination ~ arrCol", arrCol)
+        let arrSquare = generateSquare(coordLine, coordIndex)
+        // console.log("🚀 ~ file: sudoku.js ~ line 119 ~ findNumberByElimination ~ arrSquare", arrSquare)
+        let res = []
+        for(let k=1 ; k<=9 ; k++){ //k is a number between 1 and 9 that will be placed if all requirements are met
+            // console.log(!arrLine.includes(k) && !arrCol.includes(k) && !square.includes(k))
+            if(!arrLine.includes(k) && !arrCol.includes(k) && !arrSquare.includes(k)){
+                // console.log('Here')
+                res.push(k)
+            }
+        }
+        if(res.length === 1){
+            cpy[coordLine][coordIndex] = res[0]
         }
     }
 
@@ -128,9 +172,15 @@ function sudoku(puzzle) {
         }
         return res
     }
+    // console.log(giveSurroundingLines(0)) //[1, 2]
     // console.log(giveSurroundingLines(1)) //[0, 2]
+    // console.log(giveSurroundingLines(2)) //[0, 1]
+    // console.log(giveSurroundingLines(3)) //[4, 5]
+    // console.log(giveSurroundingLines(4)) //[3, 5]
     // console.log(giveSurroundingLines(5)) //[3, 4]
     // console.log(giveSurroundingLines(6)) //[7, 8]
+    // console.log(giveSurroundingLines(7)) //[6, 8]
+    // console.log(giveSurroundingLines(8)) //[6, 7]
     //given a coord, returns the coord of its surrounding, example : coordCol == 6 returns [7, 8]
     function giveSurroundingCols(coordCol){
         let res = []
@@ -156,4 +206,23 @@ let puzzle1 = [
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,8,0,0,7,9]];
 
+let puzzle2 = [
+    [5, 3, 0, 0, 7, 8, 4, 9, 2],
+    [6, 4, 2, 1, 9, 5, 3, 0, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 1, 5, 7, 6, 4, 0, 2, 3],
+    [4, 2, 6, 8, 5, 3, 9, 0, 1],
+    [7, 0, 3, 9, 2, 1, 8, 4, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 5, 4, 6, 8, 0, 1, 7, 9]]
+
 console.log(sudoku(puzzle1));
+// console.log(sudoku(puzzle2));
+
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8]
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8]
+let arr3 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// console.log(!arr1.includes(9) && !arr2.includes(9) && !arr3.includes(9))

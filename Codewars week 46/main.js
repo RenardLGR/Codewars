3f8751ffbba83ac12801983720ace6d2d310d816
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-25 05:24:37 +0100

    daily push

--- Codewars week 46/main.js
+++ Codewars week 46/main.js
@@ -421,3 +421,82 @@ function isPrime(num) {
 
 
 //=======================================================================================
+// https://www.codewars.com/kata/563e320cee5dddcf77000158
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((acc, cur) => acc+cur, 0)/marks.length)
+}
+
+//======================================================================================
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc, cur) => acc+cur, 0)
+}
+
+function arrayPlusArrayBis(arr1, arr2) {
+    let arr = [...arr1, ...arr2];
+    return arr.reduce((acc, cur) => acc+cur, 0);
+}
+
+//=========================================================================================
+// https://www.codewars.com/kata/56a5d994ac971f1ac500003e
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+// Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+// Note
+// consecutive strings : follow one after another without an interruption
+
+function longestConsec(strarr, k) {
+    let n = strarr.length
+    if(n === 0 || k > n || k<=0){ //edge cases
+        return ''
+    }
+
+    let attachedStrings = []
+    for(let i=0 ; i<strarr.length-k+1 ; i++){//loop through the array, doesn't go past the limit
+        let tempString = ''
+        for(let j=i ; j<i+k ; j++){//build the string
+            tempString+=strarr[j]
+        }
+        attachedStrings.push(tempString)
+    }
+
+    //return the index of the longest string
+    let attachedStringsLength = attachedStrings.map(s => s.length)
+    let indexLongestString = attachedStringsLength.indexOf(Math.max(...attachedStringsLength))
+    return attachedStrings[indexLongestString]
+}
+
+
+// console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)) // => "abigailtheta"
+// console.log(longestConsec([it,wkppv,ixoyx,3452,zzzzzzzzzzzz], 15)); // => ''
+
+//==================================================================================

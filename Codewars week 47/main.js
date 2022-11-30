79a2aa936b7340ec34ab9d124ebc9ee0728558c4
Olivier Reynard <olivier.reynard17@gmail.com>
2022-11-30 17:12:55 +0100

    daily push

--- Codewars week 47/main.js
+++ Codewars week 47/main.js
@@ -92,5 +92,87 @@ function greet (name, owner) {
     return `Hello ${name===owner ? 'boss' : 'guest'}`
 }
 
-console.log(greet('Daniel', 'Daniel')) // -> 'Hello boss'
-console.log(greet('Greg', 'Daniel')) // -> 'Hello guest'
+// console.log(greet('Daniel', 'Daniel')) // -> 'Hello boss'
+// console.log(greet('Greg', 'Daniel')) // -> 'Hello guest'
+
+
+//====================================================================
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+// Examples:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+
+function bouncingBall(h,  bounce,  window) {
+    if(h>0 && bounce>0 && bounce<1 && window<h){
+        let res = 1
+        let bounceHeight = bounce * h
+        while(bounceHeight>window){
+            res+=2
+            bounceHeight = bounce * bounceHeight
+        }
+        return res
+    }else{ //not valid
+        return -1
+    }
+}
+
+// console.log(bouncingBall(3, 0.66, 1.5));
+
+//=============================================================
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    let res = ''
+    for(let i=0 ; i<s.length ; i++){
+        if(s[i] !== '!'){
+            res+=s[i]
+        }
+    }
+    return res
+}
+
+function removeExclamationMarksBis(s){
+    return s.split('!').join('')
+}
+
+function removeExclamationMarksTer(s) {
+    return s.replace(/!/g, '');
+}
+
+//===============================================================
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+function setAlarm(employed, vacation){
+    return (employed && !vacation)
+}
+
+//===================================================================

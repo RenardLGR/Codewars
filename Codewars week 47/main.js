1aa0c03d44a25fff7512745ad604b4a361eb64a6
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-04 06:09:07 +0100

    daily push

--- Codewars week 47/main.js
+++ Codewars week 47/main.js
@@ -515,3 +515,20 @@ function hoopCount (n) {
 }
 
 //===========================================================
+// https://www.codewars.com/kata/587731fda577b3d1b0001196
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
+
+String.prototype.camelCase=function(){
+    if(this.length === 0) return ""//edge case
+
+    return this.trim().split(' ').map(w => w[0].toUpperCase() + w.slice(1)).join('')
+}
+
+// console.log("hello case".camelCase())
+
+//===============================================================

f48d866a855914fb46aaa5a44637910081d8d333
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-05 02:47:00 +0100

    daily push

--- Codewars week 47/main.js
+++ Codewars week 47/main.js
@@ -531,4 +531,94 @@ String.prototype.camelCase=function(){
 
 // console.log("hello case".camelCase())
 
+String.prototype.camelCaseBis=function(){
+    return this.split(' ').map(w => w.slice(0, 1).toUpperCase() + w.slice(1)).join('');
+}
+
 //===============================================================
+// https://www.codewars.com/kata/5541f58a944b85ce6d00006a
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prod you will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+// (depend on the language)
+
+// productFib(714) # should return (21, 34, true), 
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false), 
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true], 
+// productFib(800) # should return [34, 55, false], 
+// -----
+// productFib(714) # should return {21, 34, 1}, 
+// productFib(800) # should return {34, 55, 0},        
+// -----
+// productFib(714) # should return {21, 34, true}, 
+// productFib(800) # should return {34, 55, false}, 
+// Note:
+// You can see examples for your language in "Sample Tests".
+
+
+function productFib(target){ //by construction of the Fibonacci sequence
+    let fibNmin2 = 0
+    let fibNmin1 = 1
+    let fibN
+    let n = 2
+    let product = fibNmin2 * fibNmin1
+    while(product < target){
+        fibN = fibNmin2 + fibNmin1
+        fibNmin2 = fibNmin1
+        fibNmin1 = fibN
+        product = fibNmin2 * fibNmin1 
+        n++
+    }
+
+    return [fibNmin2, fibNmin1, product===target]
+}
+
+// console.log(productFib(6))
+// console.log(productFib(7))
+
+function productFibBinet(target){ //with the Binet's formula
+    let n = 1
+    let product = fibonacciN(n-1) * fibonacciN(n)
+    while(product < target){
+        n++
+        product = fibonacciN(n-1) * fibonacciN(n)
+    }
+
+    return [fibonacciN(n-1), fibonacciN(n), product===target]
+
+    function fibonacciN(n){
+        //https://fr.wikipedia.org/wiki/Suite_de_Fibonacci#Expression_fonctionnelle
+        let phi = (1+Math.sqrt(5))/2
+        let phiPrime = -1/phi
+        return Math.round((Math.pow(phi, n) - Math.pow(phiPrime, n))/Math.sqrt(5))
+    }
+    // console.log(fibonacciN(6))
+}
+
+
+// console.log(productFibBinet(6))
+// console.log(productFibBinet(7))

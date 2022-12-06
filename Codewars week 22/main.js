0f4a933d6d0c892382ae683767a41911ab220cec
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-06 03:42:55 +0100

    good week

--- Codewars week 22/main.js
+++ Codewars week 22/main.js
@@ -543,6 +543,32 @@ function knightVsKing(knightPosition, kingPosition) {
     }
 }
   
-console.log(knightVsKing([4, "C"], [6, "D"])); //=> Knight
-console.log(knightVsKing([7, "B"], [6, "C"])); //=> King
-console.log(knightVsKing([2, "F"], [6, "B"])); //=> None
+// console.log(knightVsKing([4, "C"], [6, "D"])); //=> Knight
+// console.log(knightVsKing([7, "B"], [6, "C"])); //=> King
+// console.log(knightVsKing([2, "F"], [6, "B"])); //=> None
+
+//=====================================================================
+// https://www.codewars.com/kata/5506b230a11c0aeab3000c1f
+// This program tests the life of an evaporator containing a gas.
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+// Example:
+// evaporator(10, 10, 5) -> 29
+// Note:
+// Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+
+function evaporator(content, evap_per_day, threshold){ 
+    let res = 0
+    threshold=threshold/100*content
+    while(content>threshold) {
+        content=content - evap_per_day/100*content
+        res++
+    }
+
+    return res
+}
+
+console.log(evaporator(10, 10, 5));

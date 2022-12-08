485e0f8e81950efa4c0d7070317f5f0f75b93764
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-08 06:51:26 +0100

    daily push

--- Codewars week 48/main.js
+++ Codewars week 48/main.js
@@ -353,3 +353,48 @@ function cakesBis(recipe, available){
 }
 
 //================================================
+// https://www.codewars.com/kata/55c6126177c9441a570000cc
+// My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
+
+// I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+
+// For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+
+// Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+
+// Example:
+// "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+
+// "100 180 90 56 65 74 68 86 99"
+// When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+
+// 180 is before 90 since, having the same "weight" (9), it comes before as a string.
+
+// All numbers in the list are positive numbers and the list can be empty.
+
+// Notes
+// it may happen that the input string have leading, trailing whitespaces and more than a unique whitespace between two consecutive numbers
+// For C: The result is freed.
+
+function orderWeight(strng) {
+    //sort from smallest to biggest
+    let sanitizedWeights = strng.trim().split(' ').filter(el => el.length>0)
+
+    let sortedWeights = sanitizedWeights.sort((a,b) => {
+        let aW = a.split('').reduce((acc, cur) => acc+ +cur,0)
+        let bW = b.split('').reduce((acc, cur) => acc+ +cur,0)
+
+        if(aW === bW){
+            return a.localeCompare(b)
+        }else{
+            return aW - bW
+        }
+    })
+
+    return sortedWeights.join(' ')
+}
+
+// console.log(orderWeight("56 65 74 100 99 68 86 180 90")); // => "100 180 90 56 65 74 68 86 99"
+// console.log(orderWeight("103 123 4444 99 2000")); // => "2000 103 123 4444 99"
+
+//======================================================

b9031b13d55433e7672569b95f1a06ef4aaffd24
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-10 01:37:15 +0100

    daily push

--- Codewars week 48/main.js
+++ Codewars week 48/main.js
@@ -462,3 +462,32 @@ function rangeExtractionBis(list){
 }
 
 //===================================================
+// https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+function firstNonRepeatingLetter(s) {
+    //strings are a single word
+    let res = s.split('').find((char, idx, arr) => arr.map(c => c.toLowerCase()).indexOf(char.toLowerCase()) === arr.map(c => c.toLowerCase()).lastIndexOf(char.toLowerCase()))
+
+    return res === undefined ? '' : res
+}
+
+// console.log(firstNonRepeatingLetter("sTreSS"));
+
+function firstNonRepeatingLetterBis(s){
+    let lowered = s.toLowerCase()
+    for(let i=0 ; i<s.length ; i++){
+        if(lowered.indexOf(lowered[i]) === lowered.lastIndexOf(lowered[i])){
+            return s[i]
+        }
+    }
+    return ''
+}
+
+//=====================================================

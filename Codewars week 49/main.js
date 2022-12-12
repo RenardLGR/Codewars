e353903d87b97f2b2fd77c85a83a7693cc11b00f
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-12 05:33:33 +0100

    pushing

---
+++ Codewars week 49/main.js
@@ -0,0 +1,95 @@
+const hi = 'HELLO'
+const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+//==========================================================
+// https://www.codewars.com/kata/57cfdf34902f6ba3d300001e
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+
+function twoSort(strings) {
+    return strings.sort()[0].split('').join('***')
+}
+
+// console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])) // => 'b***i***t***c***o***i***n'
+
+
+//============================================================
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
+// Task
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+// Example
+// With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+// 1 * (2 + 3) = 5
+// 1 * 2 * 3 = 6
+// 1 + 2 * 3 = 7
+// (1 + 2) * 3 = 9
+// So the maximum value that you can obtain is 9.
+
+// Notes
+// The numbers are always positive.
+// The numbers are in the range (1  ≤  a, b, c  ≤  10).
+// You can use the same operation more than once.
+// It's not necessary to place all the signs and brackets.
+// Repetition in numbers may occur .
+// You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+// Input >> Output Examples:
+// expressionsMatter(1,2,3)  ==>  return 9
+// Explanation:
+// After placing signs and brackets, the Maximum value obtained from the expression (1+2) * 3 = 9.
+
+// expressionsMatter(1,1,1)  ==>  return 3
+// Explanation:
+// After placing signs, the Maximum value obtained from the expression is 1 + 1 + 1 = 3.
+
+// expressionsMatter(9,1,1)  ==>  return 18
+// Explanation:
+// After placing signs and brackets, the Maximum value obtained from the expression is 9 * (1+1) = 18. ___# Task
+
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+// Example
+// With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+// 1 * (2 + 3) = 5
+// 1 * 2 * 3 = 6
+// 1 + 2 * 3 = 7
+// (1 + 2) * 3 = 9
+// So the maximum value that you can obtain is 9.
+
+// Notes
+// The numbers are always positive.
+// The numbers are in the range (1  ≤  a, b, c  ≤  10).
+// You can use the same operation more than once.
+// It's not necessary to place all the signs and brackets.
+// Repetition in numbers may occur .
+// You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+// Input >> Output Examples:
+// expressionsMatter(1,2,3)  ==>  return 9
+// Explanation:
+// After placing signs and brackets, the Maximum value obtained from the expression (1+2) * 3 = 9.
+
+// expressionsMatter(1,1,1)  ==>  return 3
+
+function expressionMatter(a, b, c) {
+    let opt1 = a*(b+c)
+    let opt2 = a*b*c
+    let opt3 = a+b*c
+    let opt4 = (a+b)*c
+    let opt5 = a*b+c
+    let opt6 = a+b+c
+
+    return Math.max(opt1, opt2, opt3, opt4, opt5, opt6)
+}
+
+// console.log(expressionMatter(1,2,3));
+
+
+//==============================================================

7b8a84529a8de7f401e5c5616f7f2f31c212995d
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-23 23:25:22 +0100

    daily push

--- Codewars week 50/main.js
+++ Codewars week 50/main.js
@@ -578,3 +578,19 @@ function zeroesFacBis(n){
 // console.log(zeroesFacBis(926054989)); // -> 231513738
 
 //==========================================================
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+    return ['', '', '', '', ''].length
+}
+
+function unusualFiveBis() {
+    return 'hello'.length
+}
+
+//=============================================================

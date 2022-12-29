421dfdcd27d335b6e072eb8bf81729c2cc8d1ab6
Olivier Reynard <olivier.reynard17@gmail.com>
2022-12-29 22:33:15 +0100

    daily push

--- Codewars week 50/main.js
+++ Codewars week 50/main.js
@@ -923,3 +923,35 @@ function sumStrings(a,b) {
 
 
 //========================================================
+// https://www.codewars.com/kata/50ee6b0bdeab583673000025
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// var a = "code";
+// var b = "wa.rs";
+// var name = a + b;
+
+//==========================================================
+// https://www.codewars.com/kata/55eca815d0d20962e1000106
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+// Task
+// Implement a function named
+
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10]
+// Note
+// min < max
+// step > 0
+// the range does not HAVE to include max (depending on the step)
+
+function generateRange(min, max, step){
+    let arr = []
+
+    for(let i=min ; i<=max ; i=i+step){
+        arr.push(i)
+    }
+
+    return arr
+}
+
+//=============================================================

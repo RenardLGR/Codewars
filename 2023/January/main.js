838b7eeb452053f4ad6c275e572e1b0e7aa80b74
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-06 18:41:20 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -175,3 +175,48 @@ function noBoringZerosBis(n) {
 }
 
 //===========================================================
+// https://www.codewars.com/kata/559a28007caad2ac4e000083
+// The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+// Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+// SEE IMG
+
+// Hint:
+// See Fibonacci sequence
+
+// Ref:
+// http://oeis.org/A000045
+
+// The function perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+// perimeter(5)  should return 80
+
+function perimeter(n) {
+    let memo = {}
+    let res = 0
+
+    for(let i=1 ; i<=n+1 ; i++){
+        res += 4 * fibRecMemo(i)
+    }
+
+    return res
+
+    function fibRecMemo(n){
+        if(n<2){
+            return n
+        }
+        
+        if(memo[n]){
+            return memo[n]
+        }else{
+            memo[n] = fibRecMemo(n-2) + fibRecMemo(n-1)
+            return memo[n]
+        }
+    }
+}
+
+// console.log(perimeter(5)); // 80
+// console.log(perimeter(7)); // 216
+
+//=========================================================

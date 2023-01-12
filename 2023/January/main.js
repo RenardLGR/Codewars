02097694038a9687dcc69b47bbaac787bf3945a5
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-12 05:46:23 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -614,3 +614,139 @@ function toBinary(n){
 }
 
 //==================================================
+// https://www.codewars.com/kata/54d81488b981293527000c8f
+// Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+// If there are two or more pairs with the required sum, the pair whose second element has the smallest index is the solution.
+
+// sum_pairs([11, 3, 7, 5],         10)
+// #              ^--^      3 + 7 = 10
+// == [3, 7]
+
+// sum_pairs([4, 3, 2, 3, 4],         6)
+// #          ^-----^         4 + 2 = 6, indices: 0, 2 *
+// #             ^-----^      3 + 3 = 6, indices: 1, 3
+// #                ^-----^   2 + 4 = 6, indices: 2, 4
+// #  * the correct answer is the pair whose second value has the smallest index
+// == [4, 2]
+
+// sum_pairs([0, 0, -2, 3], 2)
+// #  there are no pairs of values that can be added to produce 2.
+// == None/nil/undefined (Based on the language)
+
+// sum_pairs([10, 5, 2, 3, 7, 5],         10)
+// #              ^-----------^   5 + 5 = 10, indices: 1, 5
+// #                    ^--^      3 + 7 = 10, indices: 3, 4 *
+// #  * the correct answer is the pair whose second value has the smallest index
+// == [3, 7]
+// Negative numbers and duplicate numbers can and will appear.
+
+// NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+
+function sumPairs(ints, s) {
+    let isResult = false
+    let jIndx
+    let res
+    for(let i=0 ; i<ints.length ; i++){
+        for(let j=i+1 ; j<ints.length ; j++){
+            if(ints[i] + ints[j] === s){
+                if(!isResult){//if there is no result yet
+                    res = [ints[i], ints[j]]
+                    isResult = true
+                    jIndx = j
+                }
+                if(j < jIndx){//update if the newly index is smaller than the prev one
+                    res = [ints[i], ints[j]]
+                    jIndx = j
+                }
+            }
+        }
+    }
+
+    return isResult ? res : undefined
+}
+
+// console.log(sumPairs([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+// console.log(sumPairs([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+// console.log(sumPairs([20, -13, 40], -7)) // undefined
+// console.log(sumPairs([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+// console.log(sumPairs([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+// console.log(sumPairs([4, -2, 3, 3, 4], 8)) // [4, 4]
+// console.log(sumPairs([0, 2, 0], 0)) // [0, 0]
+// console.log(sumPairs([5, 9, 13, -3], 10)) // [13, -3]
+
+//WORKS BUT TOO LONG
+//isResult should not be kept as jIndx can do it, ifs can be one ; improvement :
+
+function sumPairsBis(ints, s) {
+    let jIndx = undefined
+    let res
+    for(let i=0 ; i<ints.length ; i++){
+        for(let j=i+1 ; j<ints.length ; j++){
+            if(ints[i] + ints[j] === s){
+                if((jIndx === undefined) || j < jIndx){//if there is no result yet OR if the newly index is smaller than the prev one
+                    res = [ints[i], ints[j]]
+                    jIndx = j
+                }
+            }
+        }
+    }
+
+    return jIndx !== undefined ? res : undefined
+}
+
+// console.log(sumPairsBis([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+// console.log(sumPairsBis([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+// console.log(sumPairsBis([20, -13, 40], -7)) // undefined
+// console.log(sumPairsBis([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+// console.log(sumPairsBis([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+// console.log(sumPairsBis([4, -2, 3, 3, 4], 8)) // [4, 4]
+// console.log(sumPairsBis([0, 2, 0], 0)) // [0, 0]
+// console.log(sumPairsBis([5, 9, 13, -3], 10)) // [13, -3]
+
+//WORKS BUT TOO LONG
+//Why would I keep looping if j or i are bigger than jIndx ; improvements :
+
+function sumPairsTer(ints, s) {
+    let jIndx = ints.length
+    let isResult = false
+    let res
+    for(let i=0 ; i<jIndx ; i++){
+        for(let j=i+1 ; j<=jIndx ; j++){
+            if(ints[i] + ints[j] === s){
+                if((!isResult) || j < jIndx){//if there is no result yet OR if the newly index is smaller than the prev one
+                    res = [ints[i], ints[j]]
+                    jIndx = j
+                    isResult = true
+                }
+            }
+        }
+    }
+
+    return isResult ? res : undefined
+}
+
+console.log(sumPairsTer([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+console.log(sumPairsTer([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+console.log(sumPairsTer([20, -13, 40], -7)) // undefined
+console.log(sumPairsTer([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+console.log(sumPairsTer([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+console.log(sumPairsTer([4, -2, 3, 3, 4], 8)) // [4, 4]
+console.log(sumPairsTer([0, 2, 0], 0)) // [0, 0]
+console.log(sumPairsTer([5, 9, 13, -3], 10)) // [13, -3]
+
+//WORKS BUT TOO LONG
+//Let's trade some time complexity for space complexity
+
+function sumPairsQuater(ints, s){
+    
+}
+
+console.log(sumPairsQuater([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+console.log(sumPairsQuater([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+console.log(sumPairsQuater([20, -13, 40], -7)) // undefined
+console.log(sumPairsQuater([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+console.log(sumPairsQuater([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+console.log(sumPairsQuater([4, -2, 3, 3, 4], 8)) // [4, 4]
+console.log(sumPairsQuater([0, 2, 0], 0)) // [0, 0]
+console.log(sumPairsQuater([5, 9, 13, -3], 10)) // [13, -3]

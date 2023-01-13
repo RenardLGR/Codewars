fb38e9095e26da50c5f9f49e5b8ca387f66846e8
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-13 05:05:20 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -726,27 +726,87 @@ function sumPairsTer(ints, s) {
     return isResult ? res : undefined
 }
 
-console.log(sumPairsTer([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
-console.log(sumPairsTer([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
-console.log(sumPairsTer([20, -13, 40], -7)) // undefined
-console.log(sumPairsTer([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
-console.log(sumPairsTer([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
-console.log(sumPairsTer([4, -2, 3, 3, 4], 8)) // [4, 4]
-console.log(sumPairsTer([0, 2, 0], 0)) // [0, 0]
-console.log(sumPairsTer([5, 9, 13, -3], 10)) // [13, -3]
+// console.log(sumPairsTer([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+// console.log(sumPairsTer([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+// console.log(sumPairsTer([20, -13, 40], -7)) // undefined
+// console.log(sumPairsTer([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+// console.log(sumPairsTer([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+// console.log(sumPairsTer([4, -2, 3, 3, 4], 8)) // [4, 4]
+// console.log(sumPairsTer([0, 2, 0], 0)) // [0, 0]
+// console.log(sumPairsTer([5, 9, 13, -3], 10)) // [13, -3]
 
 //WORKS BUT TOO LONG
 //Let's trade some time complexity for space complexity
+//Understand this exercice before : https://leetcode.com/problems/two-sum/description/
+//See below
 
-function sumPairsQuater(ints, s){
-    
+function sumPairsQuater(ints, target){
+    // Close to : https://leetcode.com/problems/two-sum/description/
+    const comp = {}; //{nToFind : firstVal}
+    for(let i=0; i<ints.length; i++){
+        if((ints[i]) in comp){ //check if the number already exists
+            return [ comp[ints[i]] , ints[i] ]
+        }
+        comp[target-ints[i]] = ints[i]
+    }
+    return undefined
 }
 
-console.log(sumPairsQuater([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
-console.log(sumPairsQuater([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
-console.log(sumPairsQuater([20, -13, 40], -7)) // undefined
-console.log(sumPairsQuater([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
-console.log(sumPairsQuater([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
-console.log(sumPairsQuater([4, -2, 3, 3, 4], 8)) // [4, 4]
-console.log(sumPairsQuater([0, 2, 0], 0)) // [0, 0]
-console.log(sumPairsQuater([5, 9, 13, -3], 10)) // [13, -3]
+// console.log(sumPairsQuater([1, 4, 8, 7, 3, 15], 8)) // [1, 7]
+// console.log(sumPairsQuater([1, -2, 3, 0, -6, 1], -6)) // [0, -6]
+// console.log(sumPairsQuater([20, -13, 40], -7)) // undefined
+// console.log(sumPairsQuater([1, 2, 3, 4, 1, 0], 2)) // [1, 1]
+// console.log(sumPairsQuater([10, 5, 2, 3, 7, 5], 10)) // [3, 7]
+// console.log(sumPairsQuater([4, -2, 3, 3, 4], 8)) // [4, 4]
+// console.log(sumPairsQuater([0, 2, 0], 0)) // [0, 0]
+// console.log(sumPairsQuater([5, 9, 13, -3], 10)) // [13, -3]
+
+// https://leetcode.com/problems/two-sum/description/
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+ 
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+const twoSum = function(nums, target) {
+    const comp = {}; //{nToFind : firstIndex}
+    for(let i=0; i<nums.length; i++){
+        if((nums[i]) in comp){ //check if the idx already exists
+            return [ comp[ nums[i] ] , i]
+        }
+        comp[target-nums[i]] = i
+    }
+    // return -1
+};
+
+// console.log(twoSum([2,7,11,15], 9)); // [ 0, 1 ]
+
+
+//============================================

4f6d2c20af4ff521a57d5700d077652cb9609b05
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-14 07:45:55 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -810,3 +810,81 @@ const twoSum = function(nums, target) {
 
 
 //============================================
+// https://www.codewars.com/kata/5ce399e0047a45001c853c2b
+// Let us consider this example (array written in general format):
+
+// ls = [0, 1, 3, 6, 10]
+
+// Its following parts:
+
+// ls = [0, 1, 3, 6, 10]
+// ls = [1, 3, 6, 10]
+// ls = [3, 6, 10]
+// ls = [6, 10]
+// ls = [10]
+// ls = []
+// The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+// The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+
+// Other Examples:
+// ls = [1, 2, 3, 4, 5, 6] 
+// parts_sums(ls) -> [21, 20, 18, 15, 11, 6, 0]
+
+// ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]
+// parts_sums(ls) -> [10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]
+
+function partsSums(ls) {
+    //naive
+    let res = []
+    let cpy = ls.slice()
+    while(cpy.length > 0){
+        res.push(cpy.reduce((acc, cur) => acc + cur, 0))
+        cpy.shift()
+    }
+
+    res.push(0)
+
+    return res
+}
+
+// console.log(partsSums([0, 1, 3, 6, 10])) // [20, 20, 19, 16, 10, 0]
+//Too long
+
+function partsSumsBis(ls){
+    //add the sum, sum = sum - cpy.shift()
+    let res = []
+    let cpy = ls.slice()
+    let sum = cpy.reduce((acc, cur) => acc + cur, 0)
+
+    res.push(sum)
+
+    while(sum > 0){
+        res.push(sum = sum - cpy.shift())
+    }
+
+    return res
+}
+
+// console.log(partsSumsBis([0, 1, 3, 6, 10])) // [20, 20, 19, 16, 10, 0]
+//Too long
+
+function partsSumsTer(ls){
+    //same but shift() should not be needed, it has an complexity of O(n)
+    let res = []
+    let cpy = ls.slice()
+    let sum = cpy.reduce((acc, cur) => acc + cur, 0)
+
+    res.push(sum)
+
+    for(let i=0 ; i<ls.length ; i++){
+        sum -= ls[i]
+        res.push(sum)
+    }
+
+    return res
+}
+
+// console.log(partsSumsTer([0, 1, 3, 6, 10])) // [20, 20, 19, 16, 10, 0]
+
+//==============================================

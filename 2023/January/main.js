755f98e897c6dc627888b2bb31ac9144946b67e3
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-23 20:42:41 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -946,4 +946,54 @@ function formatMoneyBis(amount){
     return '$' + amount.toFixed(2);
 }
 
+// console.log(formatMoneyBis(93.9));
+// console.log(formatMoneyBis(93));
+
 //====================================================
+// https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
+// In this kata, you will write a function that returns the positions and the values of the "peaks" (or local maxima) of a numeric array.
+
+// For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with a value of 5 (since arr[3] equals 5).
+
+// The output will be returned as an object with two properties: pos and peaks. Both of these properties should be arrays. If there is no peak in the given array, then the output should be {pos: [], peaks: []}.
+
+// Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages)
+
+// All input arrays will be valid integer arrays (although it could still be empty), so you won't need to validate the input.
+
+// The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore, we don't know if it is a peak or not).
+
+// Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] and [1, 2, 2, 2, 2] do not. In case of a plateau-peak, please only return the position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
+
+// Have fun!
+
+function pickPeaks(arr){
+    let res = {pos: [], peaks:[]}
+    let increasing = arr[1] > arr[0]
+
+    for(let i=1 ; i<arr.length ; i++){
+        if(increasing && arr[i] < arr[i-1]){ //local peak found
+            increasing = false
+            let val = arr[i-1]
+            let j = i-1
+            while(arr[j-1] === val){ //find the start of the plateau
+                j--
+            }
+            res.pos.push(j)
+            res.peaks.push(val)
+        }
+        if(!increasing && arr[i] > arr[i-1]){
+            increasing = true
+        }
+    }
+
+    return res
+}
+
+// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3])); // {pos: [3, 7], peaks: [6, 3]}
+// console.log(pickPeaks([0, 1, 2, 5, 1, 0])); // { pos: [ 3 ], peaks: [ 5 ] }
+// console.log(pickPeaks([1, 2, 2, 2, 1])); // { pos: [ 1 ], peaks: [ 2 ] }
+// console.log(pickPeaks([1, 2, 2, 2, 2])); // { pos: [  ], peaks: [  ] }
+// console.log(pickPeaks([1, 2, 2, 2, 3])); // { pos: [  ], peaks: [  ] }
+
+//===================================================

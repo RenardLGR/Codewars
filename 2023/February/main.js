829e32dc2cffd4366bfa5914b0886f6f8be095a4
Olivier Reynard <olivier.reynard17@gmail.com>
2023-02-25 00:51:54 +0100

    daily push

--- 2023/February/main.js
+++ 2023/February/main.js
@@ -978,3 +978,26 @@ function printArrayOther(array){
 // console.log(printArrayOther([["hello","this","is","an","array!"],["a","b","c","d","e!"]]));
 
 //==============================================
+// https://www.codewars.com/kata/5aff237c578a14752d0035ae
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+// Example
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+// Note: the result should be rounded down to the nearest integer.
+
+// Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+    let args = Array.from(arguments)
+    console.log(args);
+    return Math.floor( Math.sqrt( args.reduce((acc, cur) => acc+cur*cur, 0) )/2 )
+}
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45)) // 86

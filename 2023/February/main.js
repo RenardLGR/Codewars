c6ff35c0a28f8991b03053d540bb33cc1b72cbb6
Olivier Reynard <olivier.reynard17@gmail.com>
2023-02-26 01:39:04 +0100

    daily push

--- 2023/February/main.js
+++ 2023/February/main.js
@@ -996,8 +996,133 @@ function printArrayOther(array){
 
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
     let args = Array.from(arguments)
-    console.log(args);
     return Math.floor( Math.sqrt( args.reduce((acc, cur) => acc+cur*cur, 0) )/2 )
 }
 
-console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45)) // 86
+// console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45)) // 86
+
+//=====================================================
+// https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4
+// The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
+
+// A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
+
+// We will write a function gap with parameters:
+
+// g (integer >= 2) which indicates the gap we are looking for
+
+// m (integer > 2) which gives the start of the search (m inclusive)
+
+// n (integer >= m) which gives the end of the search (n inclusive)
+
+// In the example above gap(2, 3, 50) will return [3, 5] or (3, 5) or {3, 5} which is the first pair between 3 and 50 with a 2-gap.
+
+// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise `nil or null or None or Nothing (or ... depending on the language).
+
+// In such a case (no pair of prime numbers with a gap of `g`)
+// In C: return [0, 0]
+// In C++, Lua, COBOL: return `{0, 0}`. 
+// In F#: return `[||]`. 
+// In Kotlin, Dart and Prolog: return `[]`.
+// In Pascal: return Type TGap (0, 0).
+// Examples:
+// - gap(2, 5, 7) --> [5, 7] or (5, 7) or {5, 7}
+
+// gap(2, 5, 5) --> nil. In C++ {0, 0}. In F# [||]. In Kotlin, Dart and Prolog return []`
+
+// gap(4, 130, 200) --> [163, 167] or (163, 167) or {163, 167}
+
+// ([193, 197] is also such a 4-gap primes between 130 and 200 but it's not the first pair)
+
+// gap(6,100,110) --> nil or {0, 0} or ... : between 100 and 110 we have 101, 103, 107, 109 but 101-107is not a 6-gap because there is 103in between and 103-109is not a 6-gap because there is 107in between.
+
+// You can see more examples of return in Sample Tests.
+
+// Note for Go
+// For Go: nil slice is expected when there are no gap between m and n. Example: gap(11,30000,100000) --> nil
+
+// Ref
+// https://en.wikipedia.org/wiki/Prime_gap
+
+function gapInPrimes(g, start, end) {
+    for(let i=start ; i<=end-g ; i++){
+
+        if(isPrime(i) && isPrime(i+g)){ //if a gap is found
+            let isGapValid = true
+            //check if the gape contains no prime
+            for(let j=i+1 ; j<i+g ; j++){
+                if(isPrime(j)){
+                    isGapValid = false
+                    break
+                }
+            }
+
+            if(isGapValid){
+                return [i, i+g]
+            }
+        }
+    }
+
+    return null
+
+    function isPrime(num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// console.log(gapInPrimes(2,100,110)) // [101, 103]
+// console.log(gapInPrimes(4,100,110)) // [103, 107]
+// console.log(gapInPrimes(6,100,110)) // null
+// console.log(gapInPrimes(8,300,400)) // [359, 367]
+// console.log(gapInPrimes(10,300,400)) // [337, 347]
+
+function gapInPrimesBis(g, start, end){
+    let lastPrime = 0
+    for(let i=start ; i<=end ; i++){
+        if(isPrime(i)){
+            if(lastPrime === 0){
+                //initialization
+                lastPrime = i
+            }else{
+                if(i-lastPrime === g){
+                    return [lastPrime, i]
+                }
+                lastPrime = i
+            }
+        }
+    }
+
+    return null
+
+    function isPrime(num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// console.log(gapInPrimesBis(2,100,110)) // [101, 103]
+// console.log(gapInPrimesBis(4,100,110)) // [103, 107]
+// console.log(gapInPrimesBis(6,100,110)) // null
+// console.log(gapInPrimesBis(8,300,400)) // [359, 367]
+// console.log(gapInPrimesBis(10,300,400)) // [337, 347]
+
+//===============================================

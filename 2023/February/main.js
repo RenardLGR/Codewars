3dc1948afd8914c335fd2a80e8f1bec07872f001
Olivier Reynard <olivier.reynard17@gmail.com>
2023-03-03 00:12:19 +0100

    daily end of february

--- 2023/February/main.js
+++ 2023/February/main.js
@@ -1049,7 +1049,7 @@ function gapInPrimes(g, start, end) {
 
         if(isPrime(i) && isPrime(i+g)){ //if a gap is found
             let isGapValid = true
-            //check if the gape contains no prime
+            //check if the gap contains no prime
             for(let j=i+1 ; j<i+g ; j++){
                 if(isPrime(j)){
                     isGapValid = false
@@ -1126,3 +1126,135 @@ function gapInPrimesBis(g, start, end){
 // console.log(gapInPrimesBis(10,300,400)) // [337, 347]
 
 //===============================================
+// https://www.codewars.com/kata/564057bc348c7200bd0000ff
+// From Wikipedia:
+
+// "A divisibility rule is a shorthand way of determining whether a given integer is divisible by a fixed divisor without performing the division, usually by examining its digits."
+
+// When you divide the successive powers of 10 by 13 you get the following remainders of the integer divisions:
+
+// 1, 10, 9, 12, 3, 4 because:
+
+// 10 ^ 0 ->  1 (mod 13)
+// 10 ^ 1 -> 10 (mod 13)
+// 10 ^ 2 ->  9 (mod 13)
+// 10 ^ 3 -> 12 (mod 13)
+// 10 ^ 4 ->  3 (mod 13)
+// 10 ^ 5 ->  4 (mod 13)
+// (For "mod" you can see: https://en.wikipedia.org/wiki/Modulo_operation)
+
+// Then the whole pattern repeats. Hence the following method:
+
+// Multiply
+
+// the right most digit of the number with the left most number in the sequence shown above,
+// the second right most digit with the second left most digit of the number in the sequence.
+// The cycle goes on and you sum all these products. Repeat this process until the sequence of sums is stationary.
+
+// Example:
+// What is the remainder when 1234567 is divided by 13?
+
+// 7      6     5      4     3     2     1  (digits of 1234567 from the right)
+// ×      ×     ×      ×     ×     ×     ×  (multiplication)
+// 1     10     9     12     3     4     1  (the repeating sequence)
+
+// 7 is in the unit (10^0) number so we multiply by 1, because 10 ^ 0 ->  1 (mod 13)
+// 6 is in the tenth (10^1) number so we multiply by 1, because 10 ^ 1 -> 10 (mod 13)
+// etc.
+// Therefore following the method we get:
+
+//  7×1 + 6×10 + 5×9 + 4×12 + 3×3 + 2×4 + 1×1 = 178
+
+// We repeat the process with the number 178:
+
+// 8x1 + 7x10 + 1x9 = 87
+
+// and again with 87:
+
+// 7x1 + 8x10 = 87
+
+// From now on the sequence is stationary (we always get 87) and the remainder of 1234567 by 13 is the same as the remainder of 87 by 13 ( i.e 9).
+
+// Task:
+// Call thirt the function which processes this sequence of operations on an integer n (>=0). thirt will return the stationary number.
+
+// thirt(1234567) calculates 178, then 87, then 87 and returns 87.
+
+// thirt(321) calculates 48, 48 and returns 48
+
+function thirt(n) {
+    // 7 is in the unit (10^0) number so we multiply by 1, because 10 ^ 0 ->  1 (mod 13)
+    // 6 is in the tenth (10^1) number so we multiply by 1, because 10 ^ 1 -> 10 (mod 13)
+    // etc.
+
+    let congruent = [1, 10, 9, 12, 3, 4] //a power of 10 is congruent this number modulo 13
+
+    let prevSum = n
+
+    let isStationary = false
+
+    while(!isStationary){
+        isStationary = true //avoid the possibilty of an infinite loop if the input is faulty
+        let newSum = 0
+        let nArr = prevSum.toString().split('')
+        let powOf10 = 0
+
+        while(nArr.length > 0){ //calculate the sum
+            let cur = parseInt(nArr.pop())
+            newSum += cur * congruent[powOf10]
+            powOf10++
+            if(powOf10 === 6){
+                powOf10 = 0
+            }
+        }
+
+        if(newSum !== prevSum){ //check if stationary
+            isStationary = false
+            prevSum = newSum
+        }
+    }
+
+    return prevSum
+}
+
+// console.log(thirt(1234567)); //87
+// console.log(thirt(321)) //48
+
+function thirtBis(n){
+    //same but shorter
+
+    let congruent = [1, 10, 9, 12, 3, 4] //a power of 10 is congruent this number modulo 13
+
+    let prevSum = n
+
+    let isStationary = false
+
+    while(!isStationary){
+        isStationary = true //avoid the possibilty of an infinite loop if the input is faulty
+        let newSum = (''+prevSum).split('').reverse().reduce((acc, cur, idx) => {
+            acc += Number(cur) * congruent[idx%6]
+            //note : Number(cur) is actually useless because a String * Number return a Number
+            return acc
+        }, 0)
+
+        if(newSum !== prevSum){ //check if stationary
+            isStationary = false
+            prevSum = newSum
+        }
+    }
+
+    return prevSum
+}
+
+function thirtTer(n){
+    //ultimate short
+
+    let congruent = [1, 10, 9, 12, 3, 4] //a power of 10 is congruent this number modulo 13
+
+    let newSum = (''+n).split('').reverse().reduce((acc, cur, idx) => acc + cur*congruent[idx%6] ,0)
+
+    return newSum === n ? n : thirtTer(newSum)
+}
+
+// console.log(thirtTer(1234567)); //87
+// console.log(thirtTer(321)) //48

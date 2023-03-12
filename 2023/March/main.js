890211ea47f8439128b6b4cba89ab0aecbda8996
Olivier Reynard <olivier.reynard17@gmail.com>
2023-03-12 07:41:17 +0100

    daily push

--- 2023/March/main.js
+++ 2023/March/main.js
@@ -188,3 +188,65 @@ function longestSlideDownQuater(pyramid) {
 }
 
 //============================================
+// https://www.codewars.com/kata/51b66044bce5799a7f000003/train/javascript
+// Create a RomanNumerals class that can convert a roman numeral to and from an integer value. It should follow the API demonstrated in the examples below. Multiple roman numeral values will be tested for each helper method.
+
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+// Input range : 1 <= n < 4000
+
+// In this kata 4 should be represented as IV, NOT as IIII (the "watchmaker's four").
+
+// Examples
+// RomanNumerals.toRoman(1000); // should return 'M'
+// RomanNumerals.fromRoman('M'); // should return 1000
+// Help
+// Symbol	Value
+// I	1
+// IV	4
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1000
+
+class RomanNumerals {
+
+    static toRoman(num) {
+        let val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        let rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        let res = ''
+        for(let i=0 ; i<val.length ; i++){
+            while(num >= val[i]){
+                res += rom[i]
+                num -= val[i]
+            }
+        }
+    
+        return res
+    }
+  
+    static fromRoman(s) {
+        let val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        let rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        let res = 0
+        while(s.length > 0){
+            let cur = s.slice(0, 2)
+    
+            if(rom.includes(cur)){
+                res += val[rom.indexOf(cur)]
+                s = s.slice(2)
+            }else{
+                cur = s.slice(0, 1)
+                res += val[rom.indexOf(cur)]
+                s = s.slice(1)
+            }
+        }
+    
+        return res
+    }
+}
+
+// console.log(RomanNumerals.fromRoman('MDCLXVI')); // 1666
+// console.log(RomanNumerals.toRoman(1990)) // 'MCMXC'

d92b9f43e532dde9ec7b9865403a31a38c6b9ed3
Olivier Reynard <olivier.reynard17@gmail.com>
2023-03-18 21:51:20 +0100

    daily push

--- 2023/March/main.js
+++ 2023/March/main.js
@@ -1011,3 +1011,128 @@ function tortoiseRaceBis(v1, v2, lead) {
 // console.log(tortoiseRaceBis(80, 91, 37)); //[ 3, 21, 49 ]
 
 //===========================================
+// https://www.codewars.com/kata/54d496788776e49e6b00052f
+// Given an array of positive or negative integers
+
+//  I= [i1,..,in]
+
+// you have to produce a sorted array P of the form
+
+// [ [p, sum of all ij of I for which p is a prime factor (p positive) of ij] ...]
+
+// P will be sorted by increasing order of the prime numbers. The final result has to be given as a string in Java, C#, C, C++ and as an array of arrays in other languages.
+
+// Example:
+// I = [12, 15]; //result = [[2, 12], [3, 27], [5, 15]]
+// [2, 3, 5] is the list of all prime factors of the elements of I, hence the result.
+// 2 is a prime factor of 12 so [2, 12]
+// 3 is a prime factor of 12 and 15 so [2, 12+15=27]
+
+// Notes:
+
+// It can happen that a sum is 0 if some numbers are negative!
+// Example: I = [15, 30, -45] 5 divides 15, 30 and (-45) so 5 appears in the result, the sum of the numbers for which 5 is a factor is 0 so we have [5, 0] in the result amongst others.
+
+// In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings.
+
+function sumOfDivided(lst) {
+    let primeFactors = []
+
+    //Get every prime factors of every elements, no duplicate
+    lst.forEach(n => {
+        let primesOfN = getPrimeFactors(n)
+        primesOfN.forEach(p => {
+            if(!primeFactors.includes(p)){
+                primeFactors.push(p)
+            }
+        })
+    })
+
+    primeFactors.sort((a,b) => a-b) //sort ascendant
+
+    return primeFactors.map(pf => {
+        let subarr = [pf]
+        //Get the sum of the elements of list in which the current prime factor is valid
+        let sum = lst.reduce((acc, cur) => {
+            if(cur%pf === 0){ //if the current prime factor is a prime factor of the current element of list
+                acc += cur
+            }
+            return acc
+        }, 0)
+        subarr.push(sum)
+        return subarr
+    })
+
+
+    function getPrimeFactors(n){
+        if(n<0){ //allows negative inputs
+            n = -n
+        }
+
+        let res = []
+        for(let i=2 ; i<=n ; i++){
+            if(isPrime(i) && (n%i===0)){
+                res.push(i)
+                while (n%i === 0) { //decrease number of iterations
+                    n /= i
+                }
+            }
+        }
+
+        return res
+    }
+
+    function isPrime(n){
+        let res = true
+        if(n<=1){
+            return false
+        }
+        for(let i=2 ; i<=Math.sqrt(n) ; i++){
+            if(n%i === 0){
+                return false
+            }
+        }
+
+        return res
+    }
+}
+
+
+//Good but a little long to read, getPrimeFactors can be simplified :
+
+function sumOfDividedBis(lst){
+    //Get prime factors, sort them, remove duplicate
+    let primeFactors = lst.reduce((acc, cur) => acc.concat(getPrimeFactors(cur)), []).filter((pf, idx, arr) => arr.indexOf(pf) === idx).sort((a,b) => a-b)
+
+    return primeFactors.map(pf => {
+        let subarr = [pf]
+        //Get the sum of the elements of list in which the current prime factor is valid
+        let sum = lst.reduce((acc, cur) => {
+            if(cur%pf === 0){ //if the current prime factor is a prime factor of the current element of list
+                acc += cur
+            }
+            return acc
+        }, 0)
+        subarr.push(sum)
+        return subarr
+    })
+
+    function getPrimeFactors(n){
+        if(n<0){ //allows negative inputs
+            n = -n
+        }
+        let res = []
+        for(let i=2 ; i<=n ; i++){
+            if(n%i===0){
+                res.push(i)
+                while(n%i===0){
+                    n = n/i
+                }
+            }
+        }
+
+        return res
+    }
+}
+
+//===========================================

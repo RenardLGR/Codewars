df988fd3a7343e0605ed6e99e0327e2fe25290c4
Olivier Reynard <olivier.reynard17@gmail.com>
2023-03-26 18:48:32 +0200

    pushing

--- 2023/March/main.js
+++ 2023/March/main.js
@@ -1152,7 +1152,7 @@ function sumOfDividedBis(lst){
 
 // For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
 
-function encodeVow(string) {
+function encodeVowBis(string) {
     let res
     for(let i=0 ; i<string.length ; i++){
         if(string[i].match(/aeiou/)){
@@ -1209,4 +1209,95 @@ function decodeVow(string) {
     })
 }
 
+function encodeVowTer(string){
+    return string.replace(/[aeiou]/g, match => {
+        let code = "_aeiou"
+        return code.indexOf(match)
+      });
+}
+  
+function decodeVowTer(string) {
+    return string.replace(/[1-5]/g, match => {
+        let code = "_aeiou"
+        return code[match]
+    })
+}
+
 //===========================================
+// https://www.codewars.com/kata/52a382ee44408cea2500074c
+// Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix.
+
+// How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
+
+// A 1x1 matrix |a| has determinant a.
+
+// A 2x2 matrix [ [a, b], [c, d] ] or
+
+// |a  b|
+// |c  d|
+// has determinant: a*d - b*c.
+
+// The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants of n matrices ofn-1 x n-1 size.
+
+// For the 3x3 case, [ [a, b, c], [d, e, f], [g, h, i] ] or
+
+// |a b c|  
+// |d e f|  
+// |g h i|  
+// the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs:
+
+// |- - -|
+// |- e f|
+// |- h i|  
+// Note the alternation of signs.
+
+// The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first row [a, b, c, d], then:
+
+// det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)
+
+//Note : We can take any row or col and have the minor matrix by crossing the row and line of the element we took.
+// If we had to do it manually, we would chose the row or col with many zeroes as it is easier to compute.
+
+function determinant(m) {
+    if(m.length === 1){
+        return m[0][0]
+    }
+    else if(m.length === 2){
+        return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+    }
+    else{
+        //The minor matrix will be found from elements from the first line
+        let sign = 1
+        let res = 0
+        for(let i=0 ; i<m.length ; i++){
+            //For every element of the first line
+            let cur = m[0][i]
+            let cur_minor = []
+            //Build the minor matrix
+            for(let lin=1 ; lin<m.length ; lin++){
+                //start at 1 because we don't include the first line
+                let tempLine = []
+                for(let col=0 ; col<m.length ; col++){
+                    if(col !== i){
+                        tempLine.push(m[lin][col])
+                    }
+                }
+                cur_minor.push(tempLine.slice())
+            }
+
+            res = res + sign*cur*determinant(cur_minor)
+            sign = -sign
+        }
+        return res
+    }
+}
+
+// console.log(determinant([[2,4,2],[3,1,1],[1,2,0]])); //10
+// console.log(determinant([
+//     [2,4,5,6],
+//     [-1,5,6,9],
+//     [3,7,1,-6],
+//     [4,-2,3,5]
+// ])); //108
+
+//================================================

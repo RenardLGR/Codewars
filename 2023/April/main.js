13507948e73e08bb16f00401e93775f66811fda4
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-07 04:29:26 +0200

    daily push

--- 2023/April/main.js
+++ 2023/April/main.js
@@ -220,3 +220,40 @@ function validPhoneNumber(phoneNumber){
 // console.log(validPhoneNumber("1234567890")); // false (missing parentheses and hyphen)
 
 //================================================
+// https://www.codewars.com/kata/526989a41034285187000de4
+// Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+
+// All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+
+// Examples
+// * With input "10.0.0.0", "10.0.0.50"  => return   50 
+// * With input "10.0.0.0", "10.0.1.0"   => return  256 
+// * With input "20.0.0.10", "20.0.1.0"  => return  246
+
+function ipsBetween(start, end){
+
+    let decimalStart = parseInt(start.split('.').map(byte => {
+        let bin = Number(byte).toString(2)
+        while(bin.length < 8){
+            bin = '0' + bin
+        }
+        return bin
+    }).join(''), 2)
+
+    let decimalEnd = parseInt(end.split('.').map(byte => {
+        let bin = Number(byte).toString(2)
+        while(bin.length < 8){
+            bin = '0' + bin
+        }
+        return bin
+    }).join(''), 2)
+
+
+    return decimalEnd - decimalStart
+}
+
+// console.log(ipsBetween("10.0.0.0", "10.0.0.50")); //50
+// console.log(ipsBetween("10.0.0.0", "10.0.1.0")); //256
+// console.log(ipsBetween("20.0.0.10", "20.0.1.0")); //246
+
+//===========================================================

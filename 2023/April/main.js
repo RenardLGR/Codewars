524ba500494de5ba8197a4999143783dbd44be36
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-16 03:20:02 +0200

    daily push

--- 2023/April/main.js
+++ 2023/April/main.js
@@ -485,3 +485,291 @@ function chainAdd(n){
 // console.log(chainAdd(1)(2)(3)(4)(5)) // 15
 
 //=================================================
+// https://www.codewars.com/kata/5853213063adbd1b9b0000be
+// Short Intro
+
+// Some of you might remember spending afternoons playing Street Fighter 2 in some Arcade back in the 90s or emulating it nowadays with the numerous emulators for retro consoles.
+
+// Can you solve this kata? Suuure-You-Can!
+
+// UPDATE: a new kata's harder version is available here.
+// https://www.codewars.com/kata/58583922c1d5b415b00000ff/javascript
+
+// The Kata
+
+// You'll have to simulate the video game's character selection screen behaviour, more specifically the selection grid. Such screen looks like this:
+
+// Selection Grid Layout in text:
+
+// | Ryu  | E.Honda | Blanka  | Guile   | Balrog | Vega    |
+// | Ken  | Chun Li | Zangief | Dhalsim | Sagat  | M.Bison |
+// Input
+
+// the list of game characters in a 2x6 grid;
+// the initial position of the selection cursor (top-left is (0,0));
+// a list of moves of the selection cursor (which are up, down, left, right);
+// Output
+
+// the list of characters who have been hovered by the selection cursor after all the moves (ordered and with repetition, all the ones after a move, whether successful or not, see tests);
+// Rules
+
+// Selection cursor is circular horizontally but not vertically!
+
+// As you might remember from the game, the selection cursor rotates horizontally but not vertically; that means that if I'm in the leftmost and I try to go left again I'll get to the rightmost (examples: from Ryu to Vega, from Ken to M.Bison) and vice versa from rightmost to leftmost.
+
+// Instead, if I try to go further up from the upmost or further down from the downmost, I'll just stay where I am located (examples: you can't go lower than lowest row: Ken, Chun Li, Zangief, Dhalsim, Sagat and M.Bison in the above image; you can't go upper than highest row: Ryu, E.Honda, Blanka, Guile, Balrog and Vega in the above image).
+
+// Test
+
+// For this easy version the fighters grid layout and the initial position will always be the same in all tests, only the list of moves change.
+
+// Notice: changing some of the input data might not help you.
+
+// Examples
+
+// fighters = [
+//   ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+//   ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+// ]
+// initial_position = (0,0)
+// moves = ['up', 'left', 'right', 'left', 'left']
+// then I should get:
+
+// ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
+// as the characters I've been hovering with the selection cursor during my moves. Notice: Ryu is the first just because it "fails" the first up See test cases for more examples.
+
+// fighters = [
+//   ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+//   ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+// ]
+// initial_position = (0,0)
+// moves = ['right', 'down', 'left', 'left', 'left', 'left', 'right']
+// Result:
+
+// ['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']
+// OFF-TOPIC
+
+// Some music to get in the mood for this kata :
+
+// Street Fighter 2 - selection theme
+
+function streetFighterSelection(fighters, position, moves) {
+    let res = []
+    moves.forEach(move => {
+        switch (move) {
+            case 'up':
+                if(position[0] !== 0){
+                    position[0]--
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'down':
+                if(position[0] !== fighters.length-1){
+                    position[0]++
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'left':
+                if(position[1] !== 0){
+                    position[1]--
+                }else{
+                    position[1] = fighters[position[0]].length - 1
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'right':
+                if(position[1] !== fighters[position[0]].length - 1){
+                    position[1]++
+                }else{
+                    position[1] = 0
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            default:
+                break;
+        }
+    })
+
+    return res
+}
+
+//===============================================
+// https://www.codewars.com/kata/58583922c1d5b415b00000ff/javascript
+// This is the rightful continuation to this easier Kata here and some rules are the same with few substantial alterations.
+
+// This time we have to deal with a situation like Super Street Fighter 2 Selection Screen:
+
+// Image
+
+// As you may see, we now have 16 characters on 3 rows. You might think: let's make an array of 3 arrays! But that's not enough.
+
+// Empty space
+// The first character of the first row (Ryu) is not aligned with the first of the second row (Balrog) but with the second (Ken) and the same goes with the other side; therefore we need to introduce something new, like an offset: the Empty Space.
+
+// The empty space, represented as empty string "", will allow us to keep the grid aligned and rectangular, with spaces that won't be selectable. In this case we need 2 empty spaces (3 rows x 6 columns = 18 slots, 18 slots - 16 characters = 2 empty spaces). Like this:
+
+// |        | Ryu    | E.Honda  | Blanka  | Guile   |         |
+// | Balrog | Ken    | Chun Li  | Zangief | Dhalsim | Sagat   |
+// | Vega   | T.Hawk | Fei Long | Deejay  | Cammy   | M.Bison |
+// The moves of the selection cursor are the same as before: rotate horizontally but stop vertically. When you find empty spaces (1 or more) you need to skip them if you approach them horizontally and get to the next selectable slot with the next fighter on the left or right; and if you approach them vertically you need to just stop and stay where you are.
+
+// Example: if you are on Ryu and move left, you must get to Guile; if you are on Balrog and move up, you must stay on Balrog.
+
+// Notice: I might put empty spaces right in the middle and the rectangular grids can be any size, not only 3x6, deal with this too.
+
+// What's new
+// So, let's resume what are the new issues in this harder version of the Kata:
+
+// The initial position might be any non-empty slot in the grid (given as input).
+// The characters grid (also given as input) might have any rectangular layout, not only 3 rows.
+// The grid might contain empty spaces, both on the borders or right in the middle.
+// Input
+// Fighters grid;
+// Initial position;
+// List of moves.
+// The third input parameter is still the list of moves (all valid ones: left, right, up or down).
+
+// Output
+// The output is the same as before: the list of characters that have been hovered by the selection cursor after each move, successful or not.
+
+// Hopefully test cases will complete my explanation.
+
+function superStreetFighterSelection(fighters, position, moves){
+    let res = []
+    moves.forEach(move => {
+        switch (move) {
+            case 'up':
+                if(position[0] !== 0){
+                    position[0]--
+                    if(fighters[position[0]][position[1]] === ''){ //if I move to an empty character, cancel move
+                        position[0]++
+                    }
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'down':
+                if(position[0] !== fighters.length-1){
+                    position[0]++
+                    if(fighters[position[0]][position[1]] === ''){ //if I move to an empty character, cancel move
+                        position[0]--
+                    }
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'left':
+                if(position[1] !== 0){
+                    position[1]--
+                }else{
+                    position[1] = fighters[position[0]].length - 1
+                }
+                while(fighters[position[0]][position[1]] === ''){ //Go left until I stop having empty strings, loop if I reach the left side
+                    if(position[1] !== 0){
+                        position[1]--
+                    }else{
+                        position[1] = fighters[position[0]].length - 1
+                    }
+                }
+
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            case 'right':
+                if(position[1] !== fighters[position[0]].length - 1){
+                    position[1]++
+                }else{
+                    position[1] = 0
+                }
+
+                while(fighters[position[0]][position[1]] === ''){ //Go right until I stop having empty strings, loop if I reach the right side
+                    if(position[1] !== fighters[position[0]].length - 1){
+                        position[1]++
+                    }else{
+                        position[1] = 0
+                    }
+                }
+                res.push(fighters[position[0]][position[1]])
+                break;
+
+            default:
+                break;
+        }
+    })
+
+    return res
+}
+
+//=================================================
+// https://www.codewars.com/kata/520446778469526ec0000001
+// Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+// For example:
+
+// should return true
+// [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );  
+
+// should return false 
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );  
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );  
+
+// should return true
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ); 
+
+// should return false
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );     
+// For your convenience, there is already a function 'isArray(o)' declared and defined that returns true if its argument is an array, false otherwise.
+
+Array.prototype.sameStructureAs = function (other) {
+    if(this.length !== other.length){
+        return false
+    }
+
+    for(let i=0 ; i<this.length ; i++){
+        if(Array.isArray(this[i]) || Array.isArray(other[i])){
+            //If one of the two is an array, check if both are arrays, else is false
+            if(Array.isArray(this[i]) && Array.isArray(other[i])){
+                return this[i].sameStructureAs(other[i])
+            }else{
+                return false
+            }
+        }
+    }
+
+    return true
+};
+
+// console.log([1,[1,1]].sameStructureAs([2,[2,2]])) //true
+// console.log([1,[1,1]].sameStructureAs([[2,2],2])) //false
+// console.log([1,[1,1]].sameStructureAs([2,[2]])) //false
+
+//===========================================
+//Compare deep imbricated Arrays
+function deepCompareImbricatedArrays(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false
+    }
+
+    let res = true
+    for(let i=0 ; i<arr1.length ; i++){
+        if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])){
+            //If both are aArrays, dig deeper
+            res = res && compareDeep(arr1[i], arr2[i])
+        }else{
+            res = res && arr1[i]===arr2[i]
+        }
+    }
+
+    return res
+}
+
+// console.log( compareDeep([[[],[]]], [[[],[]]]) ) //true
+// console.log( compareDeep([1,[1,1]], [1,[1,1]]) ) //true
+// console.log( compareDeep([[1,1],1], [1,[1,1]]) ) //false
+
+//===============================================

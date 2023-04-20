4060ff2d365c065ccb7bf0016fa900f0f227e445
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-21 01:12:47 +0200

    daily push

--- 2023/April/main.js
+++ 2023/April/main.js
@@ -1193,3 +1193,21 @@ function playPass(string, shift) {
 // console.log(playPass("BORN IN 2015!", 1)); // "!4897 Oj oSpC"
 
 //================================================
+// https://www.codewars.com/kata/515f51d438015969f7000013
+// Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+// pyramid(0) => [ ]
+// pyramid(1) => [ [1] ]
+// pyramid(2) => [ [1], [1, 1] ]
+// pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+// Note: the subarrays should be filled with 1s
+
+function pyramid(n) {
+    let res = []
+    for(let i=1 ; i<=n ; i++){
+        res.push(Array(i).fill(1))
+    }
+    return res
+}
+
+//===================================================

e9006d58c88dc5cc053808276c324d0b76ef94f1
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-23 04:55:41 +0200

    daily push

--- 2023/April/main.js
+++ 2023/April/main.js
@@ -1211,3 +1211,38 @@ function pyramid(n) {
 }
 
 //===================================================
+// https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004
+// DESCRIPTION:
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+// Note: no empty arrays will be given.
+
+// Examples
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+function highestFreq(arr){
+    const frequencies = arr.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1
+        return acc
+    }, {})
+
+    let res = [0, []] // [frequency, array of elements of said frequency]
+    for(let key in frequencies){
+        key = Number(key)
+        if(frequencies[key] > res[0]){
+            res = [frequencies[key], [key]]
+        }
+        if(frequencies[key] === res[0]){
+            res = [frequencies[key], [...res[1], key]]
+        }
+    }
+
+    return res[1].sort((a,b) => b-a)[0]
+}
+
+// console.log(highestFreq([12, 10, 8, 12, 7, 6, 4, 10, 12])); //12
+// console.log(highestFreq([11,10,8,11,7,6,4,10,11,10])); //11
+
+//==========================================

22ada0d85e03024f8ca08c8e563c83a8e50c657c
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-27 03:51:18 +0200

    daily push

--- 2023/April/main.js
+++ 2023/April/main.js
@@ -1388,3 +1388,137 @@ function rot13(str) {
 // console.log(rot13("EBG13 rknzcyr.")) //  "ROT13 example."
 
 //==============================================
+// https://www.codewars.com/kata/52c4dd683bfd3b434c000292
+// DESCRIPTION:
+// "7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up, he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
+
+// Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
+
+// It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
+
+// Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+
+// "Interesting" Numbers
+// Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+
+// Any digit followed by all zeros: 100, 90000
+// Every digit is the same number: 1111
+// The digits are sequential, incementing†: 1234
+// The digits are sequential, decrementing‡: 4321
+// The digits are a palindrome: 1221 or 73837
+// The digits match one of the values in the awesomePhrases array
+// † For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+// ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+
+// So, you should expect these inputs and outputs:
+
+// "boring" numbers
+// isInteresting(3, [1337, 256]);    // 0
+// isInteresting(3236, [1337, 256]); // 0
+
+// progress as we near an "interesting" number
+// isInteresting(11207, []); // 0
+// isInteresting(11208, []); // 0
+// isInteresting(11209, []); // 1
+// isInteresting(11210, []); // 1
+// isInteresting(11211, []); // 2
+
+// nearing a provided "awesome phrase"
+// isInteresting(1335, [1337, 256]); // 1
+// isInteresting(1336, [1337, 256]); // 1
+// isInteresting(1337, [1337, 256]); // 2
+
+// Error Checking
+// A number is only interesting if it is greater than 99!
+// But 98 is about to be amazing
+// Input will always be an integer greater than 0, and less than 1,000,000,000.
+// The awesomePhrases array will always be provided, and will always be an array, but may be empty. (Not everyone thinks numbers spell funny words...)
+// You should only ever output 0, 1, or 2.
+
+//Personnal comment: 121 is a palindrome (intersting) as well as a soon to be incrementing number (123). So should the program return 2 (intersting) or 1 (soon to be intersting) ?
+
+function isInteresting(number, awesomePhrases) {
+    //Any number below 98 is not intersting
+    if(number < 98){
+        return 0
+    }
+    //98 and 99 are soon to be awesome
+    if(number === 98 || number === 99){
+        return 1
+    }
+
+    //Check if number is intersting
+    if(isDigitFollowedByZeroes(number) || isEveryDigitTheSame(number) || isSequentialIncrementing(number) || isSequentialDecrementing(number) || isPalindrome(number) || isAwesomePhrase(number)){
+        return 2
+    }
+
+    //Check if a number in the following two miles is intersting
+    for(let i=1 ; i<=2 ; i++){
+        if(isDigitFollowedByZeroes(number+i) || isEveryDigitTheSame(number+i) || isSequentialIncrementing(number+i) || isSequentialDecrementing(number+i) || isPalindrome(number+i) || isAwesomePhrase(number+i)){
+            return 1
+        }
+    }
+
+    //If none of the above returned a value, return 0
+    return 0
+
+    //Check functions declarations
+    function isDigitFollowedByZeroes(number){
+        return Number(number.toString().slice(1)) === 0
+    }
+    // console.log(isDigitFollowedByZeroes(9000));
+    // console.log(isDigitFollowedByZeroes(9001));
+
+    function isEveryDigitTheSame(number){
+        return new Set(number.toString().split('')).size === 1
+    }
+    // console.log(isEveryDigitTheSame(9999));
+    // console.log(isEveryDigitTheSame(9998));
+
+    function isSequentialIncrementing(number){
+        return number.toString().split('').every((el, idx, arr) => {
+            if(idx===0) return true
+            if(el==='0'){
+                //0 should come after 9 condition
+                el = 10
+            }
+            return Number(arr[idx-1]) === Number(el)-1
+        })
+    }
+    // console.log(isSequentialIncrementing(1234));
+    // console.log(isSequentialIncrementing(1235));
+    // console.log(isSequentialIncrementing(67890)); //true
+
+    function isSequentialDecrementing(number){
+        return number.toString().split('').every((el, idx, arr) => {
+            if(idx===0) return true
+            return Number(arr[idx-1]) === Number(el)+1
+        })
+    }
+    // console.log(isSequentialDecrementing(5432));
+    // console.log(isSequentialDecrementing(5431));
+
+    function isPalindrome(number){
+        let string = number.toString()
+        let isEvenLength = string.length%2 === 0
+        let left = string.slice(0, Math.floor(string.length/2))
+        let right = string.slice(isEvenLength ? Math.floor(string.length/2) : Math.floor(string.length/2)+1)
+        return left === right.split('').reverse().join('')
+    }
+    // console.log(isPalindrome(12321));
+    // console.log(isPalindrome(123321));
+    // console.log(isPalindrome(123323));
+
+    function isAwesomePhrase(number){
+        return awesomePhrases.includes(number)
+    }
+}
+
+// console.log(isInteresting(1336, [1337, 256])); // 1
+// console.log(isInteresting(1337, [1337, 256])); // 2
+// console.log(isInteresting(11208, [1337, 256])); // 0
+// console.log(isInteresting(11209, [1337, 256])); // 1
+// console.log(isInteresting(11211, [1337, 256])); // 2
+// console.log(isInteresting(67890, [])); // 2
+
+//===========================================================

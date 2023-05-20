ee1df9781072f3a9f05fcaf6868cf1a893fa0a73
Olivier Reynard <olivier.reynard17@gmail.com>
2023-05-20 21:07:22 +0200

    daily push

--- 2023/May/main.js
+++ 2023/May/main.js
@@ -603,3 +603,71 @@ function kebabize(str) {
 // console.log(kebabize("CAMEL")); //c-a-m-e-l
 
 //===========================================
+// https://www.codewars.com/kata/586538146b56991861000293
+// Task
+// You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+// https://en.wikipedia.org/wiki/NATO_phonetic_alphabet
+
+// Input:
+// If, you can read?
+
+// Output:
+// India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+// Note:
+// There are preloaded dictionary you can use, named NATO
+// The set of used punctuation is ,.!?.
+// Punctuation should be kept in your return string, but spaces should not.
+// Xray should not have a dash within.
+// Every word and punctuation mark should be seperated by a space ' '.
+// There should be no trailing whitespace
+
+function to_nato(words) {
+    const NATO = {
+        a: 'Alfa',
+        n: 'November',
+        b: 'Bravo',
+        o: 'Oscar',
+        c: 'Charlie',
+        p: 'Papa',
+        d: 'Delta',
+        q: 'Quebec',
+        e: 'Echo',
+        r: 'Romeo',
+        f: 'Foxtrot',
+        s: 'Sierra',
+        g: 'Golf',
+        t: 'Tango',
+        h: 'Hotel',
+        u: 'Uniform',
+        i: 'India',
+        v: 'Victor',
+        j: 'Juliett',
+        w: 'Whiskey',
+        k: 'Kilo',
+        x: 'Xray',
+        l: 'Lima',
+        y: 'Yankee',
+        m: 'Mike',
+        z: 'Zulu'
+    }
+	const punctuation = ',.!?'
+    let res =''
+    for(let i=0 ; i<words.length ; i++){
+        if(words[i] === ' '){
+            continue
+        }else if(punctuation.includes(words[i])){
+            res += words[i] + ' '
+        }else{
+            res += NATO[words[i].toLowerCase()] + ' '
+        }
+    }
+
+    //remove last space
+    res = res.slice(0, res.length-1)
+    return res
+}
+
+// console.log(to_nato("If, you can read?"));
+
+//==========================================

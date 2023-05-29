928a9f2640e23912cdc2eae6fd2561cae3d232cb
Olivier Reynard <olivier.reynard17@gmail.com>
2023-05-29 06:12:53 +0200

    daily push

--- 2023/May/main.js
+++ 2023/May/main.js
@@ -802,3 +802,41 @@ function pascalsTriangleBis(n) {
 // console.log(pascalsTriangleBis(4)) // [ 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 ]
 
 //==================================================
+// https://www.codewars.com/kata/586d6cefbcc21eed7a001155
+// For a given string s find the character c (or C) with longest consecutive repetition and return:
+
+// [c, l]
+// where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+
+// For empty string return:
+
+// ["", 0]
+// Happy coding! :)
+
+function longestRepetition(s) {
+    if(!s) return ["", 0]
+
+    let char = ''
+    let len = 0
+    for(let i=0 ; i<s.length ; i++){
+        let tempC = s[i]
+        let tempL = 0
+        let j = i
+        while(s[j] === tempC){
+            tempL++
+            j++
+        }
+        if(tempL > len){
+            char = tempC
+            len = tempL
+        }
+        i = j-1
+    }
+
+    return [char, len]
+}
+
+// console.log(longestRepetition("bbbaaabaaaa"))//[ 'a', 4 ]
+// console.log(longestRepetition("abbbbb"))//[ 'b', 5 ]
+
+//=======================================

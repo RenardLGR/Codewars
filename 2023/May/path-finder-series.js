6325f10d73102ba2e1d5b5b374f6033f1c729052
Olivier Reynard <olivier.reynard17@gmail.com>
2023-06-13 04:03:38 +0200

    daily push

--- 2023/May/path-finder-series.js
+++ 2023/May/path-finder-series.js
@@ -285,19 +285,95 @@ function pathFinderShorterPath(maze) {
 
 // Location altitude is defined as an integer number (0-9).
 
-function pathFinder(area){
-    //We will only go east our south and choose the closest altitude between the two
-    let max = area.length - 1
-    let climbRounds = 0
-    let currAltitude = area[0][0]
-    let position = [0, 0]
+function pathFinderAltitudes(area){
+    //Dijkstra's algorithm, from now on we will be using the term distance as a way to represent the sum of altitudes differences from the starting point to another point
+    //We will also call nodes each locations
 
+    //readable 2D array
+    let grid = area.split('\n').map(line => line.split(''))
+    const N = grid.length;
+    const distances = Array.from({length: N}, () => Array(N).fill(Infinity)) //keeps track of the minimum distances from the start location to each location in the grid. Initially, all distances are set to infinity except for the start location, which is set to 0.
+    const visited = Array.from({length: N}, () => Array(N).fill(false))
 
-    function move(line, col){
-        //if I can choose between going east or going south, choose the closest altitude, go east if there are equal
-        if(line === max){
-            //can't go south exception
-            
+    distances[0][0] = 0 //initialize starting point at [0, 0]
+
+    while(true){
+        let minDistance = Infinity
+        let currentNode = null
+
+        // Find the unvisited node with the smallest distance
+        for(let line=0 ; line<N ; line++){
+            for(let col=0 ; col<N ;col++){
+                if(!visited[line][col] && distances[line][col]<minDistance){
+                    minDistance = distances[line][col]
+                    currentNode = [line, col]
+                }
+            }
         }
+
+        if(currentNode === null){
+            //no more nodes to visit
+            break
+        }
+
+        const [curL, curC] = currentNode
+        visited[curL][curC] = true
+        
+        // Update distances of neighbors, knowing our current distance from the start
+        const neighbors = getNeighbors(curL, curC);
+        neighbors.forEach(([nL, nC]) => {
+            const altitudeDifference = Math.abs(grid[curL][curC] - grid[nL][nC])
+            const newDistance = distances[curL][curC] + altitudeDifference
+            //Update distances if needed
+            if(newDistance < distances[nL][nC]){
+                distances[nL][nC] = newDistance
+            }
+        })
+    }
+
+    return distances[N - 1][N - 1]
+
+    //check if I don't hop out of the grid
+    function isValid(line, col){
+        return line < N && line >= 0 && col < N && col >= 0
+    }
+
+    //get up to 4 neighbors coordinates
+    function getNeighbors(line, col){
+        const cardinalDirections = [ [-1, 0] , [0, 1] , [1, 0] , [0, -1] ] //North, East, South, West
+        const neighbors = []
+        cardinalDirections.forEach(([dL, dC]) => {
+            const newL = line + dL
+            const newC = col + dC
+
+            if(isValid(newL, newC)){
+                neighbors.push([newL, newC])
+            }
+        })
+
+        return neighbors
     }
 }
+
+// console.log(pathFinderAltitudes(`700000
+// 077770
+// 077770
+// 077770
+// 077770
+// 000007`)) // 14
+
+// console.log(pathFinderAltitudes(`777000
+// 007000
+// 007000
+// 007000
+// 007000
+// 007777`)) // 0
+
+// console.log(pathFinderAltitudes(`000000
+// 000000
+// 000000
+// 000010
+// 000109
+// 001010`)) // 4
+
+//=============================================================

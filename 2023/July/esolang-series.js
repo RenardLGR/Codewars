7fb9b6ed0dc325f5eaaa94b4359f392d772f3a34
Olivier Reynard <olivier.reynard17@gmail.com>
2023-07-16 14:40:00 +0200

    daily push

--- 2023/July/esolang-series.js
+++ 2023/July/esolang-series.js
@@ -83,3 +83,120 @@ function myFirstInterpreter(code) {
 // console.log(myFirstInterpreter('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.')); //ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 //=================================================
+// http://codewars.com/kata/esolang-interpreters-number-2-custom-smallfuck-interpreter
+// Esolang Interpreters #2 - Custom Smallfuck Interpreter
+
+// The Language
+// Smallfuck is an esoteric programming language/Esolang invented in 2002 which is a sized-down variant of the famous Brainfuck Esolang. Key differences include:
+
+// Smallfuck operates only on bits as opposed to bytes
+// It has a limited data storage which varies from implementation to implementation depending on the size of the tape
+// It does not define input or output - the "input" is encoded in the initial state of the data storage (tape) and the "output" should be decoded in the final state of the data storage (tape)
+// Here are a list of commands in Smallfuck:
+
+// > - Move pointer to the right (by 1 cell)
+// < - Move pointer to the left (by 1 cell)
+// * - Flip the bit at the current cell
+// [ - Jump past matching ] if value at current cell is 0
+// ] - Jump back to matching [ (if value at current cell is nonzero)
+// As opposed to Brainfuck where a program terminates only when all of the commands in the program have been considered (left to right), Smallfuck terminates when any of the two conditions mentioned below become true:
+
+// All commands have been considered from left to right
+// The pointer goes out-of-bounds (i.e. if it moves to the left of the first cell or to the right of the last cell of the tape)
+// Smallfuck is considered to be Turing-complete if and only if it had a tape of infinite length; however, since the length of the tape is always defined as finite (as the interpreter cannot return a tape of infinite length), its computational class is of bounded-storage machines with bounded input.
+
+// More information on this Esolang can be found here.
+// http://esolangs.org/wiki/Smallfuck
+
+// The Task
+// Implement a custom Smallfuck interpreter interpreter() (interpreter in Haskell and F#, Interpreter in C#, custom_small_fuck:interpreter/2 in Erlang) which accepts the following arguments:
+
+// code - Required. The Smallfuck program to be executed, passed in as a string. May contain non-command characters. Your interpreter should simply ignore any non-command characters.
+// tape - Required. The initial state of the data storage (tape), passed in as a string. For example, if the string "00101100" is passed in then it should translate to something of this form within your interpreter: [0, 0, 1, 0, 1, 1, 0, 0]. You may assume that all input strings for tape will be non-empty and will only contain "0"s and "1"s.
+// Your interpreter should return the final state of the data storage (tape) as a string in the same format that it was passed in. For example, if the tape in your interpreter ends up being [1, 1, 1, 1, 1] then return the string "11111".
+
+// NOTE: The pointer of the interpreter always starts from the first (leftmost) cell of the tape, same as in Brainfuck.
+
+// Good luck :D
+
+// Note:
+// Seeing a few questions on the bracket cmds, I thought I might try to offer (a rather verbose) explanation of these commands. Nomenclature: The string of commands (e.g. '>') I call 'code-tape' and the string of 0's and 1's (e.g. '10010') I call data-tape. Additionally, 'code pointer' points to the current cmd to execute
+
+// Cmd "Left Bracket" (i.e.'['):
+// "Left Bracket" command executes if the value of current cell on the data-tape is a '0' (otherwise move onto the next command)
+// Move to the right in the code-tape till matching right bracket is found; once found, consume the right bracket token (hence we jump to the code-cell directly to the right of the matching right bracket)
+// Cmd "Right Bracket" (i.e.']'):
+// "Right Bracket" command executes if the value of current cell on the data-tape is not '0' (otherwise, just move onto the next command)
+// Move to the left in the code tape till matching left-bracket is found. Once matching left bracket is found, stay at the bracket cmd on the command tape (i.e. don't consume it like the other bracket cmd)
+
+// Lastly, your code needs to be able to manage nested brackets
+
+function smallfuckInterpreter(code, tape) {
+    tape = tape.split('')
+    let codePointer = 0
+    let tapePointer = 0
+    let isDone = false
+    while (!isDone) {
+        let instruction = code[codePointer]
+        switch (instruction) {
+            case ">":
+                tapePointer++
+                if(tapePointer >= tape.length) isDone = true
+                break
+
+            case "<":
+                tapePointer--
+                if(tapePointer < 0) isDone = true
+                break
+
+            case "*":
+                tape[tapePointer] = (tape[tapePointer] === '0') ? '1' : '0'
+                break
+
+            case "[":
+                if (tape[tapePointer] === '0') {
+                    let opening = 1
+                    let closing = 0
+                    while (opening !== closing) {
+                        codePointer++
+                        if (code[codePointer] === "[") opening++
+                        if (code[codePointer] === "]") closing++
+                        if (code[codePointer] === undefined) {
+                            isDone = true
+                            break
+                        }
+                    }
+                }
+                break
+
+            case "]":
+                if (tape[tapePointer] !== '0') {
+                    let opening = 0
+                    let closing = 1
+                    while (opening !== closing) {
+                        codePointer--
+                        if (code[codePointer] === "[") opening++
+                        if (code[codePointer] === "]") closing++
+                        if (code[codePointer] === undefined) {
+                            isDone = true
+                            break
+                        }
+                    }
+                }
+                break
+
+            case undefined:
+                isDone = true
+                break
+
+            default:
+                break
+        }
+        codePointer++
+    }
+    return tape.join('')
+}
+
+// console.log(smallfuckInterpreter("*>*>*>*>*>*>*>*", "00101100")); //11010011
+// console.log(smallfuckInterpreter("*>>>*>*>>*>>>>>>>*>*>*>*>>>**>>**", "0000000000000000")); //11010011
+// console.log(smallfuckInterpreter("*>[[]*>]<*", "100")); //100

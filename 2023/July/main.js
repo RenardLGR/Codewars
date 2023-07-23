622cca64418439b33d7bdb8ceb61acdb29273eb2
Olivier Reynard <olivier.reynard17@gmail.com>
2023-07-23 06:56:21 +0200

    daily push

--- 2023/July/main.js
+++ 2023/July/main.js
@@ -570,13 +570,46 @@ function fibMatrixExponentiationFastV2(n){
     }
 }
 
-console.log(fibMatrixExponentiationFastV2(0)); // 0n
-console.log(fibMatrixExponentiationFastV2(1)); // 1n
-console.log(fibMatrixExponentiationFastV2(-13n)); // 233n
-console.log(fibMatrixExponentiationFastV2(40n)); // 102334155n
+// console.log(fibMatrixExponentiationFastV2(0)); // 0n
+// console.log(fibMatrixExponentiationFastV2(1)); // 1n
+// console.log(fibMatrixExponentiationFastV2(-13n)); // 233n
+// console.log(fibMatrixExponentiationFastV2(40n)); // 102334155n
 
 //Kata done
 
 // Using fast doubling identities
 // f(2n) = f(n)[2f(n+1) - f(N)] //even numbers
 // f(2n+1) = f(n+1)^2 + f(n)^2 //odd numbers
+
+
+//================================================
+// https://www.codewars.com/kata/58235a167a8cb37e1a0000db
+// Pair of gloves
+// Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+// Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+// Examples:
+// input = ["red", "green", "red", "blue", "blue"]
+// result = 2 (1 red pair + 1 blue pair)
+
+// input = ["red", "red", "red", "red", "red", "red"]
+// result = 3 (3 red pairs)
+
+function numberOfPairs(gloves){
+    let freq = gloves.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1
+        return acc
+    }, {})
+
+    let res = 0
+    for(let color in freq){
+        res += Math.floor(freq[color]/2)
+    }
+
+    return res
+}
+
+// console.log(numberOfPairs(["red", "green", "red", "blue", "blue"])) // 2
+
+//=================================================

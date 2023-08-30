6f04d4557fb71104903037cc3193b8309398192c
Olivier Reynard <olivier.reynard17@gmail.com>
2023-08-31 01:18:05 +0200

    daily push

--- 2023/August/main.js
+++ 2023/August/main.js
@@ -1295,3 +1295,89 @@ function treeByLevelsTer(rootNode){
     }
     return res
 }
+
+//======================================================
+// https://www.codewars.com/kata/546d15cebed2e10334000ed9/train/javascript
+// To give credit where credit is due: This problem was taken from the ACMICPC-Northwest Regional Programming Contest. Thank you problem writers.
+
+// You are helping an archaeologist decipher some runes. He knows that this ancient society used a Base 10 system, and that they never start a number with a leading zero. He's figured out most of the digits as well as a few operators, but he needs your help to figure out the rest.
+
+// The professor will give you a simple math expression, of the form
+
+// [number][op][number]=[number]
+// He has converted all of the runes he knows into digits. The only operators he knows are addition (+),subtraction(-), and multiplication (*), so those are the only ones that will appear. Each number will be in the range from -1000000 to 1000000, and will consist of only the digits 0-9, possibly a leading -, and maybe a few ?s. If there are ?s in an expression, they represent a digit rune that the professor doesn't know (never an operator, and never a leading -). All of the ?s in an expression will represent the same digit (0-9), and it won't be one of the other given digits in the expression. No number will begin with a 0 unless the number itself is 0, therefore 00 would not be a valid number.
+
+// Given an expression, figure out the value of the rune represented by the question mark. If more than one digit works, give the lowest one. If no digit works, well, that's bad news for the professor - it means that he's got some of his runes wrong. output -1 in that case.
+
+// Complete the method to solve the expression to find the value of the unknown rune. The method takes a string as a paramater repressenting the expression and will return an int value representing the unknown rune or -1 if no such rune exists.
+
+function solveExpression(exp){
+    // a '?' can only be an undiscovered number
+    let unseen = []
+    for(let i=0 ; i<=9 ; i++){
+        if(!exp.includes(''+i)) unseen.push(i)
+    }
+    const regex = /([-]?[0-9?]+)\s*([-+*])\s*([-]?[0-9?]+)\s*=\s*([-]?[0-9?]+)/;
+    // [-]?[0-9?]+ captures a sequence that can start with an optional negative sign, followed by one or more digits or "?".
+    // ([-+*]) captures the operator (+, -, or *).
+    // \s* matches any number of whitespace characters (spaces or tabs).
+    const match = exp.match(regex)
+    // console.log(match);
+
+    const operand1 = match[1]
+    const operator = match[2]
+    const operand2 = match[3]
+    const result = match[4]
+
+    // console.log(operand1, operator, operand2, result);
+
+    for(let uns of unseen){
+        //No number will begin with a 0 unless the number itself is 0
+        if(uns===0 && (!isZeroOK(operand1, operand2, result)) ) continue
+
+        let newOperand1 = operand1.replaceAll('?', uns)
+        let newOperand2 = operand2.replaceAll('?', uns)
+        let newResult = result.replaceAll('?', uns)
+        let left
+        switch (operator) {
+            case '+':
+                left = parseInt(newOperand1) + parseInt(newOperand2)
+                break;
+
+            case '-':
+                left = parseInt(newOperand1) - parseInt(newOperand2)
+                break;
+
+            case '*':
+                left = parseInt(newOperand1) * parseInt(newOperand2)
+                break;
+        
+            default:
+                break;
+        }
+
+        if(left === parseInt(newResult)) return uns
+    }
+
+    return -1
+
+    function isZeroOK(op1, op2, res){
+        op1 = op1[0] === '-' ? op1.slice(1) : op1
+        op2 = op2[0] === '-' ? op2.slice(1) : op2
+        res = res[0] === '-' ? res.slice(1) : res
+
+        return ( (op1[0]!=='?'||op1.length===1) && (op2[0]!=='?'||op2.length===1) && (res[0]!=='?'||res.length===1))
+    }
+}
+
+console.log(solveExpression('1+1=?')) //2
+console.log(solveExpression('123*45?=5?088')) //6
+console.log(solveExpression('-5?*-1=5?')) //0
+console.log(solveExpression('19--45=5?',))//-1
+console.log(solveExpression('??*??=302?')) //5
+console.log(solveExpression('?*11=??')) //2
+console.log(solveExpression('??*1=??')) //2
+console.log(solveExpression('??+??=??')) //-1
+console.log(solveExpression('123?45+?=123?45')) //0
+console.log(solveExpression('-?56373--9216=-?47157')) //8
+console.log(solveExpression('-56*?=?')) //0

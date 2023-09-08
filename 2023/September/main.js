086ab6f8ae9087e3d61894186d6da442ebf5feb2
Olivier Reynard <olivier.reynard17@gmail.com>
2023-09-08 05:03:16 +0200

    daily push

--- 2023/September/main.js
+++ 2023/September/main.js
@@ -125,3 +125,31 @@ function distributionOfBis(golds){
 }
 
 // console.log(distributionOfBis([4,2,9,5,2,7])); // [ 14, 15 ]
+
+//===============================================
+// https://www.codewars.com/kata/57f625992f4d53c24200070e
+// Time to win the lottery!
+
+// Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+// Example ticket:
+
+// [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+// To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+// Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+// All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+function bingo(tickets, win){
+    let miniWins = 0
+    tickets.forEach(([s, n]) => {
+        s.split('').forEach(char => miniWins += char.charCodeAt(0) === n ? 1 : 0)
+    })
+
+    return miniWins >= win ? 'Winner!' : 'Loser!'
+}
+
+// console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2)) // 'Loser!'
+
+//===================================================

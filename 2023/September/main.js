5baff25b00e3142b34accd6ee6d48cd11ac960b0
Olivier Reynard <olivier.reynard17@gmail.com>
2023-09-10 05:55:54 +0200

    daily push

--- 2023/September/main.js
+++ 2023/September/main.js
@@ -227,3 +227,50 @@ function groupByCommasBis(n) {
 // console.log(groupByCommasBis(100000)) // "100,000"
 
 //=================================================================
+// https://www.codewars.com/kata/5539fecef69c483c5a000015
+// Backwards Read Primes are primes that when read backwards in base 10 (from right to left) are a different prime. (This rules out primes which are palindromes.)
+
+// Examples:
+// 13 17 31 37 71 73 are Backwards Read Primes
+// 13 is such because it's prime and read from right to left writes 31 which is prime too. Same for the others.
+
+// Task
+// Find all Backwards Read Primes between two positive given numbers (both inclusive), the second one always being greater than or equal to the first one. The resulting array or the resulting string will be ordered following the natural order of the prime numbers.
+
+// Examples (in general form):
+// backwardsPrime(2, 100) => [13, 17, 31, 37, 71, 73, 79, 97] backwardsPrime(9900, 10000) => [9923, 9931, 9941, 9967] backwardsPrime(501, 599) => []
+
+// See "Sample Tests" for your language.
+
+// Notes
+// Forth Return only the first backwards-read prime between start and end or 0 if you don't find any
+// Ruby Don't use Ruby Prime class, it's disabled.
+
+function backwardsPrime(start, stop){
+    let res = []
+
+    for(let i=start ; i<=stop ; i++){
+        if(isPrime(i)){
+            let backwards = Number((''+i).split('').reverse().join(''))
+            if(isPrime(backwards) && i!==backwards){
+                res.push(i)
+            }
+        }
+    }
+
+    return res
+
+    function isPrime(num){
+        if(num <= 1) return false
+
+        for(let i=2 ; i<=Math.sqrt(num) ; i++){
+            if(num % i === 0) return false
+        }
+        return true
+    }
+}
+
+// console.log(backwardsPrime(2, 100)) // [13, 17, 31, 37, 71, 73, 79, 97]
+// console.log(backwardsPrime(109537, 109663)) // [ 109537, 109579, 109583, 109609, 109663 ]
+
+//=======================================================

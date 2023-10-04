f04459c3df378339758b2b2fbad0b4fb731c430a
Olivier Reynard <olivier.reynard17@gmail.com>
2023-10-05 00:11:32 +0200

    daily push

--- 2023/October/main.js
+++ 2023/October/main.js
@@ -133,3 +133,141 @@ function allContinentsBis(list){
 }
 
 //==============================================
+// https://www.codewars.com/kata/540afbe2dc9f615d5e000425
+// Given a Sudoku data structure with size NxN, N > 0 and √N == integer, write a method to validate if it has been filled out correctly.
+
+// The data structure is a multi-dimensional Array, i.e:
+
+// [
+//   [7,8,4,  1,5,9,  3,2,6],
+//   [5,3,9,  6,7,2,  8,4,1],
+//   [6,1,2,  4,3,8,  7,5,9],
+  
+//   [9,2,8,  7,1,5,  4,6,3],
+//   [3,5,7,  8,4,6,  1,9,2],
+//   [4,6,1,  9,2,3,  5,8,7],
+  
+//   [8,7,6,  3,9,4,  2,1,5],
+//   [2,4,3,  5,6,1,  9,7,8],
+//   [1,9,5,  2,8,7,  6,3,4]
+// ]
+// Rules for validation
+
+// Data structure dimension: NxN where N > 0 and √N == integer
+// Rows may only contain integers: 1..N (N included)
+// Columns may only contain integers: 1..N (N included)
+// 'Little squares' (3x3 in example above) may also only contain integers: 1..N (N included)
+
+class Sudoku {
+    constructor(grid){
+        this.grid = grid
+    }
+
+    isDimensionNxN(){
+        // Data structure dimension: NxN where N > 0 and √N == integer
+        let height = this.grid.length
+        if(!this.grid.every(row => row.length === height)) return false
+        if(!Number.isInteger(Math.sqrt(height))) return false
+        return true
+    }
+
+    isRowCorrect(){
+        for(let r of this.grid){
+            let flags = Array(this.grid.length).fill(false)
+            r.forEach(el => flags[el-1] = true)
+            if(flags.some(el => !el)) return false
+        }
+        return true
+    }
+
+    isColCorrect(){
+        for(let c=0 ; c<this.grid.length ; c++){
+            let flags = Array(this.grid.length).fill(false)
+            for(let r=0 ; r<this.grid.length ; r++){
+                if(!Number.isInteger(this.grid[r][c])) return false //the only place I check if every element is actually a Number
+                flags[this.grid[r][c]-1] = true
+            }
+            if(flags.some(el => !el)) return false
+        }
+        return true
+    }
+
+    isBoxCorrect(){
+        const sqrt = Math.sqrt(this.grid.length)
+        for(let r=0 ; r<this.grid.length ; r+=sqrt){
+            //For row, leftmost box is n=0, on its right we have box n=1, etc.
+            for(let n=0 ; n<sqrt ; n++){
+                let flags = Array(this.grid.length).fill(false)
+                for(let tr=r ; tr<r+sqrt ; tr++){
+                    for(let tc=n*sqrt ; tc<n*sqrt+sqrt ; tc++){
+                        flags[this.grid[tr][tc]-1] = true
+                    }
+                }
+                if(flags.some(el => !el)) return false
+            }
+        }
+        return true
+    }
+
+    isValid(){
+        return this.isDimensionNxN() && this.isRowCorrect() && this.isColCorrect() && this.isBoxCorrect()
+    }
+}
+
+const goodSudoku1 = new Sudoku([
+  [7, 8, 4, 1, 5, 9, 3, 2, 6],
+  [5, 3, 9, 6, 7, 2, 8, 4, 1],
+  [6, 1, 2, 4, 3, 8, 7, 5, 9],
+
+  [9, 2, 8, 7, 1, 5, 4, 6, 3],
+  [3, 5, 7, 8, 4, 6, 1, 9, 2],
+  [4, 6, 1, 9, 2, 3, 5, 8, 7],
+
+  [8, 7, 6, 3, 9, 4, 2, 1, 5],
+  [2, 4, 3, 5, 6, 1, 9, 7, 8],
+  [1, 9, 5, 2, 8, 7, 6, 3, 4],
+])
+
+const goodSudoku2 = new Sudoku([
+  [1, 4, 2, 3],
+  [3, 2, 4, 1],
+
+  [4, 1, 3, 2],
+  [2, 3, 1, 4],
+])
+
+const badSudoku1 = new Sudoku([
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+])
+
+const badSudoku2 = new Sudoku([[1, 2, 3, 4, 5], [1, 2, 3, 4], [1, 2, 3, 4], [1]])
+
+const badSudoku3 = new Sudoku([
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [2, 3, 1, 5, 6, 4, 8, 9, 7],
+  [3, 1, 2, 6, 4, 5, 9, 7, 8],
+  [4, 5, 6, 7, 8, 9, 1, 2, 3],
+  [5, 6, 4, 8, 9, 7, 2, 3, 1],
+  [6, 4, 5, 9, 7, 8, 3, 1, 2],
+  [7, 8, 9, 1, 2, 3, 4, 5, 6],
+  [8, 9, 7, 2, 3, 1, 5, 6, 4],
+  [9, 7, 8, 3, 1, 2, 6, 4, 5],
+])
+
+// console.log(goodSudoku1.isValid()) // true
+// console.log(goodSudoku2.isValid()) // true
+// console.log(badSudoku1.isValid()) // false
+// console.log(badSudoku2.isValid()) // false
+// console.log(badSudoku3.isValid()) // false - little box of size sqrt(N) is false
+
+//===============================================

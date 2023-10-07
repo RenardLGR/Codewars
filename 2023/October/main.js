4514ab15e22f43661c46df4cdbc87048d5f484b6
Olivier Reynard <olivier.reynard17@gmail.com>
2023-10-07 02:19:15 +0200

    daily push

--- 2023/October/main.js
+++ 2023/October/main.js
@@ -271,3 +271,27 @@ const badSudoku3 = new Sudoku([
 // console.log(badSudoku3.isValid()) // false - little box of size sqrt(N) is false
 
 //===============================================
+// https://www.codewars.com/kata/596f72bbe7cd7296d1000029
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+// Examples
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+// The input will always be an array.
+
+function deepCount(a){
+    return a.reduce((acc, cur) => {
+        if(Array.isArray(cur)){
+            return acc + 1 + deepCount(cur)
+        }else{
+            return acc + 1
+        }
+    }, 0)
+}
+
+console.log(deepCount([])) // 0
+console.log(deepCount([1, 2, 3])) // 3
+console.log(deepCount(["x", "y", ["z"]])) // 4
+console.log(deepCount([1, 2, [3, 4, [5]]])) // 7

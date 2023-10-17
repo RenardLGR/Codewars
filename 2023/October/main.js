7c7b65d61102d6f944698206df142b1f33a03222
Olivier Reynard <olivier.reynard17@gmail.com>
2023-10-17 18:17:29 +0200

    daily push

--- 2023/October/main.js
+++ 2023/October/main.js
@@ -983,5 +983,45 @@ function adjacentElementsProduct(array) {
         if(idx===0) return
         if(arr[idx-1] * el > max) max = arr[idx-1] * el
     })
-    return amx
+    return max
+}
+
+//==========================================
+// https://www.codewars.com/kata/5300901726d12b80e8000498
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+function fizzbuzz(n){
+    let res = []
+    for(let i=1 ; i<=n ; i++){
+        if(i%3===0 && i%5===0) res.push("FizzBuzz")
+        else if(i%3===0) res.push("Fizz")
+        else if(i%5===0) res.push("Buzz")
+        else res.push(i)
+    }
+    return res
+}
+
+
+//================================
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+function greet(name){
+    return 'Hello ' + name.toUpperCase()[0] + name.toLowerCase().slice(1) + '!'
 }

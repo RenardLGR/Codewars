e45fef03d9f7c1655a74b25968b4222a37b1ab82
Olivier Reynard <olivier.reynard17@gmail.com>
2023-10-24 23:53:52 +0200

    daily push

--- 2023/October/main.js
+++ 2023/October/main.js
@@ -1347,3 +1347,53 @@ var SequenceSum = (function() {
 })() //calls (or instantiates) said class, stores it under the value SequenceSum
 
 // console.log(SequenceSum.showSequence(6)) // "0+1+2+3+4+5+6 = 21"
+
+//========================================
+// https://www.codewars.com/kata/58712dfa5c538b6fc7000569
+// Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+// BRRBRRBRRBRRB
+
+// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+
+
+function countRedBeads(n) {
+    if(n === 0 || n === 1) return 0
+    return (n-1) * 2
+    // return n < 2 ? 0 : (n-1) * 2
+}
+
+//===========================================
+// https://www.codewars.com/kata/525f039017c7cd0e1a000a26
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes, and 43, 194, 4773 are not.
+
+// Write a function which takes a positive integer and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+
+// If the input number is already a palindrome, the number of steps is 0.
+
+// All inputs are guaranteed to have a final palindrome which does not overflow MAX_SAFE_INTEGER.
+
+// Example
+// For example, start with 87:
+
+//   87 +   78 =  165     - step 1, not a palindrome
+//  165 +  561 =  726     - step 2, not a palindrome
+//  726 +  627 = 1353     - step 3, not a palindrome
+// 1353 + 3531 = 4884     - step 4, palindrome!
+// 4884 is a palindrome and we needed 4 steps to obtain it, so answer for 87 is 4.
+
+// Additional info
+// Some interesting information on the problem can be found in this Wikipedia article on Lychrel numbers.
+
+var palindromeChainLength = function(n) {
+    let steps = 0
+    while(''+n !== (''+n).split('').reverse().join('')){
+        n += Number((''+n).split('').reverse().join(''))
+        steps++
+    }
+    return steps
+}
+
+// console.log(palindromeChainLength(87)) // 4
+

83fb38c0a8ab5471a32a85e4a9a7368fb2aabefd
Olivier Reynard <olivier.reynard17@gmail.com>
2023-10-26 13:56:55 +0200

    daily push

--- 2023/October/main.js
+++ 2023/October/main.js
@@ -1470,3 +1470,48 @@ function removeFirstDuplicatesBis(arr){
 // console.log(removeFirstDuplicatesBis([3, 4, 4, 3, 6, 3])) // [4, 6, 3]
 
 //======================================================
+// https://www.codewars.com/kata/55b051fac50a3292a9000025
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+var filterString = function(value) {
+    return +value.split("").filter(e => e == +e).join("")
+}
+
+// console.log(filterString("aa1bb2cc3dd")) // 123
+
+//======================================================
+// https://www.codewars.com/kata/5f0ed36164f2bc00283aed07
+// Task
+// You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
+
+// Street
+// 1|   |6
+// 3|   |4
+// 5|   |2
+//   you
+// Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
+
+// Example (address, n --> output)
+// Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+// 1, 3 --> 6
+// 3, 3 --> 4
+// 2, 3 --> 5
+// 3, 5 --> 8
+// Note about errors
+// If you are timing out, running out of memory, or get any kind of "error", read on. Both n and address could get upto 500 billion with over 200 random tests. If you try to store the addresses of 500 billion houses in a list then you will run out of memory and the tests will crash. This is not a kata problem so please don't post an issue. Similarly if the tests don't complete within 12 seconds then you also fail.
+
+// To solve this, you need to think of a way to do the kata without making massive lists or huge for loops. Read the discourse for some inspiration :)
+
+function overTheRoad(address, n){
+    //Not very clear, but basically, I am looking for the index of the address, and from that index, finding the value at the same index but opposite side of the street
+    return address%2 === 0 ? n*2 - 1 - (Math.floor(address/2) - 1) * 2 : (Math.floor((n*2 - address)/2) + 1 ) * 2
+}
+

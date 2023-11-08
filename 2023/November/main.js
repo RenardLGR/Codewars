2b25036d509ee8d4e01d5aa9ea662d9205003c68
Olivier Reynard <olivier.reynard17@gmail.com>
2023-11-08 17:56:36 +0100

    daily push

--- 2023/November/main.js
+++ 2023/November/main.js
@@ -239,3 +239,24 @@ function averages(numbers){
 
     return res
 }
+
+//=======================================
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+
+// Here's a nice Youtube video about currying, which might help you if this is new to you.
+// https://www.youtube.com/watch?v=iZLP4qOwY8I
+
+function multiplyAll(arr){
+    return (factor) => arr.map(el => el*factor)
+}
+
+// console.log(multiplyAll([1, 2, 3])(2)) // [2, 4, 6]
+

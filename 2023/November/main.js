7692549f46c8312e83be1cab0366d82882e01cf5
Olivier Reynard <olivier.reynard17@gmail.com>
2023-11-17 21:53:10 +0100

    daily push

--- 2023/November/main.js
+++ 2023/November/main.js
@@ -490,3 +490,35 @@ function foldTo(distance) {
 
     return folds
 }
+
+//============================================
+// https://www.codewars.com/kata/5a092d9e46d843b9db000064
+// In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+// Examples:
+
+// [1, -1, 2, -2, 3] => 3
+
+// 3 has no matching negative appearance
+
+// [-3, 1, 2, 3, -1, -4, -2] => -4
+
+// -4 has no matching positive appearance
+
+// [1, -1, 2, -2, 3, 3] => 3
+
+// (the only-positive or only-negative integer may appear more than once)
+
+// Good luck!
+
+function noOpposite(arr){
+    return arr.find(e => !arr.some(f => f===-e))
+}
+
+console.log(noOpposite([-3, 1, 2, 3, -1, -4, -2])) // -4
+
+function noOppositeBis(arr){
+    return arr.find(e => !arr.includes(-e))
+}
+
+console.log(noOppositeBis([-3, 1, 2, 3, -1, -4, -2])) // -4

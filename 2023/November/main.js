ffe15ba5b024a989c08f1d024459c0aff47cfda4
Olivier Reynard <olivier.reynard17@gmail.com>
2023-11-19 14:54:02 +0100

    daily push

--- 2023/November/main.js
+++ 2023/November/main.js
@@ -548,3 +548,41 @@ function pairs(arr){
     }
     return res
 }
+
+//=========================================
+// https://www.codewars.com/kata/5b76a34ff71e5de9db0000f2/train/javascript
+// In this Kata, you will be given a series of times at which an alarm sounds. Your task will be to determine the maximum time interval between alarms. Each alarm starts ringing at the beginning of the corresponding minute and rings for exactly one minute. The times in the array are not in chronological order. Ignore duplicate times, if any.
+
+// For example:
+// solve(["14:51"]) = "23:59". If the alarm sounds now, it will not sound for another 23 hours and 59 minutes.
+// solve(["23:00","04:22","18:05","06:24"]) == "11:40". The max interval that the alarm will not sound is 11 hours and 40 minutes.
+// In the second example, the alarm sounds 4 times in a day.
+
+// More examples in test cases. Good luck!
+
+function timeDifference(arr){
+    if(arr.length <= 1) return "23:59"
+
+    // Convert to minutes, sort it, keep the biggest difference
+
+    let minSorted = arr.map(t => toMin(t)).sort((a,b) => a-b)
+    minSorted = minSorted.concat(minSorted[0]+24*60) // make it cyclic
+    let res = 0
+    for(let i=1 ; i<minSorted.length ; i++){
+        if(Math.abs(minSorted[i] - minSorted[i-1]) > res) res = Math.abs(minSorted[i] - minSorted[i-1])
+    }
+
+    return toString(res - 1) // alarms ring for an entire minute
+
+    function toMin(time){
+        return Number(time.split(":")[0]) * 60 + Number(time.split(":")[1])
+    }
+
+    function toString(min){
+        let h = ("0" + Math.floor(min/60)).slice(-2)
+        let m = ("0" + min%60).slice(-2)
+        return h + ":" + m
+    }
+}
+
+// console.log(timeDifference(["23:00","04:22","18:05","06:24"])) // "11:40"

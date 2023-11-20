c73691785a23104d4b77043df829a230564f0e5d
Olivier Reynard <olivier.reynard17@gmail.com>
2023-11-20 15:13:38 +0100

    daily push

--- 2023/November/main.js
+++ 2023/November/main.js
@@ -586,3 +586,21 @@ function timeDifference(arr){
 }
 
 // console.log(timeDifference(["23:00","04:22","18:05","06:24"])) // "11:40"
+
+//====================================
+// https://www.codewars.com/kata/538835ae443aae6e03000547
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// var addOne = add(1);
+// addOne(3); // 4
+
+// var addThree = add(3);
+// addThree(3); // 6
+
+function add(n){
+    return (m) => m+n
+}
+
+// console.log(add(1)(3)) // 4

43f143adb81395220254b72f8b4a7a3e021542ef
Olivier Reynard <olivier.reynard17@gmail.com>
2023-12-01 22:07:37 +0100

    daily push

--- 2023/November/main.js
+++ 2023/November/main.js
@@ -721,3 +721,27 @@ function min(arr, toReturn) {
     if(toReturn === 'index') return arr.indexOf(Math.min(...arr))
 }
 
+//====================================
+// https://www.codewars.com/kata/597c684822bc9388f600010f/train/javascript
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    
+    getFullName(){
+        return (this.firstName && this.lastName) ? this.firstName+" "+this.lastName : this.firstName+this.lastName
+    }
+    
+}

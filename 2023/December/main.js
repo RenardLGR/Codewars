8f9e310bc592e2eb3c47b5161b48d3fc02e4b8c1
Olivier Reynard <olivier.reynard17@gmail.com>
2023-12-14 06:30:27 +0100

    daily push

--- 2023/December/main.js
+++ 2023/December/main.js
@@ -81,3 +81,107 @@ function maxRot(n) {
 
 // console.log(maxRot(56789)) // 68957
 // console.log(maxRot(38458215)) // 85821534
+
+//==========================================
+// https://www.codewars.com/kata/55b75fcf67e558d3750000a3
+// Write a class Block that creates a block (Duh..)
+
+// Requirements
+// The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+
+// Define these methods:
+
+// `getWidth()` return the width of the `Block`
+
+// `getLength()` return the length of the `Block`
+
+// `getHeight()` return the height of the `Block`
+
+// `getVolume()` return the volume of the `Block`
+
+// `getSurfaceArea()` return the surface area of the `Block`
+// Examples
+//     let b = new Block([2,4,6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+//     b.getWidth() // -> 2
+    
+//     b.getLength() // -> 4
+    
+//     b.getHeight() // -> 6
+    
+//     b.getVolume() // -> 48
+    
+//     b.getSurfaceArea() // -> 88
+// Note: no error checking is needed
+
+// Any feedback would be much appreciated
+
+class Block{
+    constructor(data){
+        const [width, length, height] = data
+        Object.assign(this, {width, length, height})
+        // [this.width, this.length, this.height] = data
+    }
+    
+    getWidth(){
+        return this.width
+    }
+
+    getLength(){
+        return this.length
+    }
+
+    getHeight(){
+        return this.height
+    }
+    
+    getVolume(){
+        return this.width * this.length * this.height
+        // let {l, w, h} = this
+        // return w*l*h
+    }
+
+    getSurfaceArea(){
+        return 2 * ( this.width * this.length + this.width * this.height + this.length * this.height )
+    }
+}
+
+//========================================
+// https://www.codewars.com/kata/5effa412233ac3002a9e471d
+// For this kata you will have to forget how to add two numbers.
+
+// It can be best explained using the following meme:
+// SEE IMG
+// 248 + 208 => 4416
+
+// Dayane Rivas adding up a sum while competing in the Guatemalan television show "Combate" in May 2016
+
+// In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
+
+// You may assume both integers are positive integers.
+
+// Examples
+// 16 + 18 => 214
+// 26 + 39 => 515
+// 122 + 81 => 1103
+// 72 + 9 => 711
+
+function add(num1, num2) {
+    let s1 = ''+num1
+    let s2 = ''+num2
+    const maxLen = Math.max(s1.length, s2.length)
+
+    // Make both strings the same length by adding leading zeroes
+    s1 = ("0".repeat(maxLen) + s1).slice(-maxLen)
+    s2 = ("0".repeat(maxLen) + s2).slice(-maxLen)
+
+    let res = ''
+
+    for(let i=0 ; i<maxLen ; i++){
+        res += +s1[i] + +s2[i]
+    }
+
+    return +res
+}
+
+// console.log(add(248, 208)) // 4416
+// console.log(add(122, 81)) // 1103

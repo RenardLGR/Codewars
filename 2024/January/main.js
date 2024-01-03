473aa333b95b6d53fe766cfdbc7e2f5c9cd4c875
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-03 20:08:54 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -70,3 +70,63 @@ function disariumNumber(n){
 
     return sum === n ? "Disarium !!" : "Not !!"
 }
+
+//===================================
+// https://www.codewars.com/kata/5828713ed04efde70e000346
+// You will be given an array of objects (associative arrays in PHP, table in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP, table in COBOL) which includes the count of each coding language represented at the meetup.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+// ];
+// your function should return the following object (associative array in PHP, table in COBOL):
+
+// { C: 2, JavaScript: 1, Ruby: 1 }
+
+// Notes:
+// The order of the languages in the object does not matter.
+// The count value should be a valid number.
+// The input array will always be valid and formatted as in the example above.
+
+
+// This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+
+function countLanguages(list) {
+    return list.reduce((acc, cur) =>  {
+        acc[cur.language] = (acc[cur.language] || 0) + 1
+        return acc
+    }, {})
+}
+
+//===================================
+// https://www.codewars.com/kata/55d410c492e6ed767000004f
+// Write a function
+
+// vowel2index(str)
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+// E.g:
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+// Your function should be case insensitive to the vowels.
+
+function vowel2index(str) {
+    const vowels = ["a","e","i","o","u"]
+    let res = ''
+    for(let i=0 ; i<str.length ; i++){
+        if(vowels.includes(str[i].toLowerCase())){
+            res += i + 1
+        }else{
+            res += str[i]
+        }
+    }
+    return res
+}
+

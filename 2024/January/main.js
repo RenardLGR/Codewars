7d2d122a0bf5badb8ee7c6df31a6cd1e1b09b5ce
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-07 23:45:13 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -130,3 +130,13 @@ function vowel2index(str) {
     return res
 }
 
+//=================================
+// https://www.codewars.com/kata/51c89385ee245d7ddf000001/train/javascript
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+// Example:
+// solution(5) // should return "Value is 00005"
+
+function padded(value){
+    return ("0000" + value).slice(-5)
+}

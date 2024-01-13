cafed7a1a3038e1ee3e274d5e7d72fb258058fa3
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-13 23:18:47 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -158,12 +158,106 @@ function padded(value){
 
 // 1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 + 9^2 + 10^2 = 385
 
-// Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbes is: 3025 - 385 = 2640
+// Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbers is: 3025 - 385 = 2640
 
 function differenceOfSquares(n){
     let n1 = 0
     let n2 = 0
-    for(let i=0 ; i<n ; i++){
-        
+    for(let i=1 ; i<=n ; i++){
+        n1 += i
+        n2 += i*i
     }
+
+    return n1*n1 - n2
 }
+
+// console.log(differenceOfSquares(10)) // 2640
+
+//=========================
+// https://www.codewars.com/kata/598f76a44f613e0e0b000026/train/javascript
+// Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+// Note: only positive integers will be tested.
+
+function sumOfIntegersInString(s){
+    const numbers = "0123456789"
+    let sum = 0
+    let temp = ""
+    let flagIsNumber = false
+
+    for(let i=0 ; i<s.length ; i++){
+        if(numbers.includes(s[i])){
+            if(flagIsNumber) temp += s[i]
+            else{
+                temp = s[i]
+                flagIsNumber = true
+            }
+        }else{
+            if(flagIsNumber){
+                sum += Number(temp)
+                temp = ""
+            }
+        }
+    }
+
+    sum += Number(temp)
+
+    return sum
+}
+
+// console.log(sumOfIntegersInString("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog")) // 3635
+
+function sumOfIntegersInStringBis(s){
+    const numbers = "0123456789"
+    let sum = 0
+    let temp = ""
+
+    for(let i=0 ; i<=s.length ; i++){
+        if(numbers.includes(s[i])){
+            temp += s[i]
+        }else{
+            // Las interation goes here, cleaning up temp if the string ended with a number
+            // Number("") === 0
+            sum += Number(temp)
+            temp = ""
+        }
+    }
+
+    return sum
+}
+
+// console.log(sumOfIntegersInStringBis("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog")) // 3635
+
+
+//=======================================
+// https://www.codewars.com/kata/58287977ef8d4451f90001a0/train/javascript
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// your function should return true.
+
+// Notes:
+
+// The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+// The input array will always be valid and formatted as in the example above.
+
+
+function isSameLanguage(list) {
+    return new Set(list.map(e => e.language)).size === 1
+}
+
+function isSameLanguageBis(list){
+    return list.every(e => e.language === list[0].language)
+}
+
+//=========================

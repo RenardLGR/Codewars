b0cd8df7bc121116c86ca96b9e091e4f85a3632a
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-23 03:01:10 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -879,3 +879,74 @@ function survivors5(arr) {
 // console.log(survivors5(["a b", "cde", "f g"])) // "abcdefg"
 
 //============================
+// https://www.codewars.com/kata/6138ee916cb50f00227648d9
+// The King organizes the jousting. You are a young human lady and your fiancé is an ogre. Today is his anniversary and he would love to visit the tournament, but it's forbidden for ogres to visit the Kingdom. So you decided to go there, to paint the exact moments of clash of cavalry and to present these paintings to your beloved.
+
+// You are given the array / tuple (listField) of two strings of equal length. Each the string contains "$->" and "<-P"(knight with lance) respectively. The knights move towards each other and they can only take simultaneous steps of length vKnightLeft and vKnightRight. When the index of ">" is equal or more than the index of "<", return the array / tuple representing the knights' positions.
+
+// Some examples of the collision:
+
+//   ["$->  ",  
+//    "  <-P"]     
+//   ["   $-> ",
+//    "    <-P"]
+//   ["   $-> ",
+//    "   <-P "]
+// Notes:
+// "The knight "$->" always starts in the position 0 of the first string;
+// "The knight "<-P" always starts in the last position of the second string;
+// Velocity of knights can be different from 0 to 3 inclusive;
+// Sometimes the collision can happen immediately;
+// Sometimes there is no an immediate collision and velocitity of both knights is 0. At this case return an original array / tuple.
+
+// Example 1:
+// given
+// listField = ["$->    ",
+//              "    <-P"]
+             
+// vKnightLeft = 1
+// vKnightRight = 1  
+
+// return
+//  [" $->   ", 
+//   "   <-P "]
+
+// Example 2:
+// given
+// listField = ["$->",
+//              "<-P"]
+             
+// vKnightLeft = 1
+// vKnightRight = 1  
+
+// return
+//  ["$->", 
+//   "<-P"]
+// If you like this kata, check out the another one: Kingdoms Ep2: The curse (simplified)
+// https://www.codewars.com/kata/6159dda246a119001a7de465
+
+function joust(listField, vKnightLeft, vKnightRight) {
+	let leftSpear = listField[0].indexOf(">")
+	let rightSpear = listField[1].indexOf("<")
+    while((vKnightLeft + vKnightRight) > 0 && leftSpear < rightSpear){
+        //move left
+        for(let i=0 ; i<vKnightLeft ; i++){
+            listField[0] = " " + listField[0]
+            listField[0] = listField[0].slice(0, listField[0].length - 1)
+        }
+
+        //move right
+        for(let i=0 ; i<vKnightRight ; i++){
+            listField[1] = listField[1] + " "
+            listField[1] = listField[1].slice(1)
+        }
+    	leftSpear = listField[0].indexOf(">")
+	    rightSpear = listField[1].indexOf("<")
+    }
+
+    return listField
+}
+
+// console.log(joust(["$->    ", "    <-P"], 1, 1)) //  [" $->   ", "   <-P "]
+
+//================================

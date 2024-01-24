25f0edaf25e4b062429119b95bd8f0f43dc84fca
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-24 03:43:22 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -950,3 +950,66 @@ function joust(listField, vKnightLeft, vKnightRight) {
 // console.log(joust(["$->    ", "    <-P"], 1, 1)) //  [" $->   ", "   <-P "]
 
 //================================
+// https://www.codewars.com/kata/6159dda246a119001a7de465
+// More difficult version: Kingdoms Ep2: The curse (normal)
+// https://www.codewars.com/kata/615b636c3f8bcf0038ae8e8b
+
+// Our King was cursed - He can not pronounce an entire word anymore. Looking for the witch, the inquisition punishes every beautiful and intelligent woman in the Kingdom. Trying to save your wife and to stop the violence, you beg the audience of the Highest Priest, explaining that you can understand the King's speech. The future of your family is in your hands! Given the string speech and the array vocabulary. You should return a string of replaced "encoded" words in speech with appropriate word from vocabulary.
+
+// Notes:
+// Encoded words consist of lowercase letters and at least one asterisk;
+// There will always be only one appropriate word from vocabulary for every word in speech;
+// speech consists of lowercase letters, spaces and marks ?!,. ;
+// There might be more words in vocabulary than words in speech;
+// The length of an encoded word must be the same as an appropriate word of vocabulary;
+// The minimum length of a word is 3;
+// Example:
+// given: speech = "***lo w***d!" and vocabulary = ["hello", "world"]
+
+// return "hello world!" 
+// given: speech = "c**l, w*ak!" and vocabulary = ["hell", "cell", "week", "weak"]
+
+// return "cell, weak!" 
+// If you like this kata, check out the another one: Kingdoms Ep.3: Archery Tournament
+// https://www.codewars.com/kata/616eedc41d5644001ff97462/javascript
+
+function translate(speech, vocabulary) {
+    const specialChars = "?!,. ".split("")
+    let res = ""
+    let currWord = ""
+    for(let i=0 ; i<speech.length ; i++){
+        if(specialChars.includes(speech[i])){
+            if(currWord.length > 0){
+                res += decodeWord(currWord, vocabulary) + speech[i]
+                currWord = ""
+            }else{
+                res += speech[i]
+            }
+        }else{
+            currWord += speech[i]
+        }
+    }
+
+    if(currWord.length > 0){
+        res += decodeWord(currWord, vocabulary)
+    }
+
+    return res
+
+    function decodeWord(encoded, vocabulary){
+        return vocabulary.find(w => {
+            return ( encoded.split('').every((c, idx) => {
+                if(c !== "*"){
+                    return w[idx] === c
+                }
+                return true
+            }) ) && encoded.length === w.length
+        })
+    }
+    // console.log(decodeWord("c**l", ["hell", "cell", "week", "weak"])) // "cell"
+    // console.log(decodeWord("w*ak", ["hell", "cell", "week", "weak"])) // "weak"
+    // console.log(decodeWord("****", ["aaa", "bbbb"])) // "bbbb"
+    // console.log(decodeWord("***", ["aaa", "bbbb"])) // "aaa"
+}
+
+// console.log(translate("*ow ****v* **** ****u oq**y *t***. s*opq. qro***, q*x", ["ooqqu","ptqqq","qqqovq","qpqq","qpx","oqqqy","qropoo","sqopq","qow"])) // "qow qqqovq qpqq ooqqu oqqqy ptqqq. sqopq. qropoo, qpx"

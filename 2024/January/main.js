5d73e67178cfdb57839b062dfe7c385f8b200f70
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-26 22:31:30 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -1032,8 +1032,6 @@ function translate(speech, vocabulary) {
 // Example:
 // Given a speech "a**? *c*. **e," and a vocabulary of ["ace", "acd", "abd"], the expected answer is "abd? acd. ace,".
 
-// If you don't see the image, create the issie please
-
 // Read the end of the story in the "epilogue" test.
 
 function translate2(speech, vocabulary) {
@@ -1088,4 +1086,76 @@ function translate2(speech, vocabulary) {
     }
 }
 
-console.log(translate2("a**? *c*. **e,", ["ace", "acd", "abd"])) // "abd? acd. ace,"
+// console.log(translate2("a**? *c*. **e,", ["ace", "acd", "abd"])) // "abd? acd. ace,"
+
+//=====================================
+// https://www.codewars.com/kata/616eedc41d5644001ff97462/javascript
+// Yesterday the Priest gambled away his monastery, vineyard and all his fortune to the Witch. To make some gold for a living, he decided to enter the King's archery tournament (he is a good archer because before he was a famous outlaw).
+
+// Task
+// You are given the array (target archery) of strings of asterisks (for better visibility) and letters(arrows hitting the target). You should count the number of points of each kind of arrow(lowercase letters), sort them and return the array of the sorted letters. Also, it may happen that two arrows of the same kind end up at the same position. If that happens, you'll see an uppercase letter instead.
+
+// Uppercase letters
+// An uppercase letter means two corresponding lowercase letters in the same place(count it as two arrows). However, there can't be two different types of letters in the same place (because the King ordered to invalidate the previous arrows of the different type in the same place).
+
+// How to score the points:
+// The target is a square (in real life is a round) of "rings". The most remote ring from the center is worth one point and the next rings will be worth one point more than the previous.
+
+// How to sort:
+// Sort the letters by their score(sum of points) in ascending order. If their scores are equal, sort the letters by quantity of arrows in the descending order. If the quantities of arrows are equal, sort by letters(UTF-16) in the ascending order.
+
+// Notes:
+// The square target might be of different length (always odd);
+
+// There will be always the target.
+
+// Example 1:
+//     target:
+//     ["z**",
+//      "*a*",
+//      "***"]
+//     return  ["z", "a"]
+// because z = 1 point and a = 2 points
+
+// Example 2:
+//     target:
+//     ["z*B",
+//      "*a*",
+//      "**z"] 
+//     return [b, z, a]
+// because: b = 2 points(2 arrows), z = 2 points(2 arrows) and a = 2(1 arrow).
+
+// If you like this kata, check out the another one: Kingdoms Ep.4: The Dancing Witch
+// https://www.codewars.com/kata/6171a85207ab6b003fadc43e/javascript
+
+function countAndSort(target) {
+    const size = target.length
+    let quantityArrowMap = {}
+    let scoreMap = {}
+    
+    //map score and arrow quantity
+    for(let row=0 ; row<size ; row++){
+        for(let col=0 ; col<size ; col++){
+            if(target[row][col] !== "*"){
+                let score = scoreCalc(row, col)
+                if(target[ow][col] === target[ow][col].toUpperCase()){ //if uppercase
+                    quantityArrowMap[target[ow][col].toLowerCase()] = (quantityArrowMap[target[ow][col].toLowerCase()] || 0) + 2
+                    scoreMap[target[ow][col].toLowerCase()] = (scoreMap[target[ow][col].toLowerCase()] || 0) + 2*score
+                }else{ // if lower case
+                    quantityArrowMap[target[ow][col].toLowerCase()] = (quantityArrowMap[target[ow][col].toLowerCase()] || 0) + 1
+                    scoreMap[target[ow][col].toLowerCase()] = (scoreMap[target[ow][col].toLowerCase()] || 0) + score
+                }
+            }
+        }
+    }
+
+    return Object.keys(scoreMap).sort((a,b) => {
+        if(scoreMap)
+        //a - b is for ascending order
+    })
+   
+    function scoreCalc(row, col){
+        // score is the distance to the closest side + 1
+        return Math.min(row, size-1-row, col, size-1-col) + 1
+    }
+}

19769430fcce6b1215eecbb3a9d3e7b3a49fb8d4
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-30 06:32:13 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -1396,3 +1396,43 @@ let island3 = [
  ]
 
 // console.log(searchForFood(newEdgeIsland, [2, 7], 9)) // 29
+
+//==================================
+// https://www.codewars.com/kata/5596f6e9529e9ab6fb000014
+// Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second. The check should be case sensitive.
+
+// For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+
+// If the second string isn't a valid rotation of the first string, the method returns -1.
+// Examples:
+// "coffee", "eecoff" => 2
+// "eecoff", "coffee" => 4
+// "moose", "Moose" => -1
+// "isn't", "'tisn" => 2
+// "Esham", "Esham" => 0
+// "dog", "god" => -1
+
+function shiftedDiff(first,second){
+    let shifted = first
+    for(let i=0 ; i<first.length ; i++){
+        if(shifted === second) return i
+        shifted = shiftByOne(shifted)
+    }
+
+    return -1
+
+    // A shift is putting the last character of a string in front
+    function shiftByOne(string){
+        if(string.length === 1) return string
+        let res = string[string.length-1]
+        for(let i=0 ; i<string.length-1 ; i++){
+            res += string[i]
+        }
+        return res
+    }
+}
+
+// console.log(shiftedDiff("coffee", "eecoff")) // 2
+// console.log(shiftedDiff("eecoff","coffee")) // 4
+// console.log(shiftedDiff("isn't","'tisn")) // 2
+

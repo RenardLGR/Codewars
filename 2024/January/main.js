a9c472717849e2392cf327eee1f6bfd3218d8ceb
Olivier Reynard <olivier.reynard17@gmail.com>
2024-01-31 05:27:34 +0100

    daily push

--- 2024/January/main.js
+++ 2024/January/main.js
@@ -1436,3 +1436,49 @@ function shiftedDiff(first,second){
 // console.log(shiftedDiff("eecoff","coffee")) // 4
 // console.log(shiftedDiff("isn't","'tisn")) // 2
 
+function shiftedDiffBis(first, second){
+    return (first.length === second.length) ? (second + second).indexOf(first) : -1
+}
+
+// console.log(shiftedDiffBis("coffee", "eecoff")) // 2
+// console.log(shiftedDiffBis("eecoff","coffee")) // 4
+// console.log(shiftedDiffBis("isn't","'tisn")) // 2
+
+//===================================
+// https://www.codewars.com/kata/5436f26c4e3d6c40e5000282/train/javascript
+// A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+
+// In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+
+// [ 0,  1,    3,      6,   ...]
+//   0  0+1  0+1+2  0+1+2+3
+// Your Task
+// Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+
+// Examples
+//  5  -->  [0,  1,  3,  6,  10,  15]
+// -5  -->  [0, -1, -3, -6, -10, -15]
+//  7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
+
+function sumOfN(n) {
+    let sign = Math.sign(n)
+
+    return Array.from({length : Math.abs(n) + 1}, (e, i) => i === 0 ? 0 : sign * sumOneToN(i))
+
+    function sumOneToN(n){
+        return n * (n+1) / 2
+    }
+}
+
+// console.log(sumOfN(5)) // [0,  1,  3,  6,  10,  15]
+// console.log(sumOfN(-5)) // [0, -1, -3, -6, -10, -15]
+// console.log(sumOfN(7)) // [0,  1,  3,  6,  10,  15,  21,  28]
+
+function sumOfNTer(n){
+    let temp = 0
+    return Array.from({length : Math.abs(n) + 1}, (e, i) => i === 0 ? 0 : temp = Math.sign(n) * (Math.abs(temp) + i))
+}
+
+// console.log(sumOfNTer(5)) // [0,  1,  3,  6,  10,  15]
+// console.log(sumOfNTer(-5)) // [0, -1, -3, -6, -10, -15]
+// console.log(sumOfNTer(7)) // [0,  1,  3,  6,  10,  15,  21,  28]

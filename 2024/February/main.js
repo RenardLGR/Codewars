f535ab7d237d3c95e7bca5909c73fcac58b873e9
Olivier Reynard <olivier.reynard17@gmail.com>
2024-02-04 08:20:10 +0100

    daily push

--- 2024/February/main.js
+++ 2024/February/main.js
@@ -27,3 +27,83 @@ function multiplesBis(m, n){
 }
 
 //==================================
+// https://www.codewars.com/kata/541af676b589989aed0009e7
+// Write a function that counts how many different ways you can make change for an amount of money, given an array of coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2:
+
+// 1+1+1+1, 1+1+2, 2+2.
+// The order of coins does not matter:
+
+// 1+1+2 == 2+1+1
+// Also, assume that you have an infinite amount of coins.
+
+// Your function should take an amount to change and an array of unique denominations for the coins:
+
+// countChange(4, [1,2]) // => 3
+// countChange(10, [5,2,3]) // => 4
+// countChange(11, [5,7]) //  => 0
+
+//Combinations are represented as Strings where each coin is separated by a ":" sorted asc
+//Example : for countChange(4, [1,2]) we will have 
+function countChange(money, coins) {
+    let combinations = []
+    solve(0, "")
+    return combinations.length
+
+    function solve(sum, combination){
+        if(sum > money) return
+
+        if(sum === money){
+            let sortedStr = sortString(combination)
+            //First time this combination is encountered
+            if(!combinations.includes(sortedStr)){
+                combinations.push(sortedStr)
+            }
+            return
+        }
+
+        for(let coin of coins){
+            solve(sum + coin, combination + ":" + coin)
+        }
+
+    }
+
+    function sortString(str){
+        return str.split(":").sort((a,b) => +a - +b).join(":")
+    }
+}
+
+// console.log(countChange(4, [1,2])) // => 3
+// console.log(countChange(10, [5,2,3])) // => 4
+// console.log(countChange(11, [5,7])) //  => 0
+// console.log(countChange(300, [5, 10, 20, 50, 100, 200, 500])) //  => 1222 // TOO LONG
+
+//Did not work, too long
+
+function countChangeBis(money, coins){
+    coins.sort((a,b) => a-b)
+    let combinations = []
+    solve(0, [], 0)
+    // console.log(combinations)
+    return combinations.length
+
+    function solve(sum, combination, moneyIdxStart){
+        if(sum > money) return
+
+        if(sum === money){
+            combinations.push(combination)
+            return
+        }
+
+        //By changing where we start our loop, we avoid doing [1,1,2] and then [2,1,1] since the following elements in our combination array are ascending and coins has no duplicates
+        for(let i=moneyIdxStart ; i<coins.length ; i++){
+            let newCombination = [...combination, coins[i]]
+            solve(sum+coins[i], newCombination, i)
+        }
+
+    }
+}
+
+// console.log(countChangeBis(4, [1,2])) // => 3
+// console.log(countChangeBis(10, [5,2,3])) // => 4
+// console.log(countChangeBis(11, [5,7])) //  => 0
+// console.log(countChangeBis(300, [5, 10, 20, 50, 100, 200, 500])) //  => 1222

834bfd6ac4bc5b5f7f447901e841a569ade746cf
Olivier Reynard <olivier.reynard17@gmail.com>
2024-02-23 23:47:08 +0100

    daily push

--- 2024/February/main.js
+++ 2024/February/main.js
@@ -668,3 +668,20 @@ function noDupes(nums){
 }
 
 console.log(noDupes([3, 4, 4, 3, 6, 3])) // [4, 6, 3]
+
+//===============================================
+// https://www.codewars.com/kata/58640340b3a675d9a70000b9/train/javascript
+// This Kata is intended as a small challenge for my students
+
+// Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// Example (Input --> Output)
+
+// "drake" --> "drk"
+// "aeiou" --> ""
+// remove_vowels("drake") // => "drk"
+// remove_vowels("aeiou") // => ""
+
+var removeVowels = function(str){
+    return str.split("").filter(e => !"aeiou".includes(e)).join("")
+}

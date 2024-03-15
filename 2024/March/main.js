54d33ccd9d633237e0e8a429fdd0342e5b902b00
Olivier Reynard <olivier.reynard17@gmail.com>
2024-03-15 18:31:30 +0100

    daily push

--- 2024/March/main.js
+++ 2024/March/main.js
@@ -180,3 +180,25 @@ function mostFrequentItemCountBis(collection) {
 }
 
 // console.log(mostFrequentItemCountBis([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3])) // 5
+
+//========================
+// https://www.codewars.com/kata/5650ab06d11d675371000003
+// The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+// Example:
+// Split the below string into other strings of size #3
+// 'supercalifragilisticexpialidocious'
+
+// Will return a new string
+// 'sup erc ali fra gil ist ice xpi ali doc iou s'
+// Assumptions:
+
+// String length is always greater than 0
+// String has no spaces
+// Size is always positive
+
+var splitInParts = function(s, partLength){
+    return s.split('').reduce((acc, cur, idx) => idx%partLength===0 ? acc + " " + cur : acc + cur ,"").trim()
+}
+
+console.log(splitInParts("supercalifragilisticexpialidocious", 3)) // "sup erc ali fra gil ist ice xpi ali doc iou s"

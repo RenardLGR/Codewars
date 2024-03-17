143040d1afe6f8600ed5789a42a2ee51161d71f8
Olivier Reynard <olivier.reynard17@gmail.com>
2024-03-17 16:01:47 +0100

    daily push

--- 2024/March/main.js
+++ 2024/March/main.js
@@ -244,3 +244,17 @@ function getUniqueChars(str1, str2){
 function getUniqueCharsBis(str1, str2){
     return (str1+str2).split("").filter(c => !str1.includes(c) || !str2.includes(c)).join("")
 }
+
+//===========================================
+// https://www.codewars.com/kata/5639bdcef2f9b06ce800005b/train/javascript
+// In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+
+// For example:
+
+// "This Is A Test" ==> "TIAT"
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+function makeString(s){
+    return s.split(" ").map(w => w[0]).join("")
+}
+

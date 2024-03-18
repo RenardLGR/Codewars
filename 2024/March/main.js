6359aa3509561246d071f9d9f119b6891ba5fe73
Olivier Reynard <olivier.reynard17@gmail.com>
2024-03-18 21:06:38 +0100

    daily push

--- 2024/March/main.js
+++ 2024/March/main.js
@@ -258,3 +258,15 @@ function makeString(s){
     return s.split(" ").map(w => w[0]).join("")
 }
 
+//============================================
+// https://www.codewars.com/kata/5417423f9e2e6c2f040002ae
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+// 123 => [1,2,3]
+// 1 => [1]
+// 8675309 => [8,6,7,5,3,0,9]
+
+function digitize(n) {
+    return ("" + n).split("").map(Number)
+}

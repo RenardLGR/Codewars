282290403a9082fd6f8370695fae20b194f58943
Olivier Reynard <olivier.reynard17@gmail.com>
2024-04-07 22:44:02 +0200

    daily push

--- 2024/April/main.js
+++ 2024/April/main.js
@@ -33,3 +33,16 @@ function nicknameGenerator(name){
     let vowels = "aeiou"
     return vowels.includes(name[2]) ? name.slice(0, 4) : name.slice(0, 3)
 }
+
+//====================================
+// https://www.codewars.com/kata/5939ab6eed348a945f0007b2
+// When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+
+// Example:
+// 'red white blue' //returns string value of white
+
+// 'red blue gold' //returns gold
+
+function longestWord(stringOfWords){
+    return stringOfWords.split(" ").reduce((acc, cur) => cur.length >= acc.length ? cur : acc, "")
+}

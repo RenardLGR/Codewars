32edcb5e06c0197a37e9b6214132f5ac0fffb274
Olivier Reynard <olivier.reynard17@gmail.com>
2024-04-10 23:18:10 +0200

    skyscrapers puzzle........

--- 2023/October/skyscrapers-reverse-eng.js
+++ 2023/October/skyscrapers-reverse-eng.js
@@ -123,6 +123,7 @@ function dfs(idx) {
     const i = order[idx];
     const possible_bak = [...possible];
 
+    //try different shifts
     for (let j = 0; j < N; j++) {
         const m = (1 << j) & possible[i];
         if (m === 0) continue;
@@ -175,10 +176,10 @@ function solvePuzzleGPT(clues) {
         }
     }
 
-    console.log("possible BEFORE check uniques():", possible)
+    // console.log("possible BEFORE check uniques():", possible)
 
     while (check_unique() > 0);
-    console.log("possible AFTER check uniques():", possible)
+    // console.log("possible AFTER check uniques():", possible)
 
     const idx_npos = [];
     for (let i = 0; i < N * N; i++) {
@@ -192,6 +193,7 @@ function solvePuzzleGPT(clues) {
     order = idx_npos.map(item => item.index);
     dfs(0);
 
+    console.log("res GPT masks:", possible)
     const result = [];
     for (let i = 0; i < N; i++) {
         result.push([...results[i]]);
@@ -201,7 +203,7 @@ function solvePuzzleGPT(clues) {
 
 
 // console.log(solvePuzzleGPT([ 0, 3, 0, 5, 3, 4,  0, 0, 0, 0, 0, 1, 0, 3, 0, 3, 2, 3, 3, 2, 0, 3, 1, 0])) // [[ 5, 2, 6, 1, 4, 3 ], [ 6, 4, 3, 2, 5, 1 ], [ 3, 1, 5, 4, 6, 2 ], [ 2, 6, 1, 5, 3, 4 ], [ 4, 3, 2, 6, 1, 5 ], [ 1, 5, 4, 3, 2, 6 ]]
-// console.log("res GPT:",solvePuzzleGPT([ 3, 2, 2, 3, 2, 1, 1, 2, 3, 3, 2, 2, 5, 1, 2, 2, 4, 3, 3, 2, 1, 2, 2, 4])) //[[ 2, 1, 4, 3, 5, 6], [ 1, 6, 3, 2, 4, 5], [ 4, 3, 6, 5, 1, 2], [ 6, 5, 2, 1, 3, 4], [ 5, 4, 1, 6, 2, 3], [ 3, 2, 5, 4, 6, 1]]
+console.log("res GPT:",solvePuzzleGPT([ 3, 2, 2, 3, 2, 1, 1, 2, 3, 3, 2, 2, 5, 1, 2, 2, 4, 3, 3, 2, 1, 2, 2, 4])) //[[ 2, 1, 4, 3, 5, 6], [ 1, 6, 3, 2, 4, 5], [ 4, 3, 6, 5, 1, 2], [ 6, 5, 2, 1, 3, 4], [ 5, 4, 1, 6, 2, 3], [ 3, 2, 5, 4, 6, 1]]
 
 
 function solvePuzzleRE(clues){
@@ -451,6 +453,10 @@ function solve(clues){
     let possible = Array.from({length:N}, (_) => Array(N).fill(MASK))
 
     fillKnownElement()
+    //it gets wrong from here ->
+    console.log("backtrack:", backtrack())
+    console.table(possible)
+    return maskToNum(possible)
 
     //Initialization : Given the clues, we can figure out some masks possibilities
     function fillKnownElement(){
@@ -641,9 +647,92 @@ function solve(clues){
         return skyscraperSet
     }
 
-    //This function sets a height, modifying the row and col as necessary
+    function backtrack(){
+        // if(!isValid()){
+        //     return false
+        // }
+
+        for(let row=0 ; row<N ; row++){
+            for(let col=0 ; col<N ; col++){
+                if(isMultiplePossibleSkyscraper(possible[row][col])){
+                    let possibleCopy = copy2DArray(possible)
+                    for(let shift=0 ; shift<N ; shift++){
+                        //Check if the height is available
+                        if((1 << shift) & possible[row][col]){
+                            // let possibleCopy = copy2DArray(possible)
+                            setValue(row, col, 1 << shift)
+                            if(isValid() && backtrack()){
+                                return true
+                            }
+                            else{
+                                possible = possibleCopy
+                            }
+                            // possible = possibleCopy // this doesn't change anything
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return isValid()
+    }
+
+    //Check if the clues are being respected, i.e. if a fully set col or row respects the clue
+    function isValid(){
+        // const cluesCpy = clues.slice()
+        // let cluesClean = [cluesCpy.splice(0,N), cluesCpy.splice(0,N), cluesCpy.splice(0,N), cluesCpy.splice(0,N)]
+
+        loopClue: for(let c=0 ; c<clues.length ; c++){
+            if(clues[c] === 0) continue
+            let direction = Math.floor(c/N) // returns a number in interval [0;3] answering if the clue is top to bottom, right to left, bottom to top or left to right
+            // let idx = c%N
+            let isRowColComplete = true
+            let largest = 0
+            let visible = 0
+            //Go through the row/col associated with the clue
+            for(let i=0 ; i<N ; i++){
+                let curr
+                switch(direction){
+                    //top to bottom
+                    case 0:
+                        curr = possible[i][c%N]
+                        break;
+
+                    //right to left
+                    case 1:
+                        curr = possible[c%N][N-1-i]
+                        break;
+
+                    //bottom to top
+                    case 2:
+                        curr = possible[N-1-i][N-1-c%N]
+                        break;
+
+                    //left to right
+                    case 3:
+                        curr = possible[N-1-c%N][i]
+                        break;
+
+                }
+                // We can't check for a row/col to be correct if at least one skyscraper is not set, continue to the next clue
+                if(isMultiplePossibleSkyscraper(curr)){
+                    isRowColComplete = false
+                    continue loopClue
+                }
+                if(curr > largest){
+                    visible++
+                    largest = curr
+                }
+            }
+            if(visible !== clues[c]) return false
+        }
+
+        return true
+    }
+
+    //This function sets a height, removing this possibility in the row and col as necessary
     function setValue(row, col, mask){
-        //Example : Given a current bitmask of "011100" = 28b and trying to remove the skyscraperMask of "001000" = 8 with ~"001000" = "110111"
+        //Example : Given a current bitmask of "011100" = 28b and trying to remove the skyscraperMask of "001000" = 8b with ~"001000" = "110111"
         //The new bitmask should be "011100" & "110111" = "010100" = 20b and is given by the formula newBitmask = oldBitmask & ~skyscraperMask
         //or simply put, cur &= ~skyscraperMask
         for(let i=0 ; i<N ; i++){
@@ -654,6 +743,152 @@ function solve(clues){
         }
         possible[row][col] = mask
     }
+
+    //! NO NEED?
+    //This function removes a height, adding this possibility in the row and col as necessary
+    function removeValue(row, col, mask){
+        //Given a current bitmask of "010100" = 20 and trying to add the skyscraperMask of "001000" = 8
+        //The new bitmask should be "010100" | "001000" = "011100" = 28 and is given by the formula newBitmask = oldBitmask | skyscraperMask
+        //or simply put, cur |= skyscraperMask
+        for(let i=0 ; i<this.N ; i++){
+            //modify row
+            possible[row][i] |= mask
+            //modify col
+            possible[i][col] |= mask
+        }
+        possible[row][grid] = mask
+    }
+
+    function isMultiplePossibleSkyscraper(mask){
+        // Count the number of set bits in the binary representation
+        let count = 0
+        while (mask) {
+            // In each iteration, mask & 1 is used to check the value of the least significant bit (LSB) of the current mask.
+            // If the LSB is 1, mask & 1 evaluates to 1, and count is incremented by 1.
+            // If the LSB is 0, mask & 1 evaluates to 0, and count remains unchanged.
+            count += mask & 1
+            // After checking the LSB, the entire mask is right-shifted by 1 position.
+            mask >>= 1
+        }
+
+        // If there is exactly one set bit, return false
+        return count !== 1
+    }
+
+    function copy2DArray(grid){
+        return grid.map(subarr => subarr.slice())
+    }
+
+    function maskToNum(grid){
+        return grid.map(subarr => subarr.map(mask => {
+            for(let shift=0 ; shift<N ; shift++){
+                if((1 << shift) & mask){
+                    return shift + 1
+                }
+            }
+        }))
+    }
 }
 
-// console.log("res :",solve([ 3, 2, 2, 3, 2, 1, 1, 2, 3, 3, 2, 2, 5, 1, 2, 2, 4, 3, 3, 2, 1, 2, 2, 4])) //[[ 2, 1, 4, 3, 5, 6], [ 1, 6, 3, 2, 4, 5], [ 4, 3, 6, 5, 1, 2], [ 6, 5, 2, 1, 3, 4], [ 5, 4, 1, 6, 2, 3], [ 3, 2, 5, 4, 6, 1]]
+console.log("res :",solve([ 3, 2, 2, 3, 2, 1, 1, 2, 3, 3, 2, 2, 5, 1, 2, 2, 4, 3, 3, 2, 1, 2, 2, 4])) //[[ 2, 1, 4, 3, 5, 6], [ 1, 6, 3, 2, 4, 5], [ 4, 3, 6, 5, 1, 2], [ 6, 5, 2, 1, 3, 4], [ 5, 4, 1, 6, 2, 3], [ 3, 2, 5, 4, 6, 1]]
+
+
+// let grid = [2,  1,  8, 4, 16, 32,  1, 32, 4,
+// 2,  8, 16, 8,  4, 32, 16,  1, 2,
+// 32, 16,  2, 1,  4,  8, 16,  8, 1,
+// 32,  2,  4, 4,  2, 16,  8, 32, 1
+// ].reduce((acc, cur) =>{
+//     if(acc[acc.length-1].length === 6){
+//         acc.push([])
+//     }
+//     acc[acc.length-1].push(cur)
+//     return acc
+// } ,[[]])
+
+// console.log(grid);
+
+// =============================== TESTING... ===================================
+
+
+let clues = [ 3, 2, 2, 3, 2, 1, 1, 2, 3, 3, 2, 2, 5, 1, 2, 2, 4, 3, 3, 2, 1, 2, 2, 4]
+possible = [
+    [ 2, 1, 8, 4, 16, 32 ],
+    [ 1, 32, 4, 2, 8, 16 ],
+    [ 8, 4, 32, 16, 1, 2 ],
+    [ 32, 16, 2, 1, 4, 8 ],
+    [ 16, 8, 1, 32, 2, 4 ],
+    [ 4, 2, 16, 8, 32, 1 ]
+  ]
+
+// N = 6
+
+function isMultiplePossibleSkyscraper(mask){
+    // Count the number of set bits in the binary representation
+    let count = 0
+    while (mask) {
+        // In each iteration, mask & 1 is used to check the value of the least significant bit (LSB) of the current mask.
+        // If the LSB is 1, mask & 1 evaluates to 1, and count is incremented by 1.
+        // If the LSB is 0, mask & 1 evaluates to 0, and count remains unchanged.
+        count += mask & 1
+        // After checking the LSB, the entire mask is right-shifted by 1 position.
+        mask >>= 1
+    }
+
+    // If there is exactly one set bit, return false
+    return count !== 1
+}
+
+//Check if the clues are being respected, i.e. if a fully set col or row respects the clue
+function isValid() {
+    // const cluesCpy = clues.slice()
+    // let cluesClean = [cluesCpy.splice(0,N), cluesCpy.splice(0,N), cluesCpy.splice(0,N), cluesCpy.splice(0,N)]
+
+    loopClue: for (let c = 0; c < clues.length; c++) {
+        if (clues[c] === 0) continue
+        let direction = Math.floor(c / N) // returns a number in interval [0;3] answering if the clue is top to bottom, right to left, bottom to top or left to right
+        // let idx = c%N
+        let isRowColComplete = true
+        let largest = 0
+        let visible = 0
+        //Go through the row/col associated with the clue
+        for (let i = 0; i < N; i++) {
+            let curr
+            switch (direction) {
+                //top to bottom
+                case 0:
+                    curr = possible[i][c % N]
+                    break;
+
+                //right to left
+                case 1:
+                    curr = possible[c % N][N - 1 - i]
+                    break;
+
+                //bottom to top
+                case 2:
+                    curr = possible[N - 1 - i][N - 1 - c % N]
+                    break;
+
+                //left to right
+                case 3:
+                    curr = possible[N - 1 - c % N][i]
+                    break;
+
+            }
+            // We can't check for a row/col to be correct if at least one skyscraper is not set, continue to the next clue
+            if (isMultiplePossibleSkyscraper(curr)) {
+                isRowColComplete = false
+                continue loopClue
+            }
+            if (curr > largest) {
+                visible++
+                largest = curr
+            }
+        }
+        if (visible !== clues[c]) return false
+    }
+
+    return true
+}
+
+// console.log(isValid())

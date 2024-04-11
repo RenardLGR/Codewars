ec22d3aecab03efed2b9fa2544b979258d501567
Olivier Reynard <olivier.reynard17@gmail.com>
2024-04-11 23:47:10 +0200

    skyscrapers.........

--- 2023/October/skyscrapers-reverse-eng.js
+++ 2023/October/skyscrapers-reverse-eng.js
@@ -892,3 +892,57 @@ function isValid() {
 }
 
 // console.log(isValid())
+
+// ==================== SUDOKU ====================
+const sudoku = [[5,3,0,0,7,0,0,0,0],[6,0,0,1,9,5,0,0,0],[0,9,8,0,0,0,0,6,0],[8,0,0,0,6,0,0,0,3],[4,0,0,8,0,3,0,0,1],[7,0,0,0,2,0,0,0,6],[0,6,0,0,0,0,2,8,0],[0,0,0,4,1,9,0,0,5],[0,0,0,0,8,0,0,7,9]]
+const sol = [[5,3,4,6,7,8,9,1,2],[6,7,2,1,9,5,3,4,8],[1,9,8,3,4,2,5,6,7],[8,5,9,7,6,1,4,2,3],[4,2,6,8,5,3,7,9,1],[7,1,3,9,2,4,8,5,6],[9,6,1,5,3,7,2,8,4],[2,8,7,4,1,9,6,3,5],[3,4,5,2,8,6,1,7,9]]
+
+const sudoku2 = [[4,0,0,0,5,0,0,0,0],[0,2,0,1,9,0,6,0,4],[5,1,0,4,0,0,0,0,0],[0,0,0,5,7,0,9,0,2],[0,4,0,0,0,0,0,8,0],[2,0,5,0,6,4,0,0,0],[0,0,0,0,0,1,0,6,3],[3,0,8,0,2,0,0,1,0],[0,0,0,0,4,0,0,0,5]]
+const sol2 = [[4,9,7,8,5,6,3,2,1],[8,2,3,1,9,7,6,5,4],[5,1,6,4,3,2,7,9,8],[6,3,1,5,7,8,9,4,2],[7,4,9,2,1,3,5,8,6],[2,8,5,9,6,4,1,3,7],[9,5,4,7,8,1,2,6,3],[3,7,8,6,2,5,4,1,9],[1,6,2,3,4,9,8,7,5]]
+
+function sudokuSolver(grid){
+    solve()
+    return grid
+    function solve(){
+        for(let row=0 ; row<9 ; row++){
+            for(let col=0 ; col<9 ; col++){
+                if(grid[row][col] === 0){
+                    for(let num=1 ; num<=9 ; num++){
+                        if(isValid(row, col, num)){
+                            grid[row][col] = num
+                            if(solve()){
+                                return true
+                            }
+                            else grid[row][col] = 0
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+
+    function isValid(row, col, number){
+        //Check uniqueness in row & col
+        for(let i=0 ; i<9 ; i++){
+            if(grid[row][i] === number) return false
+            if(grid[i][col] === number) return false
+        }
+
+        //Check uniqueness in box
+        let rowStart = Math.floor(row/3)*3
+        let colStart = Math.floor(col/3)*3
+        for(let r=rowStart ; r<rowStart+3 ; r++){
+            for(let c=colStart ; c<colStart+3 ; c++){
+                if(grid[r][c] === number) return false
+            }
+        }
+
+        return true
+    }
+}
+
+// console.log(JSON.stringify(sudokuSolver(sudoku)))
+// console.log(JSON.stringify(sudokuSolver(sudoku2)))

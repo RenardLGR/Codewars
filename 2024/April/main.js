003316a917db629f3aaf455f7dc3278d40ce9dfe
Olivier Reynard <olivier.reynard17@gmail.com>
2024-04-19 19:32:51 +0200

    daily push

--- 2024/April/main.js
+++ 2024/April/main.js
@@ -70,3 +70,20 @@ function twoArePositive(a, b, c) {
 function twoArePositiveBis(a, b, c) {
     return (a > 0) + (b > 0) + (c > 0) === 2
 }
+
+//============================================
+// https://www.codewars.com/kata/56d19b2ac05aed1a20000430
+// Given an array of numbers, return the difference between the largest and smallest values.
+
+// For example:
+
+// [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+// [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+// The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+function betweenExtremes(numbers) {
+    let sorted = numbers.sort((a,b) => b-a)
+    return sorted[0] - sorted[sorted.length-1]
+}

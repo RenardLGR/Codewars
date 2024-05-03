fdf288b495c90614a19661fe5e63c73ac001eefb
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-03 19:54:34 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -21,3 +21,22 @@ function lostSheep(friday,saturday,total){
     return total - friday.reduce((acc, cur) => acc + cur, 0) - saturday.reduce((acc, cur) => acc + cur, 0)
     //return total - friday.concat(saturday).reduce((acc, cur) => acc + cur, 0)
 }
+
+//=============================================
+// https://www.codewars.com/kata/59727ff285281a44e3000011
+// My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+
+// "dolphin" -> "The Dolphin"
+
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+
+// "alaska" -> "Alaskalaska"
+
+// Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+function bandNameGenerator(str) {
+    return str[0] === str[str.length-1] ? str[0].toUpperCase() + str.slice(1) + str.slice(1) : "The " + str[0].toUpperCase() + str.slice(1)
+}
+
+// console.log(bandNameGenerator("dolphin")) // "The Dolphin"
+// console.log(bandNameGenerator("alaska")) // "Alaskalaska"

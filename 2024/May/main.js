485f4ab5e440324156dfaef6e656cef96d390359
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-11 23:49:06 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -211,3 +211,22 @@ function spaceyTer(array) {
 
 // console.log(spaceyTer(['kevin', 'has','no','space'])) // ['kevin','kevinhas','kevinhasno','kevinhasnospace']
 
+//============================================
+// https://www.codewars.com/kata/55caf1fd8063ddfa8e000018
+// In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+// The result should be a string of numbers, separated by comma and space.
+
+// Example
+// # first element: 1, difference: 2, how many: 5
+// arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+
+function arithmeticSequenceElements(a, d, n) {
+	let res = ""
+    let curr = a
+    for(let i=0 ; i<n ; i++){
+        res += curr + ", "
+        curr += d
+    }
+    return res.slice(0, res.length-2)
+}

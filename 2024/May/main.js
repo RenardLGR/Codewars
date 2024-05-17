a29aebce61dbc669ffccd48d83f3880afb334231
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-17 23:07:11 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -334,3 +334,19 @@ function twoDecimalPlacesBis(number){
 
 // console.log(twoDecimalPlacesBis(32.8493)) // 32.84
 // console.log(twoDecimalPlacesBis(-2662382.91989303)) // -2662382.91
+
+//===================================
+// https://www.codewars.com/kata/59a1cdde9f922b83ee00003b
+// The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+
+// Write a function which takes an integer array and returns its Stanton measure.
+
+// Examples
+// The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+
+// The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
+
+function stantonMeasure(a){
+    let n = a.filter(e => e === 1).length
+    return a.filter(e => e === n).length
+}

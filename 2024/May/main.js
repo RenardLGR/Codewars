926dc213e02cdce24019034da58d8659ebaf7bdd
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-18 23:26:10 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -350,3 +350,25 @@ function stantonMeasure(a){
     let n = a.filter(e => e === 1).length
     return a.filter(e => e === n).length
 }
+
+//=============================
+// https://www.codewars.com/kata/5959ec605595565f5c00002b
+// Write a function that reverses the bits in an integer.
+
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+// You can assume that the number is not negative.
+
+function reverseBits(n){
+    return parseInt(n.toString(2).split("").reverse().join(""), 2)
+}
+
+console.log(reverseBits(417)) // 267
+
+function reverseBitsBis(n){
+    let res = 0
+    let shifts = 0
+    while(n){
+        
+    }
+}

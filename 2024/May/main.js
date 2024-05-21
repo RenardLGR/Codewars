f44cd1ee71afcec466497ef2b5765d5e75ed71d1
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-21 23:51:28 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -407,3 +407,15 @@ function reverseBitsTer(n){
 function average(scores) {
     return Math.round(scores.reduce((acc, cur) => acc+cur, 0) / scores.length)
 }
+
+//=======================================
+// https://www.codewars.com/kata/5250a89b1625e5decd000413
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+var flatten = function (array){
+    return array.flat()
+}

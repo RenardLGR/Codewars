1467f1f65a25579ad4b9792004121f4b896ee380
Olivier Reynard <olivier.reynard17@gmail.com>
2024-05-23 14:57:47 +0200

    daily push

--- 2024/May/main.js
+++ 2024/May/main.js
@@ -428,3 +428,44 @@ var flatten = function (array){
 function getMinMax(arr){
     return[Math.min(...arr), Math.max(...arr)]
 }
+
+//===========================================
+// https://www.codewars.com/kata/51fc12de24a9d8cb0e000001
+// ISBN-10 identifiers are ten digits long. The first nine characters are digits 0-9. The last digit can be 0-9 or X, to indicate a value of 10.
+
+// An ISBN-10 number is valid if the sum of the digits multiplied by their position modulo 11 equals zero.
+
+// For example:
+
+// ISBN     : 1 1 1 2 2 2 3 3 3  9
+// position : 1 2 3 4 5 6 7 8 9 10
+// This is a valid ISBN, because:
+
+// (1*1 + 1*2 + 1*3 + 2*4 + 2*5 + 2*6 + 3*7 + 3*8 + 3*9 + 9*10) % 11 = 0
+// Examples
+// 1112223339   -->  true
+// 111222333    -->  false
+// 1112223339X  -->  false
+// 1234554321   -->  true
+// 1234512345   -->  false
+// 048665088X   -->  true
+// X123456788   -->  false
+
+function validISBN10(isbn){
+    if(isbn.length !== 10) return false
+    let numbers = "0123456789"
+    let sum = 0
+    
+    for(let i=0 ; i<9 ; i++){
+        if(!numbers.includes(isbn[i])) return false
+        sum += Number(isbn[i]) * (i+1)
+    }
+    if(isbn[9] !== "X" && !numbers.includes(isbn[9])) return false
+
+    if(isbn[9] === "X") sum += 10 * 10
+    else sum += Number(isbn[9]) * 10
+
+    return sum % 11 === 0
+}
+
+// console.log(validISBN10("1112223339")) // true

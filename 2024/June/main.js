ef1c2d8ad9091c53aab4d218027ae51766c11761
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-05 22:59:22 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -65,3 +65,14 @@ class Person{
 }
 
 //===================================================
+// https://www.codewars.com/kata/5a1a9e5032b8b98477000004
+// Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+
+// Indices in sequence start from 0.
+
+// If the sequence is empty, you should return 0.
+
+function evenLast(numbers) {
+    if(numbers.length === 0) return 0
+    return numbers[numbers.length - 1] * numbers.reduce((acc, curr, idx) => idx%2 === 0 ? acc + curr : acc, 0)
+}

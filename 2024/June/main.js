8fe89fae8ba351f2284a256fb2de576bd33d943d
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-10 20:56:22 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -119,3 +119,23 @@ function covfefeQuater(str){
     //return str.match(regexp) ...
     return regexp.test(str) ? str.replace(regexp, "covfefe") : str + " covfefe"
 }
+
+
+//=========================================
+// https://www.codewars.com/kata/51c7d8268a35b6b8b40002f2
+// Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+function keyEqualsValue(pairs){
+    let res = ""
+    for(let key in pairs){
+        res += `${key} = ${pairs[key]},`
+    }
+
+    return res.slice(0, -1)
+}

b440f811d8f1383e3e2da3d7bbee48c905ba5250
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-12 19:30:57 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -147,3 +147,39 @@ function keyEqualsValueBis(pairs){
 function keyEqualsValueTer(pairs){
     return Object.entries(pairs).map(([k, v]) => `${k} = ${v}`).join(",")
 }
+
+//=============================================
+// https://www.codewars.com/kata/5b358a1e228d316283001892/train/javascript
+// You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using asterisks (*).
+
+// For example:
+
+// "Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+// As you can see, the letter c is shown only once, but with 2 asterisks.
+
+// The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+
+// Note that the return string must list the letters in order of their first appearance in the original string.
+
+// More examples:
+
+// "Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+// "Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+// Have fun! ;)
+
+function getStrings(city){
+    const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+    return city.toLowerCase().split("").reduce((acc, curr, idx, arr) => {
+        //check for letter && first occurrence
+        if(alphaL.includes(curr) && idx === arr.indexOf(curr)){
+            let rep = arr.filter(e => e === curr).length
+            acc += curr + ":" + "*".repeat(rep) + ","
+        }
+
+        return acc
+    }, "").slice(0, -1)
+}
+
+console.log(getStrings("Chicago")) // "c:**,h:*,i:*,a:*,g:*,o:*"
+console.log(getStrings("Bangkok")) // "b:*,a:*,n:*,g:*,k:**,o:*"
+console.log(getStrings("Las Vegas")) // "l:*,a:**,s:**,v:*,e:*,g:*"

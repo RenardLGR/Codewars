669bb9434b8811057268fae79815ee568308a57e
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-14 08:14:09 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -194,6 +194,62 @@ function getStringsBis(city){
     return Object.entries(freq).map(([key, val]) => `${key}:${val}`).join(",")
 }
 
-console.log(getStringsBis("Chicago")) // "c:**,h:*,i:*,a:*,g:*,o:*"
-console.log(getStringsBis("Bangkok")) // "b:*,a:*,n:*,g:*,k:**,o:*"
-console.log(getStringsBis("Las Vegas")) // "l:*,a:**,s:**,v:*,e:*,g:*"
+// console.log(getStringsBis("Chicago")) // "c:**,h:*,i:*,a:*,g:*,o:*"
+// console.log(getStringsBis("Bangkok")) // "b:*,a:*,n:*,g:*,k:**,o:*"
+// console.log(getStringsBis("Las Vegas")) // "l:*,a:**,s:**,v:*,e:*,g:*"
+
+//===============================================
+// https://www.codewars.com/kata/569b5cec755dd3534d00000f
+// The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he could have made a mistake.
+
+// if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+
+// Could you help him?
+
+// Task
+// The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+
+// Should the last donation be a non positive number (<= 0) John wants us:
+
+// to return:
+
+// Nothing in Haskell, Elm
+// None in F#, Ocaml, Rust, Scala
+// -1 in C, D, Fortran, Nim, PowerShell, Go, Pascal, Prolog, Lua, Perl, Erlang
+// or to throw an error (some examples for such a case):
+
+// IllegalArgumentException() in Clojure, Java, Kotlin
+// ArgumentException() in C#
+// echo ERROR in Shell
+// argument-error in Racket
+// std::invalid_argument in C++
+// ValueError in Python
+// So, he will clearly see that his expectations are not great enough. In "Sample Tests" you can see what to return.
+
+// Notes:
+// all donations and navg are numbers (integers or floats), arr can be empty.
+// See examples below and "Sample Tests" to see which return is to be done.
+// new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
+// new_avg([14, 30, 5, 7, 9, 11, 15], 2) 
+// should raise an error (ValueError or invalid_argument or argument-error or DomainError or ... ) 
+// or return `-1` or ERROR or Nothing or None depending on the language.
+
+function newAvg(arr, newavg) {
+    let n = arr.length
+    let s = arr.reduce((acc, cur) => acc + cur, 0)
+    //Let the average or mean m, the number of elements n and the sum s
+    //We have m = s/n
+    //We want to add x so (s+x)/(n+1) = newavg
+    //We have x = (n+1) * (newavg - s/n+1)
+    //        x = newavg*n + newavg - s
+
+    if(newavg < s/n){
+        throw new Error("Expected New Average is too low")
+    }
+
+    return Math.ceil(newavg*n + newavg - s)
+}
+
+// console.log(newAvg([14, 30, 5, 7, 9, 11, 16], 90)) // 628
+// console.log(newAvg([14, 30, 5, 7, 9, 11, 15], 92)) // 645
+// console.log(newAvg([14, 30, 5, 7, 9, 11, 15], 2)) // Error : "Expected New Average is too low"

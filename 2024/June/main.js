b1ca1cf575dfb9c8e66d7e36901f7c7c49f650bc
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-24 20:24:08 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -322,3 +322,11 @@ function findAdmin(list, lang) {
 function arrowArea(a,b) {
     return a*b / 4
 }
+
+//=========================================
+// https://www.codewars.com/kata/567bed99ee3451292c000025/train/javascript
+// Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+
+String.prototype.vowel = function() {
+    return this.length === 1 ? /[aeiou]/i.test(this) : false
+}

dfa79ad6a2df43363df33a7301b3b222cdc31ef8
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-26 18:48:32 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -336,3 +336,44 @@ String.prototype.vowelBis = function() {
     //$: This is the end-of-line anchor. It asserts that the match must end at the end of the string or line.
     return /^[aeiou]$/i.test(this)
 }
+
+//================================================
+// https://www.codewars.com/kata/54e8c3e89e2ae6f4900005a1
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+// For example:
+
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+// After adding the property the result should be:
+
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+// The questions array is already defined for you and is not the same as the one in the example.
+
+function usersAnswerIsNull(questions){
+    questions = questions.map(o => {
+        o.usersAnswer = null
+        return o
+    })
+}
+
+function usersAnswerIsNullBis(questions){
+    questions.forEach(o => o.usersAnswer = null)
+}

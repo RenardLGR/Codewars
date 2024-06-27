55329f740b107d1f2f05cfaf58ea673539f23be0
Olivier Reynard <olivier.reynard17@gmail.com>
2024-06-27 21:04:53 +0200

    daily push

--- 2024/June/main.js
+++ 2024/June/main.js
@@ -377,3 +377,20 @@ function usersAnswerIsNull(questions){
 function usersAnswerIsNullBis(questions){
     questions.forEach(o => o.usersAnswer = null)
 }
+
+//===============================================
+// https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7
+// The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+// Example
+// unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//                "B---eg---in---n---er---"
+// Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
+
+function unscrambleEggs(word){
+    return word.replace(/egg/ig, "")
+}
+
+function unscrambleEggsBis(word){
+    return word.split("egg").join("")
+}

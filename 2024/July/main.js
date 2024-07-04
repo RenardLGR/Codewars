2c408745e2c6e8702346842c0546e13fab9f9d47
Olivier Reynard <olivier.reynard17@gmail.com>
2024-07-04 23:29:56 +0200

    daily push

--- 2024/July/main.js
+++ 2024/July/main.js
@@ -4,3 +4,24 @@ const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 //===============================================
+// https://www.codewars.com/kata/57fb142297e0860073000064
+// Count the number of exclamation marks and question marks, return the product.
+
+// Examples
+// ""          --->   0
+// "!"         --->   0
+// "!ab? ?"    --->   2
+// "!!"        --->   0
+// "!??"       --->   2
+// "!???"      --->   3
+// "!!!??"     --->   6
+// "!!!???"    --->   9
+// "!???!!"    --->   9
+// "!????!!!?" --->  20
+
+function product (string) {
+    let em = string.split("").filter(e => e === "!")
+    let qm = string.split("").filter(e => e === "?")
+
+    return em.length * qm.length
+}

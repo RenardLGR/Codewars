408fe94639b8d1e873a3f94794f93490a5f93b67
Olivier Reynard <olivier.reynard17@gmail.com>
2024-07-16 23:24:01 +0200

    daily push

--- 2024/July/main.js
+++ 2024/July/main.js
@@ -172,3 +172,26 @@ function consecutiveTer(array){
     //This supposes every element of the array is unique
     return array.sort((a,b) => a-b).reduce((acc, cur, idx, arr) => idx===0 ? 0 : acc + cur - arr[idx-1] - 1, 0)
 }
+
+//===============================================
+// https://www.codewars.com/kata/5f709c8fb0d88300292a7a9d
+// Who is the killer?
+// Some people have been killed!
+// You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+// Task.
+// Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+// {'James': ['Jacob', 'Bill', 'Lucas'],
+//  'Johnny': ['David', 'Kyle', 'Lucas'],
+//  'Peter': ['Lucy', 'Kyle']}
+// and also a list of the names of the dead people:
+
+// ['Lucas', 'Bill']
+// return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+
+function killer(suspectInfo, dead) {
+    for(let suspect in suspectInfo){
+        if(dead.every(d => suspectInfo[suspect].includes(d))) return suspect
+    }
+}

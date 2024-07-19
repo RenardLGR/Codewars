f0ae220c311acc550d86b020fa97b91eeb51c516
Olivier Reynard <olivier.reynard17@gmail.com>
2024-07-19 23:35:06 +0200

    daily push

--- 2024/July/main.js
+++ 2024/July/main.js
@@ -199,3 +199,21 @@ function killer(suspectInfo, dead) {
 function killerBis(suspectInfo, dead){
     return Object.keys(suspectInfo).find(s => dead.every(d => suspectInfo[s].includes(d)))
 }
+
+//==========================================================
+// https://www.codewars.com/kata/558dd9a1b3f79dc88e000001/train/javascript
+// You are given an array of n+1 integers 1 through n. In addition there is a single duplicate integer.
+
+// The array is unsorted.
+
+// An example valid array would be [3, 2, 5, 1, 3, 4]. It has the integers 1 through 5 and 3 is duplicated. [1, 2, 4, 5, 5] would not be valid as it is missing 3.
+
+// You should return the duplicate value as a single integer.
+
+function findDup(arr){
+    for(let i=0 ; i<arr.length ; i++){
+        for(let j=i+1 ; j<arr.length ; j++){
+            if(arr[i] === arr[j]) return arr[i]
+        }
+    }
+}
